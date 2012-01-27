7b75f8321d988ffd777bbfc5b403ed338926a2b3
Hanchuan Peng <hanchuan.peng@gmail.com>
2012-01-27 16:10:30 +0000

    add the simple bioformatio plugin for yinan's further tuning

---
+++ hackathon/bioformatio_simple_bind/iBioformatIO.cpp
@@ -0,0 +1,132 @@
+/* iBioformatIO.cpp
+ * 2011-12-02: create this program by Yang Yu
+ */
+
+
+// 
+
+#ifndef __IBIOFORMATIO_CPP__
+#define __IBIOFORMATIO_CPP__
+
+#include <QtGui>
+#include <QFileInfo>
+
+#include <cmath>
+#include <stdlib.h>
+#include <ctime>
+
+#include "iBioformatIO.h"
+
+#include "basic_surf_objs.h"
+#include "stackutil.h"
+#include "volimg_proc.h"
+#include "img_definition.h"
+#include "basic_landmark.h"
+
+#include "mg_utilities.h"
+#include "mg_image_lib.h"
+
+#include "basic_landmark.h"
+#include "basic_4dimage.h"
+
+#include <iostream>
+using namespace std;
+
+#include <string.h>
+
+
+//plugin interface
+const QString title = "Image IO Using Bioformat";
+
+Q_EXPORT_PLUGIN2(imageIO_Bioformat, IBioformatIOPlugin);
+
+QStringList IBioformatIOPlugin::menulist() const
+{
+    return QStringList() << tr("ImageIO_Bioformat")
+                         << tr("About");
+}
+
+void IBioformatIOPlugin::domenu(const QString &menu_name, V3DPluginCallback2 &callback, QWidget *parent)
+{
+    if (menu_name == tr("ImageIO_Bioformat"))
+    {
+        
+        // input
+        QString m_FileName = QFileDialog::getOpenFileName(parent, QObject::tr("Open An Image"),
+                                                          QDir::currentPath(),
+                                                          QObject::tr("Image File (*.*)"));
+        
+        if(m_FileName.isEmpty())
+        {
+            printf("\nError: Your image does not exist!\n");
+            return;
+        }
+        
+        // temp
+        QString baseName = QFileInfo(m_FileName).baseName();
+        QString tmpfile = QDir::tempPath().append("/").append(baseName).append(".tif");
+        
+        //
+        QString cmd_loci = QString("java -cp loci_tools.jar loci.formats.tools.ImageConverter %1 %2").arg(m_FileName.toStdString().c_str()).arg(tmpfile.toStdString().c_str());
+        
+        system(qPrintable(cmd_loci));
+        
+        // load
+        V3DLONG *sz_relative = 0;
+        int datatype_relative = 0;
+        unsigned char* relative1d = 0;
+        
+        if (loadImage(const_cast<char *>(tmpfile.toStdString().c_str()), relative1d, sz_relative, datatype_relative)!=true)
+        {
+            fprintf (stderr, "Error happens in reading the subject file [%s]. Exit. \n",tmpfile.toStdString().c_str());
+            return;
+        }
+        
+        // visualize
+        Image4DSimple p4DImage;
+        
+        if(datatype_relative == V3D_UINT8)
+        {
+            p4DImage.setData((unsigned char*)relative1d, sz_relative[0], sz_relative[1], sz_relative[2], sz_relative[3], V3D_UINT8);
+        }
+        else if(datatype_relative == V3D_UINT16)
+        {
+            p4DImage.setData((unsigned char*)relative1d, sz_relative[0], sz_relative[1], sz_relative[2], sz_relative[3], V3D_UINT16);
+        }
+        else if(datatype_relative == V3D_FLOAT32)
+        {
+            p4DImage.setData((unsigned char*)relative1d, sz_relative[0], sz_relative[1], sz_relative[2], sz_relative[3], V3D_FLOAT32);
+        }
+        else
+        {
+            printf("\nError: The program only supports UINT8, UINT16, and FLOAT32 datatype.\n");
+            return;
+        }
+        
+        v3dhandle newwin = callback.newImageWindow();
+        callback.setImage(newwin, &p4DImage);
+        callback.setImageName(newwin, tmpfile.toStdString().c_str());
+        callback.updateImageWindow(newwin);
+        
+    }
+    else if (menu_name == tr("About"))
+    {
+        QMessageBox::information(parent, "Version info", QString("Image IO using Bioformat Plugin %1 (Dec. 2, 2011) developed by Yang Yu and Hanchuan Peng. (Janelia Research Farm Campus, HHMI)").arg(getPluginVersion()).append("\n"));
+        return;
+    }
+}
+
+// plugin func
+QStringList IBioformatIOPlugin::funclist() const
+{
+    return QStringList() << tr("imageIOBioformat");
+}
+
+bool IBioformatIOPlugin::dofunc(const QString & func_name, const V3DPluginArgList & input, V3DPluginArgList & output, V3DPluginCallback2 & callback,  QWidget * parent)
+{
+    //
+}
+
+#endif
+
+
---
+++ hackathon/bioformatio_simple_bind/iBioformatIO.h
@@ -0,0 +1,35 @@
+/* iBioformatIO.h
+ * 2011-12-02: create this program by Yang Yu
+ */
+
+
+#ifndef __IBIOFORMATIO_H__
+#define __IBIOFORMATIO_H__
+
+#include <QtGui>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <v3d_interface.h>
+
+class IBioformatIOPlugin : public QObject, public V3DPluginInterface2_1
+{
+    Q_OBJECT
+    Q_INTERFACES(V3DPluginInterface2_1);
+	
+public:
+    float getPluginVersion() const {return 1.0f;} // version info 
+    
+	QStringList menulist() const;
+	void domenu(const QString &menu_name, V3DPluginCallback2 &callback, QWidget *parent);
+	
+	QStringList funclist() const;
+	bool dofunc(const QString & func_name, const V3DPluginArgList & input, V3DPluginArgList & output, V3DPluginCallback2 & v3d, QWidget * parent);
+	
+};
+
+#endif
+
+
+
