7d267d5a2aa4dfeaaa60ca49735337b498df0319
Hang Xiao <hxiao10@googlemail.com>
2012-03-23 16:19:06 +0000

    update fetch_mapview.cpp

--- hackathon/hang_mapview/src/fetch_mapview.cpp
+++ hackathon/hang_mapview/src/fetch_mapview.cpp
@@ -8,12 +8,12 @@ using namespace std;
 
 int main(int argc, char ** argv)
 {
-	int L = log(8)/log(2.0);
-	int M = log(8)/log(2.0);
-	int N = log(8)/log(2.0);
-	int l = log(256)/log(2.0);
-	int m = log(128)/log(2.0);
-	int n = log(64)/log(2.0);
+	int L = 14;//log(8)/log(2.0);
+	int M = 38;//log(8)/log(2.0);
+	int N = 3;//log(8)/log(2.0);
+	int l = 512;//log(256)/log(2.0);
+	int m = 256;//log(128)/log(2.0);
+	int n = 64;//log(64)/log(2.0);
 	string prefix = "test";
 	cout<<"prefix = "<<prefix<<endl;
 	cout<<"L = "<<L<<endl;
@@ -24,12 +24,12 @@ int main(int argc, char ** argv)
 	cout<<"n = "<<n<<endl;
 
 	ImageMapView mapview;
-	mapview.setPara(prefix, L+l, M+m, N+n, l, m, n);
+	mapview.setPara(prefix, L, M, N, l, m, n);
 
 	unsigned char * outimg1d = 0; 
-	long x0 = 10, y0 = 20, z0 = 65;
-	V3DLONG outsz[4] = {100, 280, 51, 1};
-	mapview.getImage(2, outimg1d, x0, y0, z0, outsz[0], outsz[1], outsz[2]);
+	long x0 = 0, y0 = 0, z0 = 0;
+	V3DLONG outsz[4] = {512, 256, 64, 1};
+	mapview.getImage(0, outimg1d, x0, y0, z0, outsz[0], outsz[1], outsz[2]);
 	if(outimg1d) saveImage("test_out.raw", outimg1d, outsz, V3D_UINT8);
     
     if (outimg1d) {delete []outimg1d; outimg1d=0;}
