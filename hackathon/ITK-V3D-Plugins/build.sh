93f70198cd88a14d56886ba46c4ab47d95d72064
Yuping Seu <yupingseu@gmail.com>
2012-08-08 01:29:19 +0000

    improve build.sh

--- hackathon/ITK-V3D-Plugins/build.sh
+++ hackathon/ITK-V3D-Plugins/build.sh
@@ -28,7 +28,7 @@ if [ $build_ITK = "y" -o $build_ITK = "Y" ]; then
 	mkdir $ITK_BUILD_DIR && cd $ITK_BUILD_DIR
 	ITK_DIR=`pwd`
 	echo $ITK_DIR
-	cmake -DBUILD_TESTING=OFF -DBUILD_EXAMPLES=OFF -DITK_USE_REVIEW=ON ../InsightToolkit-4.1.0
+	cmake -DBUILD_TESTING=OFF -DBUILD_EXAMPLES=OFF -DITK_USE_REVIEW=ON -DBUILD_SHARED_LIBS=ON ../$ITK_SOURCE_DIR
 	make -j4
 	cd ../
 else
@@ -42,24 +42,21 @@ else
 fi
 echo "======================================"
 echo " now build plugins"
-echo " set the vaa3d basic_c_fun source dir: "
+echo " Please set the vaa3d basic_c_fun source dir: "
 read V3D_BASIC_C_FUN_SOURCE_DIR
 echo 'you vaa3d basic_c_fun dir is :'$V3D_BASIC_C_FUN_SOURCE_DIR
 if [ ! -f $V3D_BASIC_C_FUN_SOURCE_DIR/v3d_interface.h ]; then
 	echo 'error basic_c_fun  dir please redo it'
 	exit 0
 fi
-echo 'set the vaa3d binary dir:'
+echo 'Please set the vaa3d binary dir:'
 read V3D_BINARY_DIR
 echo 'you binary dir is :'$V3D_BINARY_DIR
 if [ -d $PLUGINS_BUILD_DIR ]; then
 	cd $PLUGINS_BUILD_DIR
-	cmake -DV3D_BASIC_C_FUN_SOURCE_DIR=$V3D_BASIC_C_FUN_SOURCE_DIR -DV3D_BINARY_DIR=$V3D_BINARY_DIR -DITK_DIR=$ITK_DIR ../
-	make -j4
-	cd ../
 else
 	mkdir $PLUGINS_BUILD_DIR && cd $PLUGINS_BUILD_DIR
-	cmake -DV3D_BASIC_C_FUN_SOURCE_DIR=$V3D_BASIC_C_FUN_SOURCE_DIR -DV3D_BINARY_DIR=$V3D_BINARY_DIR -DITK_DIR=$ITK_DIR ../
-	make -j4
-	cd ../
 fi
+cmake -DV3D_BASIC_C_FUN_SOURCE_DIR=$V3D_BASIC_C_FUN_SOURCE_DIR -DV3D_BINARY_DIR=$V3D_BINARY_DIR -DITK_DIR=$ITK_DIR ../
+make -j4
+cd ../
