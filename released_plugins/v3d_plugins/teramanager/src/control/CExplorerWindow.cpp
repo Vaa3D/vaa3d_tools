d4d2d9a8482c5724ca79e6ad25c41a0e11c1875d
Alexandro Bria <alexandro86@gmail.com>
2013-04-30 23:15:17 +0000

    Minor bug fixes.

--- released_plugins/v3d_plugins/teramanager/src/control/CExplorerWindow.cpp
+++ released_plugins/v3d_plugins/teramanager/src/control/CExplorerWindow.cpp
@@ -911,21 +911,21 @@ XYZ CExplorerWindow::getRenderer3DPoint(int x, int y)  throw (MyException)
             titleShort.c_str(), x, y );
     #endif
 
-//    if(QMessageBox::Yes == QMessageBox::question(this, "Confirm", QString("Double-click zoom-in feature may cause Vaa3D to crash. Do you confirm?"), QMessageBox::No | QMessageBox::Yes, QMessageBox::Yes))
-//    {
-//        Renderer_gl1::MarkerPos pos;
-//        pos.x = x;
-//        pos.y = y;
-//        for (int i=0; i<4; i++)
-//                pos.view[i] = ((Renderer_gl1*)view3DWidget->getRenderer())->viewport[i];
-//        for (int i=0; i<16; i++)
-//        {
-//            pos.P[i]  = ((Renderer_gl1*)view3DWidget->getRenderer())->projectionMatrix[i];
-//            pos.MV[i] = ((Renderer_gl1*)view3DWidget->getRenderer())->markerViewMatrix[i];
-//        }
-//        return ((Renderer_gl1*)view3DWidget->getRenderer())->getCenterOfMarkerPos(pos);
-//    }
-//    else
+    if(QMessageBox::Yes == QMessageBox::question(this, "Confirm", QString("Double-click zoom-in feature may cause Vaa3D to crash. Do you confirm?"), QMessageBox::No | QMessageBox::Yes, QMessageBox::Yes))
+    {
+        Renderer_gl1::MarkerPos pos;
+        pos.x = x;
+        pos.y = y;
+        for (int i=0; i<4; i++)
+                pos.view[i] = ((Renderer_gl1*)view3DWidget->getRenderer())->viewport[i];
+        for (int i=0; i<16; i++)
+        {
+            pos.P[i]  = ((Renderer_gl1*)view3DWidget->getRenderer())->projectionMatrix[i];
+            pos.MV[i] = ((Renderer_gl1*)view3DWidget->getRenderer())->markerViewMatrix[i];
+        }
+        return ((Renderer_gl1*)view3DWidget->getRenderer())->getCenterOfMarkerPos(pos);
+    }
+    else
         throw MyException("Action cancelled by the user");
 
 //    throw MyException("Double click zoom-in has been temporarily disabled.");
