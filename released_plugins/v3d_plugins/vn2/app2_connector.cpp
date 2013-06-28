8d1b70d71e59e86a3c246a31ca4fc2c25e35c134
Hanchuan Peng <hanchuan.peng@gmail.com>
2013-06-28 23:57:24 +0000

    replace round() in app2_conneftor

--- released_plugins/v3d_plugins/vn2/app2_connector.cpp
+++ released_plugins/v3d_plugins/vn2/app2_connector.cpp
@@ -387,9 +387,9 @@ bool proc_app2(PARA_APP2 &p, const QString & versionStr)
     if(1)
     {
         QString rootposstr="", tmps;
-        tmps.setNum(round(inmarkers[0].x)).prepend("_x"); rootposstr += tmps;
-        tmps.setNum(round(inmarkers[0].y)).prepend("_y"); rootposstr += tmps;
-        tmps.setNum(round(inmarkers[0].z)).prepend("_z"); rootposstr += tmps;
+        tmps.setNum(int(inmarkers[0].x+0.5)).prepend("_x"); rootposstr += tmps;
+        tmps.setNum(int(inmarkers[0].y+0.5)).prepend("_y"); rootposstr += tmps;
+        tmps.setNum(int(inmarkers[0].z+0.5)).prepend("_z"); rootposstr += tmps;
         //QString outswc_file = callback.getImageName(curwin) + rootposstr + "_app2.swc";
         QString outswc_file = QString(p.p4dImage->getFileName()) + rootposstr + "_app2.swc";
               
