6e3503cd0a2400b3544d4e76b5b9c19fdef5f467
Zhi Zhou <alan.zhizhou@gmail.com>
2013-07-25 22:36:29 +0000

    add gsdt based enhancement

--- hackathon/zhi/3D_Selective_Enhancement/Adaptive_Thresholding_plugin.cpp
+++ hackathon/zhi/3D_Selective_Enhancement/Adaptive_Thresholding_plugin.cpp
@@ -14,6 +14,8 @@
 #include <vector>
 #include "stackutil.h"
 #include <Eigen/Dense>
+#include <boost/lexical_cast.hpp>
+
 
 /*#define WANT_STREAM       // include iostream and iomanipulators
 #include "/home/zhiz/work/v3d_external/v3d_main/jba/newmat11/newmatap.h"
@@ -22,13 +24,14 @@
 using namespace RBD_LIBRARIES;
 #endif
 */
-
 using namespace Eigen;
 using namespace std;
 Q_EXPORT_PLUGIN2(Adaptive_Thresholding, adpThresholding);
 
 void processImage(V3DPluginCallback2 &callback, QWidget *parent);
+void processImage2(V3DPluginCallback2 &callback, QWidget *parent);
 bool processImage(const V3DPluginArgList & input, V3DPluginArgList & output);
+
 
 template <class T> void AdpThresholding(T* data1d,
                      V3DLONG *in_sz,
@@ -40,12 +43,19 @@ template <class T> void AdpThresholding(T* data1d,
 		     unsigned int d,	
                      T* &outimg,
 		     T* gsdtdata1d);
+
+template <class T> void AdpThresholding_adpwindow(T* data1d,
+                    V3DLONG *in_sz,
+                    unsigned int c,
+                    T* &outimg,T* gsdtdata1d );
+
 int swapthree(float& dummya, float& dummyb, float& dummyc);
  
 QStringList adpThresholding::menulist() const
 {
 	return QStringList() 
-		<<tr("AdpTheresholding")
+        <<tr("Enhancement_fixedWindow")
+        <<tr("Enhancement_adpWindow")
 		<<tr("about");
 }
 
@@ -58,11 +68,15 @@ QStringList adpThresholding::funclist() const
 
 void adpThresholding::domenu(const QString &menu_name, V3DPluginCallback2 &callback, QWidget *parent)
 {
-	if (menu_name == tr("AdpTheresholding"))
+    if (menu_name == tr("Enhancement_fixedWindow"))
 	{
 		processImage(callback,parent);
 	}
-	else
+    else if(menu_name == tr("Enhancement_adpWindow"))
+    {
+       processImage2(callback,parent);
+
+    }else
 	{
         (menu_name == tr("help"));
         v3d_msg(tr("Detect fiber. "
@@ -189,8 +203,6 @@ void processImage(V3DPluginCallback2 &callback, QWidget *parent)
         case V3D_FLOAT32: AdpThresholding((float *)data1d, in_sz, Wx, Wy, Wz,c, p,d,(float* &)outimg,(float *)gsdtdata1d);break;
         default: v3d_msg("Invalid data type. Do nothing."); return;
     }
-	
-   
     
      // display
      Image4DSimple * new4DImage = new Image4DSimple();
@@ -351,6 +363,267 @@ template <class T> void AdpThresholding(T* data1d,
 		
 }
 
+void processImage2(V3DPluginCallback2 &callback, QWidget *parent)
+{
+    v3dhandle curwin = callback.currentImageWindow();
+    if (!curwin)
+    {
+        QMessageBox::information(0, "", "You don't have any image open in the main window.");
+        return;
+    }
+
+    Image4DSimple* p4DImage = callback.getImage(curwin);
+
+    if (!p4DImage)
+    {
+        QMessageBox::information(0, "", "The image pointer is invalid. Ensure your data is valid and try again!");
+        return;
+    }
+
+    unsigned char* data1d = p4DImage->getRawData();
+    //V3DLONG totalpxls = p4DImage->getTotalBytes();
+    V3DLONG pagesz = p4DImage->getTotalUnitNumberPerChannel();
+
+    V3DLONG N = p4DImage->getXDim();
+    V3DLONG M = p4DImage->getYDim();
+    V3DLONG P = p4DImage->getZDim();
+    V3DLONG sc = p4DImage->getCDim();
+
+    V3DLONG in_sz[4];
+    in_sz[0] = N; in_sz[1] = M; in_sz[2] = P; in_sz[3] = sc;
+
+    ImagePixelType pixeltype = p4DImage->getDatatype();
+    /*int c = 1;
+    unsigned char * gsdtdata1d = 0;
+    int datatype;
+    V3DLONG * in_zz = 0;
+
+    loadImage("/data/mat/zhi/gsdtImage.tif", gsdtdata1d, in_zz, datatype,0);*/
+     //add input dialog
+
+    AdaptiveWindowDialog dialog(callback, parent);
+    if (!dialog.image)
+        return;
+
+    if (dialog.exec()!=QDialog::Accepted)
+        return;
+
+    dialog.update();
+
+    Image4DSimple* subject = dialog.image;
+    if (!subject)
+        return;
+    ROIList pRoiList = dialog.pRoiList;
+
+    int c = dialog.ch+1;
+    int th_idx = dialog.th_idx;
+    double th = dialog.thresh;
+
+    if(th_idx ==0)
+    {
+        V3DLONG offsetc = (c-1)*pagesz;
+        for(V3DLONG iz = 0; iz < P; iz++)
+        {
+            double PixelSum = 0;
+            V3DLONG offsetk = iz*M*N;
+            for(V3DLONG iy = 0; iy < M; iy++)
+            {
+                V3DLONG offsetj = iy*N;
+                for(V3DLONG ix = 0; ix < N; ix++)
+                {
+
+                double PixelVaule = data1d[offsetc + offsetk + offsetj + ix];
+               // filter
+                PixelSum = PixelSum + PixelVaule;
+
+                }
+            }
+            th = th + PixelSum/(M*N*P);
+        }
+
+    }
+
+    // filter
+
+    saveImage("temp.tif", (unsigned char *)data1d, in_sz, pixeltype);
+    //invoke gsdt function
+    V3DPluginArgItem arg;
+    V3DPluginArgList input;
+    V3DPluginArgList output;
+
+
+    arg.type = "random";std::vector<char*> args1;
+   // std:: string inputName(callback.getImageName(curwin).toStdString());char* inputName2 =  new char[inputName.length() + 1]; strcpy(inputName2, inputName.c_str());
+    args1.push_back("temp.tif"); arg.p = (void *) & args1; input<< arg;
+    arg.type = "random";std::vector<char*> args;
+    char channel = '0' + (c-1);
+    string threshold = boost::lexical_cast<string>(th); char* threshold2 =  new char[threshold.length() + 1]; strcpy(threshold2, threshold.c_str());
+    args.push_back(threshold2);args.push_back("1");args.push_back(&channel);args.push_back("1"); arg.p = (void *) & args; input << arg;
+    arg.type = "random";std::vector<char*> args2;args2.push_back("gsdtImage.tif"); arg.p = (void *) & args2; output<< arg;
+
+    QString full_plugin_name = "gsdt";
+    QString func_name = "gsdt";
+
+    callback.callPluginFunc(full_plugin_name,func_name, input,output);
+
+    unsigned char * gsdtdata1d = 0;
+    int datatype;
+    V3DLONG * in_zz = 0;
+
+    char * outimg_file = ((vector<char*> *)(output.at(0).p))->at(0);
+    loadImage(outimg_file, gsdtdata1d, in_zz, datatype,0);
+    //remove("gsdtImage.tif");
+    remove("temp.tif");
+
+
+
+
+    void* outimg = 0;
+    switch (pixeltype)
+    {
+    case V3D_UINT8: AdpThresholding_adpwindow(data1d, in_sz, c,(unsigned char* &)outimg, gsdtdata1d); break;
+    case V3D_UINT16: AdpThresholding_adpwindow((unsigned short int *)data1d, in_sz, c, (unsigned short int* &)outimg,(unsigned short int *)gsdtdata1d); break;
+    case V3D_FLOAT32: AdpThresholding_adpwindow((float *)data1d, in_sz, c, (float* &)outimg,(float *)gsdtdata1d);break;
+    default: v3d_msg("Invalid data type. Do nothing."); return;
+    }
+
+    // display
+    Image4DSimple * new4DImage = new Image4DSimple();
+    new4DImage->setData((unsigned char *)outimg, N, M, P, 1, pixeltype);
+    v3dhandle newwin = callback.newImageWindow();
+    callback.setImage(newwin, new4DImage);
+    callback.setImageName(newwin, "3D adaptive enhancement result");
+    callback.updateImageWindow(newwin);
+
+    return;
+}
+
+template <class T> void AdpThresholding_adpwindow(T* data1d,
+                                                  V3DLONG *in_sz,
+                                                  unsigned int c,
+                                                  T* &outimg,
+                                                  T* gsdtdatald)
+{
+
+
+         V3DLONG N = in_sz[0];
+         V3DLONG M = in_sz[1];
+         V3DLONG P = in_sz[2];
+         V3DLONG sc = in_sz[3];
+         V3DLONG pagesz = N*M*P;
+         int Wx,Wy,Wz;
+         V3DLONG offsetc = (c-1)*pagesz;
+         int Th_gsdt = 200;
+         T *pImage = new T [pagesz];
+        if (!pImage)
+        {
+            printf("Fail to allocate memory.\n");
+            return;
+         }
+         else
+         {
+            for(V3DLONG i=0; i<pagesz; i++)
+                pImage[i] = 0;
+          }
+           T maxfl = 0;
+         //outimg = pImage;
+
+        for(V3DLONG iz = 0; iz < P; iz++)
+        {
+            V3DLONG offsetk = iz*M*N;
+            for(V3DLONG iy = 0; iy < M; iy++)
+            {
+                V3DLONG offsetj = iy*N;
+                for(V3DLONG ix = 0; ix < N; ix++)
+                {
+
+                    T GsdtValue = gsdtdatald[offsetk + offsetj + ix];
+                    T PixelValue = data1d[offsetc+offsetk + offsetj + ix];
+                    Wx = (int)round((log(GsdtValue)/log(2)));
+                    //printf("%d %d\n",PixelValue,Wx);
+
+                    if (Wx > 0 && PixelValue > 0 && PixelValue < Th_gsdt)
+                    {
+                        Wy = Wx;
+                        Wz = Wx*2;
+
+                        V3DLONG xb = ix-Wx; if(xb<0) xb = 0;
+                        V3DLONG xe = ix+Wx; if(xe>=N-1) xe = N-1;
+                        V3DLONG yb = iy-Wy; if(yb<0) yb = 0;
+                        V3DLONG ye = iy+Wy; if(ye>=M-1) ye = M-1;
+                        V3DLONG zb = iz-Wz; if(zb<0) zb = 0;
+                        V3DLONG ze = iz+Wz; if(ze>=P-1) ze = P-1;
+
+                       // printf("window size is %d\n",Wx);
+                        //Seletive approach
+                        float  fxx = data1d[offsetk+offsetj+xe]+ data1d[offsetk+offsetj+xb]- 2*data1d[offsetk+offsetj+ix];
+                        float fyy = data1d[offsetk+(ye)*N+ix]+data1d[offsetk+(yb)*N+ix]-2*data1d[offsetk+offsetj+ix];
+                        float fzz = data1d[(ze)*M*N+offsetj+ix]+data1d[(zb)*M*N+offsetj+ix]- 2*data1d[offsetk+offsetj+ix];
+
+                        float fxy = 0.25*(data1d[offsetk+(ye)*N+xe]+data1d[offsetk+(yb)*N+xb]-data1d[offsetk+(ye)*N+xb]-data1d[offsetk+(yb)*N+xe]);
+                        float fxz = 0.25*(data1d[(ze)*M*N+offsetj+xe]+data1d[(zb)*M*N+offsetj+xb]-data1d[(ze)*M*N+offsetj+xb]-data1d[(zb)*M*N+offsetj+xe]);
+                        float fyz = 0.25*(data1d[(ze)*M*N+(ye)*N+ix]+data1d[(zb)*M*N+(yb)*N+ix]-data1d[(ze)*M*N+(yb)*N+ix]-data1d[(zb)*M*N+(ye)*N+ix]);
+
+                        Matrix3f A;
+                        A << fxx,fxy,fxz,fxy,fyy,fyz,fxz,fyz,fzz;
+                        SelfAdjointEigenSolver<Matrix3f> eigensolver(A, false);
+
+                        float a1 = eigensolver.eigenvalues()(0);
+                        float a2 = eigensolver.eigenvalues()(1);
+                        float a3 = eigensolver.eigenvalues()(2);
+                        swapthree(a1, a2, a3);
+                        float output1 = 0;
+                        float output2 = 0;
+                        float output3 = 0;
+                        if(a1<0 && a2 < 0)
+                        {
+                              T dataval =  abs(a2)*(abs(a2)-abs(a3))/abs(a1);
+                              pImage[offsetk+offsetj+ix] = dataval;
+                              if(maxfl<dataval) maxfl = dataval;
+                        }
+
+
+                    }else
+                      pImage[offsetk+offsetj+ix] = 0;
+                }
+            }
+        }
+
+       T *pImage2 = new T [pagesz];
+       if (!pImage2)
+       {
+           printf("Fail to allocate memory.\n");
+           return;
+        }
+        else
+        {
+           for(V3DLONG i=0; i<pagesz; i++)
+               pImage2[i] = 0;
+         }
+       for(V3DLONG iz = 0; iz < P; iz++)
+        {
+            V3DLONG offsetk = iz*M*N;
+            for(V3DLONG iy = 0; iy < M; iy++)
+            {
+                V3DLONG offsetj = iy*N;
+                for(V3DLONG ix = 0; ix < N; ix++)
+                {
+                         T PixelValue = data1d[offsetc+offsetk + offsetj + ix];
+                        if(PixelValue < Th_gsdt)
+                        {
+                            T dataval2 = 255*pImage[offsetk+offsetj+ix]/maxfl;
+                            pImage2[offsetk+offsetj+ix] = dataval2;
+                        }else
+                            pImage2[offsetk+offsetj+ix] =  data1d[offsetk+offsetj+ix];
+                }
+
+
+            }
+
+        }
+        outimg = pImage2;
+
+}
 int swapthree(float& dummya, float& dummyb, float& dummyc)
 {
  
--- hackathon/zhi/3D_Selective_Enhancement/Adaptive_Thresholding_plugin.h
+++ hackathon/zhi/3D_Selective_Enhancement/Adaptive_Thresholding_plugin.h
@@ -24,5 +24,151 @@ public:
 	bool dofunc(const QString &func_name, const V3DPluginArgList &input, V3DPluginArgList &output, V3DPluginCallback2 &callback, QWidget *parent);
 };
 
+//define a simple dialog for choose Adaptive Median parameters
+class AdaptiveWindowDialog : public QDialog
+    {
+        Q_OBJECT
+
+    public:
+        AdaptiveWindowDialog(V3DPluginCallback2 &cb, QWidget *parent)
+        {
+            //initialization of variables
+            image = 0;
+            gridLayout = 0;
+
+            //
+
+            v3dhandleList win_list = cb.getImageWindowList();
+
+            if(win_list.size()<1)
+            {
+                QMessageBox::information(0, "Adaptive Median", QObject::tr("No image is open."));
+                return;
+            }
+
+            //create a dialog
+            gridLayout = new QGridLayout();
+
+            image = cb.getImage(cb.currentImageWindow());
+
+            if (!image || !image->valid())
+            {
+                v3d_msg("The image is not valid yet. Check your data.");
+                return;
+            }
+
+            pRoiList=cb.getROI(cb.currentImageWindow());
+            QString imageName = cb.getImageName(cb.currentImageWindow());
+            label_imagename1 = new QLabel(imageName.prepend("You have selected the image [").append("]"));
+            gridLayout->addWidget(label_imagename1, 1,0,1,3);
+
+            label_imagename2 = new QLabel(QObject::tr("This feature will use the Grayscale Distance Tranform (GSDT) plugin to determine the local window size adaptively."));
+            gridLayout->addWidget(label_imagename2, 2,0,1,3);
+
+            label_imagename3 = new QLabel(QObject::tr("Thus please specify the following information to run GSDT."));
+            gridLayout->addWidget(label_imagename3, 3,0,1,3);
+
+            int c = image->getCDim();
+                 label_channel = new QLabel(QObject::tr("Choose a channel: "));
+            gridLayout->addWidget(label_channel, 4,0);
+
+            // channel info
+            QStringList chList;
+            if (c==1)
+                chList << "red (ch 1)";
+            else if(c==2)
+                chList << "red (ch 1)" << "green (ch 2)";
+            else if(c>=3)
+                chList << "red (ch 1)" << "green (ch 2)" << "blue (ch 3)";
+            for (int i=3;i<c;i++)
+                chList << QString("ch ").setNum(i+1);
+            combo_channel = new QComboBox(); combo_channel->addItems(chList);
+            gridLayout->addWidget(combo_channel, 4,1);
+
+            //
+            label_th = new QLabel(QObject::tr("Choose a background threshold (0~255): "));
+            gridLayout->addWidget(label_th, 6,0);
+
+            QStringList thList;
+            thList << "mean" << "usr defined";
+
+            combo_th = new QComboBox(); combo_th->addItems(thList);
+            gridLayout->addWidget(combo_th, 6,1);
+
+            //
+            spin_th = new QDoubleSpinBox();
+            gridLayout->addWidget(spin_th, 6,2);
+
+            spin_th->setEnabled(false);
+            spin_th->setMaximum(255); spin_th->setMinimum(0);
+            //
+            ok     = new QPushButton("OK");
+            cancel = new QPushButton("Cancel");
+            gridLayout->addWidget(cancel, 7,1);
+            gridLayout->addWidget(ok,     7,2);
+
+            setLayout(gridLayout);
+            setWindowTitle(QString("Adaptive Median"));
+
+            //slot interface
+            connect(ok,     SIGNAL(clicked()), this, SLOT(accept()));
+            connect(cancel, SIGNAL(clicked()), this, SLOT(reject()));
+
+            connect(combo_channel, SIGNAL(currentIndexChanged(int)), this, SLOT(update()));
+            connect(combo_th, SIGNAL(currentIndexChanged(int)), this, SLOT(update()));
+
+            connect(spin_th, SIGNAL(valueChanged(double)), this, SLOT(update()));
+            update(); //update at least once so to initialize the variables. by Hanchuan Peng, 2011-02-11
+        }
+
+        ~AdaptiveWindowDialog(){}
+
+        public slots:
+        void update()
+        {
+            ch = combo_channel->currentIndex();
+
+            th_idx = combo_th->currentIndex();
+
+            if(th_idx == 1)
+            {
+                spin_th->setEnabled(true);
+            }
+            else
+            {
+                spin_th->setEnabled(false);
+            }
+
+            thresh = spin_th->text().toDouble();
+
+        }
+
+    public:
+        int ch, th_idx;
+        int volsz;
+        double thresh;
+        Image4DSimple* image;
+        ROIList pRoiList;
+        bool b_filtersmallobjs;
+
+        QGridLayout *gridLayout;
+
+        QLabel* label_imagename1;
+        QLabel* label_imagename2;
+            QLabel* label_imagename3;
+        QLabel* label_channel;
+        QComboBox* combo_channel;
+
+        QLabel* label_th;
+        QComboBox* combo_th;
+
+        QDoubleSpinBox* spin_th;
+
+        QPushButton* ok;
+        QPushButton* cancel;
+    };
+
+
+
 #endif
 
