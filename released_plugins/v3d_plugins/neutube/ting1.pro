8ff055c755b2670f705eccc43ec1719627615acd
Hanchuan Peng <hanchuan.peng@gmail.com>
2014-01-16 19:18:03 +0000

    now recover the previous buildable neutube simple interface which relies on system call to invoke neutube

--- released_plugins/v3d_plugins/neutube/ting1.pro
+++ released_plugins/v3d_plugins/neutube/ting1.pro
@@ -1,28 +1,13 @@
 
 TEMPLATE	= lib
 CONFIG	+= qt plugin warn_off
-QT += opengl xml
 #CONFIG	+= x86_64
-VAA3DPATH = ../../../v3d_main
-INCLUDEPATH += $$VAA3DPATH/v3d $$VAA3DPATH/basic_c_fun $$VAA3DPATH/common_lib/include   src_neutube/neurolabi/c \
-    src_neutube/neurolabi/c/include src_neutube/neurolabi/gui \
-    src_neutube/neurolabi/lib/genelib/src src_neutube/neurolabi/build \
-    src_neutube/neurolabi/gui/ext src_neutube/neurolabi/gui/ext/QsLog
+VAA3DPATH = ../../../../v3d_external
+INCLUDEPATH	+= $$VAA3DPATH/v3d_main/basic_c_fun
 
-INCLUDEPATH += src_neutube/neurolabi/lib/libpng-1.6.7 src_neutube/neurolabi/lib/jansson/include
-
-
-LIBS += -L$${PWD}/src_neutube/neurolabi/build -lneutube -L$${PWD}/src_neutube/neurolabi/c/lib -lneurolabi \
-    -lxml2 -ljansson -lGLEW -framework AGL -framework OpenGL -lfftw3 -lfftw3f -ldl \
-    -framework AppKit -framework IOKit     -framework OpenGL \
-    -framework ApplicationServices -ldl -framework AppKit -framework IOKit \
-    -framework OpenGL -framework ApplicationServices -framework CoreFoundation
-
-HEADERS	+= ting1_plugin.h \
-    nvinterface.h
-SOURCES	+= ting1_plugin.cpp \
-    nvinterface.cpp
-SOURCES	+= $$VAA3DPATH/basic_c_fun/v3d_message.cpp
+HEADERS	+= ting1_plugin.h
+SOURCES	+= ting1_plugin.cpp
+SOURCES	+= $$VAA3DPATH/v3d_main/basic_c_fun/v3d_message.cpp
 
 TARGET	= $$qtLibraryTarget(neutube)
-DESTDIR	= ../../v3d/plugins/neuron_tracing/tracing_tubes
+DESTDIR	= $$VAA3DPATH/bin/plugins/neuron_tracing/neutube
--- released_plugins/v3d_plugins/neutube/ting1_plugin.cpp
+++ released_plugins/v3d_plugins/neutube/ting1_plugin.cpp
@@ -6,39 +6,14 @@
 #include "v3d_message.h"
 #include <vector>
 #include "ting1_plugin.h"
-#include "zstack.hxx"
-#include "nvinterface.h"
-#include "zstackframe.h"
-
-
 using namespace std;
-Q_EXPORT_PLUGIN2(neutube, neutube);
-
-
-neutube::neutube() : m_3dApp(QCoreApplication::applicationDirPath())
-{
-    m_mainWindow = new MainWindow();
-    std::cout << "Initializing 3D ..." << std::endl;
-    m_3dApp.initialize();
-
-    m_mainWindow->config();
-    m_mainWindow->initOpenglContext();
-}
-
-neutube::~neutube()
-{
-    m_3dApp.deinitializeGL();
-    m_3dApp.deinitialize();
-    delete m_mainWindow;
-}
-
-
+Q_EXPORT_PLUGIN2(ting1, neutube);
+ 
 QStringList neutube::menulist() const
 {
 	return QStringList() 
 		<<tr("neutube")
 		<<tr("about");
-
 }
 
 QStringList neutube::funclist() const
@@ -52,43 +27,19 @@ void neutube::domenu(const QString &menu_name, V3DPluginCallback2 &callback, QWi
 {
 	if (menu_name == tr("neutube"))
 	{
-        v3d_msg(tr("Show window"));
+//        system()
 
-        m_mainWindow->show();
-
-        v3dhandle curwin = callback.currentImageWindow();
-        if(!curwin)
-        {
-            return;
-        }
-        Image4DSimple *p4DImage = callback.getImage(curwin);
-
-        if (!p4DImage) {
-            return;
-        }
-
-        Mc_Stack *stack = NVInterface::makeStack(p4DImage);
-
-        ZStackFrame *newFrame = new ZStackFrame;
-        ZStack *stackObject = new ZStack(stack);
-        newFrame->loadStack(stackObject);
-        m_mainWindow->addStackFrame(newFrame);
-        m_mainWindow->presentStackFrame(newFrame);
-        m_mainWindow->enableStackActions(true);
-        m_mainWindow->updateAction();
-        m_mainWindow->updateMenu();
+     //   neurotube_main()
 	}
 	else
 	{
-        v3d_msg(tr("neuTube plugin v0.2. a collaboration with Hanchuan. "
-            "Developed by Ting Zhao and HP, 2013-12-9"));
+		v3d_msg(tr("a collaboration with Hanchuan. "
+			"Developed by Ting Zhao and HP, 2012-12-9"));
 	}
 }
 
-
 bool neutube::dofunc(const QString & func_name, const V3DPluginArgList & input, V3DPluginArgList & output, V3DPluginCallback2 & callback,  QWidget * parent)
 {
-
 	vector<char*> infiles, inparas, outfiles;
 	if(input.size() >= 1) infiles = *((vector<char*> *)input.at(0).p);
 	if(input.size() >= 2) inparas = *((vector<char*> *)input.at(1).p);
