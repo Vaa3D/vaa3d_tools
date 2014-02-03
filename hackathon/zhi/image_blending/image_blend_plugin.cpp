e966687e8ec3eee1bc16a5b1c2f7c379efaa5110
Zhi Zhou <alan.zhizhou@gmail.com>
2014-02-03 17:47:49 +0000

    revised the command line for input images

--- hackathon/zhi/image_blending/image_blend_plugin.cpp
+++ hackathon/zhi/image_blending/image_blend_plugin.cpp
@@ -38,21 +38,139 @@ bool image_blend::dofunc(const QString & func_name, const V3DPluginArgList & inp
 {
 	if (func_name == tr("image_blend"))
 	{
-        if (output.size() != 1) return false;
+      /*  if (output.size() != 1) return false;
         if (input.size() != 2) return false;
 
         char * inimg_file1 = ((vector<char*> *)(input.at(0).p))->at(0);
         char * inimg_file2 = ((vector<char*> *)(input.at(1).p))->at(0);
 
+        char * output_file = ((vector<char*> *)(output.at(0).p))->at(0);*/
+
+        if(input.size()<1 || (input.size()==1 && output.size()<1) ) // no inputs
+        {
+            return true;
+        }
+
+        vector<char*> * infilelist;
+        vector<char*> * paralist;
+        vector<char*> * outfilelist;
+
+        char * infile = NULL; //input_image_file
+        char * paras = NULL; // parameters
+        char * outfile = NULL; // output_image_file
+
+        if(input.size()>0) {infilelist = (vector<char*> *)(input.at(0).p);}
+        if(output.size()>0) { outfilelist = (vector<char*> *)(output.at(0).p);}  // specify output
+        if(input.size()>1) { paralist = (vector<char*> *)(input.at(1).p); paras =  paralist->at(0);} // parameters
+        if(!infilelist->empty()) { infile = infilelist->at(0); }
+        if(!outfilelist->empty()) { outfile = outfilelist->at(0); }
+
         char * output_file = ((vector<char*> *)(output.at(0).p))->at(0);
 
+        QString inimg_file1,inimg_file2;
+        if(paras)
+        {
+            int argc = 0;
+            int len = strlen(paras);
+            int posb[2048];
+
+            for(int i = 0; i < len; i++)
+            {
+                if(i==0 && paras[i] != ' ' && paras[i] != '\t')
+                {
+                    posb[argc++] = i;
+                }
+                else if((paras[i-1] == ' ' || paras[i-1] == '\t') && (paras[i] != ' ' && paras[i] != '\t'))
+                {
+                    posb[argc++] = i;
+                }
+            }
+
+            char **argv = NULL;
+            try
+            {
+                argv =  new char* [argc];
+                for(int i = 0; i < argc; i++)
+                {
+                    argv[i] = paras + posb[i];
+                }
+            }
+            catch(...)
+            {
+                printf("\nError: fail to allocate memory!\n");
+                return false;
+            }
+
+            for(int i = 0; i < len; i++)
+            {
+                if(paras[i]==' ' || paras[i]=='\t')
+                    paras[i]='\0';
+            }
+
+            char* key;
+            for(int i=0; i<argc; i++)
+            {
+                //qDebug()<<"argv"<<i<<"["<<argv[i]<<"]";
+                if(i+1 != argc) // check that we haven't finished parsing yet
+                {
+                    key = argv[i];
+
+                    qDebug()<<">>key ..."<<key;
+
+                    if (*key == '^')
+                    {
+                        while(*++key)
+                        {
+                            if (!strcmp(key, "t"))
+                            {
+                                inimg_file1 = QString( argv[i+1] );
+                                qDebug()<<"filename_img_1=["<<inimg_file1<<"]";
+                                i++;
+                            }
+                            else if (!strcmp(key, "ct"))
+                            {
+                                key++;
+                                //channel_ref_tar = atoi( argv[i+1] ) - 1; // red 1 green 2 blue 3
+                                i++;
+                            }
+                            else if (!strcmp(key, "s"))
+                            {
+                                inimg_file2 = QString( argv[i+1] );
+                                qDebug()<<"filename_img_2=["<<inimg_file2<<"]";
+                                i++;
+                            }
+                            else if (!strcmp(key, "cs"))
+                            {
+                                key++;
+                              //  channel_ref_sub = atoi( argv[i+1] ) - 1; // red 1 green 2 blue 3
+                                i++;
+                            }
+                            else
+                            {
+                                cout<<"parsing ..."<<key<<i<<"Unknown command. Type 'v3d -x plugin_name -f function_name' for usage"<<endl;
+                                return false;
+                            }
+
+                        }
+                    }
+                    else
+                    {
+                        cout<<"parsing ..."<<key<<i<<"Unknown command. Type 'v3d -x plugin_name -f function_name' for usage"<<endl;
+                        return false;
+                    }
+                }
+            }
+
+
+          if (argv) {delete []argv; argv=0;}
+        }
 
         unsigned char * image1 = 0;
         V3DLONG in_sz1[4];
         int datatype1;
-        if(!simple_loadimage_wrapper(callback, inimg_file1, image1, in_sz1, datatype1))
+        if(!simple_loadimage_wrapper(callback, inimg_file1.toStdString().c_str(), image1, in_sz1, datatype1))
         {
-            cerr<<"load image1 "<<inimg_file1<<" error!"<<endl;
+            cerr<<"load image1 "<<inimg_file1.toStdString()<<" error!"<<endl;
             if (image1) {delete []image1; image1=0;}
             return false;
         }
@@ -61,9 +179,9 @@ bool image_blend::dofunc(const QString & func_name, const V3DPluginArgList & inp
         unsigned char * image2 = 0;
         V3DLONG in_sz2[4];
         int datatype2;
-        if(!simple_loadimage_wrapper(callback, inimg_file2, image2, in_sz2, datatype2))
+        if(!simple_loadimage_wrapper(callback, inimg_file2.toStdString().c_str(), image2, in_sz2, datatype2))
         {
-            cerr<<"load image2 "<<inimg_file2<<" error!"<<endl;
+            cerr<<"load image2 "<<inimg_file2.toStdString()<<" error!"<<endl;
              if (image2) {delete []image2; image2=0;}
             return false;
         }
