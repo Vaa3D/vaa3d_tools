e267631d1d6a457a5113a5bbc2660160fd0f09b7
Brian Long <brianl@alleninstitute.org>
2014-02-11 17:35:36 +0000

    added forgotten header file and made .pro file more general

--- hackathon/brl/index2binary/index2binary.pro
+++ hackathon/brl/index2binary/index2binary.pro
@@ -2,7 +2,7 @@
 TEMPLATE	= lib
 CONFIG	+= qt plugin warn_off
 #CONFIG	+= x86_64
-VAA3DPATH = /Users/brl/vaa3d_related/v3d_external
+VAA3DPATH = ../../../../v3d_external
 INCLUDEPATH	+= $$VAA3DPATH/v3d_main/basic_c_fun
 
 HEADERS	+= index2binary_plugin.h
---
+++ hackathon/brl/index2binary/index2binary_plugin.h
@@ -0,0 +1,27 @@
+/* INDEX2BINARY_plugin.h
+ *
+ */
+ 
+#ifndef __INDEX2BINARY_PLUGIN_H__
+#define __INDEX2BINARY_PLUGIN_H__
+
+#include <QtGui>
+#include <v3d_interface.h>
+
+class Index2Binary : public QObject, public V3DPluginInterface2_1
+{
+	Q_OBJECT
+	Q_INTERFACES(V3DPluginInterface2_1);
+
+public:
+	float getPluginVersion() const {return 1.1f;}
+
+	QStringList menulist() const;
+	void domenu(const QString &menu_name, V3DPluginCallback2 &callback, QWidget *parent);
+
+	QStringList funclist() const ;
+	bool dofunc(const QString &func_name, const V3DPluginArgList &input, V3DPluginArgList &output, V3DPluginCallback2 &callback, QWidget *parent);
+};
+
+#endif
+
