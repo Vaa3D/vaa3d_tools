fa59a2f2b2db0c29a64d9eb238c04414e3478012
xinnamin <xinnamin@gmail.com>
2014-07-15 00:19:51 +0000

    neuron identification works, output is saved to file in same directory as vaa3d exe. pixel function algorithm needs to be adjusted, see comments for count_cells.

--- hackathon/xindi/auto_identify/auto_identify_plugin.cpp
+++ hackathon/xindi/auto_identify/auto_identify_plugin.cpp
@@ -5,11 +5,13 @@
  
 #include "v3d_message.h"
 #include <vector>
+#include <math.h>
 #include "auto_identify_plugin.h"
 #include "string"
 #include "sstream"
 #include "../../v3d_main/neuron_editing/v_neuronswc.h"
 #include "../../v3d_main/neuron_tracing/neuron_tracing.h"
+#include <time.h>
 
 using namespace std;
 Q_EXPORT_PLUGIN2(auto_identify, AutoIdentifyPlugin);
@@ -43,9 +45,9 @@ template <class T> LandmarkList duplicates(T* data1d, LandmarkList fullList,
                                            V3DLONG *dimNum, int PointAve, int rad, int c);
 V_NeuronSWC get_v_neuron_swc(const NeuronTree *p);
 V_NeuronSWC_list get_neuron_segments(const NeuronTree *p);
-NeuronTree VSWClist_2_neuron_tree(V_NeuronSWC_list *p);
-NeuronTree VSWC_2_neuron_tree(V_NeuronSWC *p);
-NeuronSWC make_neuron_swc(V_NeuronSWC_unit *p);
+//NeuronTree VSWClist_2_neuron_tree(V_NeuronSWC_list *p);
+NeuronTree VSWC_2_neuron_tree(V_NeuronSWC *p, int id);
+NeuronSWC make_neuron_swc(V_NeuronSWC_unit *p, int id, bool &start);
 template <class T> double compute_radius(T* data1d, V3DLONG *dimNum, vector<V_NeuronSWC_unit> segment, int c);
 bool export_list2file(QList<NeuronTree> & N2, QString fileSaveName, QString fileOpenName);
 
@@ -119,14 +121,17 @@ bool AutoIdentifyPlugin::dofunc(const QString & func_name, const V3DPluginArgLis
  * [completed tasks]
  * test neuron SWC needs to be its own structure marked with the comment "test"
  * radius calculation of branches works
- * categorizing non-test data sets based on test data radius works
+ * averate intensity of branches works
+ * categorizing non-test data sets based on test data works
+ * saving newly labeled SWC file works
  *
  * [current goals/issues]
  * updating window to reflect type changes does not work yet
+ * add in consistency of intensity as potential type identifiers
+ *      because the radius calculation is currently skipping dark spots, darker neurons may be classified as brighter than they should be
  *
  * [future goals]
- * add in parent node info / branching as potential type identifiers beyond just radius
- * find way to identify individal segments rather than entire structures as test data
+ * find way to identify individal segments rather than entire structures as test data. NOT SURE IF EVEN POSSIBLE
  * optimize radius calculation algorithm to be more robust
  *  ##################################
 */
@@ -145,6 +150,8 @@ void identify_neurons(V3DPluginCallback2 &callback, QWidget *parent)
     Image4DSimple* p4DImage = callback.getImage(curwin); //the data of the image is in 4D (channel + 3D)
 
     unsigned char* data1d = p4DImage->getRawData(); //sets data into 1D array
+
+    QString curfile = callback.getImageName(curwin);
 
     //defining the dimensions
     V3DLONG N = p4DImage->getXDim();
@@ -172,6 +179,7 @@ void identify_neurons(V3DPluginCallback2 &callback, QWidget *parent)
     {
         vector<int> segCatArr;
         vector<double> segRadArr;
+        vector<double> segIntensArr;
         int structNum = mTreeList->count();
 
         //get radius and type from test data
@@ -189,9 +197,19 @@ void identify_neurons(V3DPluginCallback2 &callback, QWidget *parent)
             {
                 segCatArr.push_back(seg_list.at(j).row.at(0).type);
                 double radAve = compute_radius(data1d,dimNum,seg_list.at(j).row,c);
+                double x,y,z,intensity=0;
+                for (int k=0; k<seg_list.at(j).row.size(); k++)
+                {
+                    x=seg_list.at(j).row.at(k).x;
+                    y=seg_list.at(j).row.at(k).y;
+                    z=seg_list.at(j).row.at(k).z;
+                    intensity += pixelVal(data1d,dimNum,x,y,z,c);
+                }
+                double intensAve = intensity/seg_list.at(j).row.size();
                 segRadArr.push_back(radAve);
+                segIntensArr.push_back(intensAve);
                 //v3d_msg(QString("cat %1 rad %2").arg(seg_list.at(j).row.at(0).type).arg(radAve));
-            } //compute_radius fails and stalls program to a standstill if any of the SWCs aren't traced well
+            }
         }
         //v3d_msg(QString("%1 %2 %3 %4").arg(segCatArr.size()).arg(segCatArr.at(0)).arg(segRadArr.size()).arg(segRadArr.at(0)));
 
@@ -207,16 +225,28 @@ void identify_neurons(V3DPluginCallback2 &callback, QWidget *parent)
             V_NeuronSWC_list seg_list = get_neuron_segments(&mTree);
             //syntax: list.at(i) is segment, segment.row is vector of units, vector.at(i) is unit, unit.type is category
             int segNum = seg_list.size();
+            int id=1;
             //v3d_msg(QString("read in segment of length %1").arg(segNum));
             for (int j=0; j<segNum; j++) //loops through segments within one structure
             {
                 double radAve = compute_radius(data1d,dimNum,seg_list.at(j).row,c);
+                double x,y,z,intensity=0;
+                for (int k=0; k<seg_list.at(j).row.size(); k++)
+                {
+                    x=seg_list.at(j).row.at(k).x;
+                    y=seg_list.at(j).row.at(k).y;
+                    z=seg_list.at(j).row.at(k).z;
+                    intensity += pixelVal(data1d,dimNum,x,y,z,c);
+                }
+                double intensAve = intensity/seg_list.at(j).row.size();
                 //v3d_msg(QString("radius %1").arg(radAve));
-                double diff,diff_min=255;
+                double diffRad,diffInt,diff,diff_min=255;
                 int cur_type=3;
                 for (int k=0; k<segRadArr.size(); k++)
                 {
-                    diff = abs(radAve-segRadArr.at(k));
+                    diffRad = abs(radAve-segRadArr.at(k));
+                    diffInt = abs(intensAve-segIntensArr.at(k));
+                    diff = (diffRad+diffInt)/2; //depending on further testing, may end up weighing this average differently
                     //v3d_msg(QString("diff %3 between calculated %1 and test %2").arg(radAve).arg(segRadArr.at(k)).arg(diff));
                     if (diff<diff_min)
                     {
@@ -230,9 +260,9 @@ void identify_neurons(V3DPluginCallback2 &callback, QWidget *parent)
                     seg_list.at(j).row.at(l).type=cur_type; //sets every unit in segment to be new type
                 }
                 //attempting to draw in new updated neuron trees, not working...
-                newTree = VSWC_2_neuron_tree(&seg_list.at(j)); //translates segment into a NeuronTree
-                callback.setSWC(curwin,newTree);
-                //v3d_msg(QString("newTree has %1 nodes").arg(newTree.listNeuron.size()));
+                newTree = VSWC_2_neuron_tree(&seg_list.at(j),id); //translates segment into a NeuronTree
+                id += seg_list.at(j).row.size();
+                //callback.setSWC(curwin,newTree);
                 //mTreeList->replace(i,newTree);
                 newTreeList.append(newTree);
                 //v3d_msg(QString("changed segment %1 of rad %3 to type %2").arg(j).arg(cur_type).arg(radAve));
@@ -242,7 +272,7 @@ void identify_neurons(V3DPluginCallback2 &callback, QWidget *parent)
         //need to draw newTreeList into window and remove mTreeList
         //*mTreeList = newTreeList;
         //callback.updateImageWindow(curwin);
-        //export_list2file(newTreeList,"saveName","openName"); //not working yet
+        export_list2file(newTreeList,"Labeled_SWC.swc",curfile);
     }
 
     return;
@@ -251,11 +281,11 @@ void identify_neurons(V3DPluginCallback2 &callback, QWidget *parent)
 
 /*  ##################################
  * [completed tasks]
- * all main algorithms are functional
+ * all main algorithms are functional, may not be optimized
  *
  * [current goals/issues]
  * mass_center tends to skew markers towards (0,0,0), compromises test data calculations
- * pixel function, changing coordinate range from cube to sphere is causing extreme slowdown
+ * pixel function has two algorithms, the first works but is slow, the second is faster but returns bad data
  *
  * [future goals]
  * include overload for when test data includes no background markers
@@ -555,6 +585,8 @@ template <class T> LandmarkList main_func(T* data1d, V3DLONG *dimNum, int c, Lan
     ValAveArr = new int[marks]; radAveArr = new int[marks];
     LocationSimple tempLocation(0,0,0);
     double ValAve=0,radAve=0;
+    clock_t t1;
+    t1 = clock();
     for (int i=0; i<marks; i++)
     {
         tempLocation = MarkList.at(i);
@@ -569,6 +601,8 @@ template <class T> LandmarkList main_func(T* data1d, V3DLONG *dimNum, int c, Lan
 
 //v3d_msg(QString("ValAve %1, radAve %2").arg(ValAve).arg(radAve));
     }
+    t1 = clock() - t1;
+    cout<<"dynamic pixel time "<<t1<<endl;
 
 //v3d_msg("scan checkpoint");
 
@@ -599,11 +633,15 @@ template <class T> LandmarkList main_func(T* data1d, V3DLONG *dimNum, int c, Lan
     double PixStDev = sqrt(stP/marks);
 
 
-v3d_msg(QString("markers have been scanned. Pixval %1 and stdev %2. radVal %3 and stdev %4."
-                      "segVal %5 and stdev %6").arg(PixVal).arg(PixStDev).arg(radAve).arg(radStDev).arg(ValAve).arg(ValStDev));
-//v3d_msg(QString("category %1").arg(cat));
+//v3d_msg(QString("markers have been scanned. Pixval %1 and stdev %2. radVal %3 and stdev %4."
+//                      "segVal %5 and stdev %6").arg(PixVal).arg(PixStDev).arg(radAve).arg(radStDev).arg(ValAve).arg(ValStDev));
+v3d_msg(QString("category %1").arg(cat));
+    clock_t t;
+    t = clock();
     //scans image and generates new set of markers based on testing data
     LandmarkList newList = count(data1d,dimNum,ValAve,2*ValStDev,PixVal,PixStDev,0,radAve,5+radStDev,c,cat);
+    t = clock() - t;
+    cout<<"count time "<<t<<endl;
 
     //recenters list via mean shift
 v3d_msg("recentering");
@@ -788,6 +826,7 @@ template <class T> pair<int,int> pixel(T* data1d,
     V3DLONG k,j,i;
     //average data of each segment
     int datatotal=0,runs=0;
+    double t,dist;
     for (k = zLow; k <= zHigh; k++)
     {
          V3DLONG shiftZ = k*M*N;
@@ -796,22 +835,45 @@ template <class T> pair<int,int> pixel(T* data1d,
              V3DLONG shiftY = j*N;
              for (i = xLow; i <= xHigh; i++)
              {
-                 //######The below code changes the check range from a box to a sphere, but slows down the code immensely when called in count function
-                 //double t = (i-xc)*(i-xc)+(j-yc)*(j-yc)+(k-zc)*(k-zc);
-                 //double dist = sqrt(t);
-                 //if (dist<=rad)
+                 t = (i-xc)*(i-xc)+(j-yc)*(j-yc)+(k-zc)*(k-zc);
+                 dist = sqrt(t);
+                 if (dist<=rad)
                  {
                      int dataval = data1d[ shiftC + shiftZ + shiftY + i ];
                      datatotal += dataval;
                      runs++;
                  }
-                 //else continue;
+                 else continue;
              }
          }
     }
+
+    /*double x,y,z,datatotal,pi=3.14;
+    int runs=0;
+    if (rad==0); rad=1;
+    double radinterval = rad;
+    radinterval/=5;
+    for (double r=0; r<rad; r+=radinterval) //r isn't increasing
+    {
+        for (double theta=0; theta<2*pi; theta+=(pi/4))
+        {
+            for (double phi=0; phi<pi; phi+=(pi/4))
+            {
+                //cout<<"pixel iteration "<<runs<<endl;
+                //cout<<r<<" "<<theta<<" "<<phi<<endl<<endl;
+                x = xc+r*cos(theta)*sin(phi);
+                y = yc+r*sin(theta)*sin(phi);
+                z = zc+r*cos(phi);
+                double dataval = pixelVal(data1d,dimNum,x,y,z,c);
+                datatotal += dataval;
+                runs++;
+            }
+        }
+    }*/
     int dataAve = datatotal/runs;
     //data of point
-    int pointval = data1d[ shiftC + zc*M*N + yc*N + xc ];
+    int pointval = pixelVal(data1d,dimNum,xc,yc,zc,c);
+    //cout<<dataAve<<endl;
 
     return make_pair(dataAve,pointval);
 }
@@ -874,6 +936,7 @@ template <class T> LandmarkList count(T* data1d,
     //this part is for dynamically calculated rad
     else
     {
+        cout<<"starting count"<<endl;
         seg = radAve/2;
         int init;
         if ((radAve-radStDev) < 1) { init=1;}
@@ -910,7 +973,7 @@ template <class T> LandmarkList count(T* data1d,
                         else continue;
                     }
                 }
-            }
+            } cout<<"rad iteration "<<i<<endl;
         }
 
     }
@@ -1004,22 +1067,23 @@ V_NeuronSWC_list get_neuron_segments(const NeuronTree *p)
     seg_list = cur_seg.decompose();
     return seg_list;
 }
-NeuronSWC make_neuron_swc(V_NeuronSWC_unit *p)
+NeuronSWC make_neuron_swc(V_NeuronSWC_unit *p, int id, bool &start)
 {
     NeuronSWC N;
 
-    N.n     = p->n;
+    N.n     = id;
     N.type  = p->type;
     N.x     = p->x;
     N.y     = p->y;
     N.z     = p->z;
     N.r     = p->r;
-    N.parent = p->parent;
+    if (start==false)    N.parent = id-1;
+    else                 N.parent = -1;
 
     return N;
 }
 
-NeuronTree VSWClist_2_neuron_tree(V_NeuronSWC_list *p)
+/*NeuronTree VSWClist_2_neuron_tree(V_NeuronSWC_list *p)
 {
     QList<NeuronSWC> nTree;
     for (int i=0; i<p->size(); i++)
@@ -1035,15 +1099,19 @@ NeuronTree VSWClist_2_neuron_tree(V_NeuronSWC_list *p)
     NeuronTree newTree;
     newTree.listNeuron = nTree;
     return newTree;
-}
-NeuronTree VSWC_2_neuron_tree(V_NeuronSWC *p)
+}*/
+NeuronTree VSWC_2_neuron_tree(V_NeuronSWC *p, int id)
 {
     QList<NeuronSWC> nTree;
+    bool start;
     for (int j=0; j<p->row.size(); j++)
     {
+        if (j==0)   start=true;
+        else        start=false;
         V_NeuronSWC_unit v = p->row.at(j);
-        NeuronSWC n = make_neuron_swc(&v);
+        NeuronSWC n = make_neuron_swc(&v,id,start);
         nTree.append(n);
+        id++;
     }
 
     NeuronTree newTree;
@@ -1058,7 +1126,7 @@ template <class T> double compute_radius(T* data1d, V3DLONG *dimNum, vector<V_Ne
     V3DLONG P = dimNum[2];
     double radAve;
 
-    if (segment.size()>2)
+    /*if (segment.size()>2)
     {
         double radTot=0;
         double k,j,i,t,dist,plane;
@@ -1071,7 +1139,7 @@ template <class T> double compute_radius(T* data1d, V3DLONG *dimNum, vector<V_Ne
             double norm[] = {P2.x-P1.x,P2.y-P1.y,P2.z-P1.z};
             double rad=0;
             int pVal = pixelVal(data1d,dimNum,P0.x,P0.y,P0.z,c);
-            if (pVal<50) { /*v3d_msg("pVal low, bad neuron, skipping");*/ continue; }
+            if (pVal<50) { v3d_msg("pVal low, bad neuron, skipping"); continue; }
             int pValCircTot=0, runs=0;
             double check=0;
             do
@@ -1112,17 +1180,59 @@ template <class T> double compute_radius(T* data1d, V3DLONG *dimNum, vector<V_Ne
             radTot += rad;
         }
         radAve = radTot/(segment.size()-2);
+    }*/
+
+
+    if (segment.size()>2)
+    {
+        double rad,radTot=0;
+        for (int unit=1; unit<segment.size()-1; unit++) //going to omit first and last unit per segment
+        {
+            V_NeuronSWC_unit P1,P2,P0;
+            P0 = segment.at(unit);
+            P1 = segment.at(unit-1);
+            P2 = segment.at(unit+1);
+            double Vnum[] = {P2.x-P1.x,P2.y-P1.y,P2.z-P1.z};
+            double Vnorm = sqrt(Vnum[0]*Vnum[0]+Vnum[1]*Vnum[1]+Vnum[2]*Vnum[2]);
+            double V[] = {Vnum[0]/Vnorm,Vnum[1]/Vnorm,Vnum[2]/Vnorm}; //axis of rotation
+            double Anum[] = {-V[1],V[0],(-V[1]*V[0]-V[0]*V[1])/V[2]};
+            double Anorm = sqrt(Anum[0]*Anum[0]+Anum[1]*Anum[1]+Anum[2]*Anum[2]);
+            double A[] = {Anum[0]/Anorm,Anum[1]/Anorm,Anum[2]/Anorm}; //perpendicular to V
+            double B[] = {A[1]*V[2]-A[2]*V[1],A[2]*V[0]-A[0]*V[2],A[0]*V[1]-A[1]*V[0]}; //perpendicular to A and V
+            rad=0;
+            int pVal = pixelVal(data1d,dimNum,P0.x,P0.y,P0.z,c);
+            if (pVal<50) { /*v3d_msg("pVal low, bad neuron, skipping");*/ continue; }
+            int pValCircTot=0, runs=0;
+            double check=255, pi=3.14;
+            double x,y,z;
+            do
+            {
+                rad += 0.2;
+                for (double theta=0; theta<2*pi; theta+=pi/8)
+                {
+                    x = P0.x+rad*cos(theta)*A[0]+rad*sin(theta)*B[0];
+                    y = P0.y+rad*cos(theta)*A[1]+rad*sin(theta)*B[1];
+                    z = P0.z+rad*cos(theta)*A[2]+rad*sin(theta)*B[2];
+                    int pValCirc = pixelVal(data1d,dimNum,x,y,z,c);
+                    pValCircTot += pValCirc;
+                    runs++;
+                }
+                check=pValCircTot/runs;
+            } while (check > pVal*2/3);
+            radTot += rad;
+        }
+        radAve = radTot/(segment.size()-2);
     }
 
     else //2-unit segment
     {
-        v3d_msg("2-unit neuron SWC radius calculation to be implemented");
+        v3d_msg("2-unit neuron SWC radius calculation to be implemented, please remove from test data");
     }
 
     return radAve;
 }
 
-//this algorithm is much worse than mark_or_curve
+//obsolete function
 void markers_singleChannel(V3DPluginCallback2 &callback, QWidget *parent)
 {
     v3dhandle curwin = callback.currentImageWindow();
@@ -1223,7 +1333,8 @@ void markers_singleChannel(V3DPluginCallback2 &callback, QWidget *parent)
     }
     return;
 }
-bool export_list2file(QList<NeuronTree> & N2, QString &fileSaveName, QString &fileOpenName)
+
+bool export_list2file(QList<NeuronTree> & N2, QString fileSaveName, QString fileOpenName)
 {
     QFile file(fileSaveName);
     if (!file.open(QIODevice::WriteOnly|QIODevice::Text))
@@ -1237,9 +1348,12 @@ bool export_list2file(QList<NeuronTree> & N2, QString &fileSaveName, QString &fi
     {
         N1 = N2.at(j).listNeuron;
         for (V3DLONG i=0;i<N1.size();i++)
+        {
             myfile << N1.at(i).n <<" " << N1.at(i).type << " "<< N1.at(i).x <<" "<<N1.at(i).y << " "<< N1.at(i).z << " "<< N1.at(i).r << " " <<N1.at(i).pn << "\n";
+        }
     }
     file.close();
+    v3d_msg(QString("SWC file %1 has been generated, size %2").arg(fileSaveName).arg(N1.size()));
     cout<<"swc file "<<fileSaveName.toStdString()<<" has been generated, size: "<<N1.size()<<endl;
     return true;
 };
