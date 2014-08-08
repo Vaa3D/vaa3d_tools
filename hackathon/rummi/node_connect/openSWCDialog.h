38a3975c95332af5c79ac763f7e9600beab50c04
surobhi.ganguly <surobhi.ganguly@gmail.com>
2014-08-08 19:55:41 +0000

    first commit

---
+++ hackathon/rummi/node_connect/openSWCDialog.h
@@ -0,0 +1,25 @@
+#ifndef __OPENSWCDIALOG_H_
+#define __OPENSWCDIALOG_H_
+#include <QtGui>
+#include "v3d_interface.h"
+class OpenSWCDialog: public QDialog
+{
+    Q_OBJECT
+
+public:
+    V3DPluginCallback2 * callback;
+    QListWidget * listWidget;
+    QList<NeuronTree> _3DViewerTreeList;
+    NeuronTree  nt;
+    QString file_name;
+
+    OpenSWCDialog(QWidget * parent, V3DPluginCallback2 * callback);
+    void getAllNeuronTreeNames(V3DPluginCallback2 * callback);
+
+public slots:
+    bool run();
+    bool setTree(const QString & file);
+};
+
+
+#endif
