b9a495d694419820a2f92eb5a8c65318bd52fb41
surobhi.ganguly <surobhi.ganguly@gmail.com>
2014-08-18 16:46:11 +0000

    syncing code

--- released_plugins/v3d_plugins/typeset_childbranch/typeset_plugin.cpp
+++ released_plugins/v3d_plugins/typeset_childbranch/typeset_plugin.cpp
@@ -68,11 +68,11 @@ bool TypesetPlugin::dofunc(const QString & func_name, const V3DPluginArgList & i
 
     if (func_name == tr("typeset_swc"))
     {
-        v3d_msg("To be implemented.");
+        v3d_msg("to be implemented.");
     }
     else if (func_name == tr("help"))
     {
-        v3d_msg("To be implemented.");
+        v3d_msg("to be implemented.");
     }
     else return false;
 
