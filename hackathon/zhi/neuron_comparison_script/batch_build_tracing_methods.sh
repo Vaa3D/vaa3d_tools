c0cd72683c5343ba35b67b6493c6b8f4a2fedb3a
Zhi Zhou <alan.zhizhou@gmail.com>
2014-11-02 22:51:52 +0000

    add 'svn up' step and also add MST tracing in the build script

--- hackathon/zhi/neuron_comparison_script/batch_build_tracing_methods.sh
+++ hackathon/zhi/neuron_comparison_script/batch_build_tracing_methods.sh
@@ -1,27 +1,38 @@
 #!/bin/bash
-#THis is a shell progrem to batch build tracing methods plugins, including APP1, APP2, MOST, neuTube, snake,simpletracing, and TreMap.
+#THis is a shell progrem to batch build tracing methods plugins, including APP1, APP2, MOST, neuTube, snake,simpletracing, TreMap, and MST.
 
 #APP1 and APP2 
 cd ../../../released_plugins/v3d_plugins/neurontracing_vn2/
 make clean
+svn up
 qmake
 make
 
 #MOST 
 cd ../neurontracing_MOST/
 make clean
+svn up
 qmake
 make	
 
 #simpletracing 
 cd ../neurontracing_simpletracing/
 make clean
+svn up
 qmake
 make
 
 #TreMap 
 cd  ../../../hackathon/zhi/neurontracing_mip/
 make clean
+svn up
+qmake
+make
+
+#MST 
+cd  ../../../hackathon/zhi/neurontracing_mst/
+make clean
+svn up
 qmake
 make
 
@@ -31,11 +42,13 @@ if [ ! -d ITK_include ]; then
 	unzip ITK_include.zip		
 fi	
 make clean
+svn up
 qmake
 make
 
 #neuTube 
 cd ../../../hackathon/zhi/neuTube_zhi/
 make clean
+svn up
 qmake
 make
