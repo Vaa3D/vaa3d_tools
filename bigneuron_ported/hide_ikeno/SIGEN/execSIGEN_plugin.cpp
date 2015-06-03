74d63deb36e5dd512a23d18b9049a9a6b0d9f68a
ikeno <ikeno@shse.u-hyogo.ac.jp>
2015-06-03 18:21:10 +0000

    by H.Ikeno

--- bigneuron_ported/hide_ikeno/SIGEN/execSIGEN_plugin.cpp
+++ bigneuron_ported/hide_ikeno/SIGEN/execSIGEN_plugin.cpp
@@ -41,11 +41,13 @@ struct input_PARA
 };
 
 void reconstruction_func(V3DPluginCallback2 &callback, QWidget *parent, input_PARA &PARA, bool bmenu);
+void reconstruction_func2(V3DPluginCallback2 &callback, QWidget *parent, input_PARA &PARA, bool bmenu);
 
 QStringList SIGEN::menulist() const
 {
 	return QStringList() 
         <<tr("tracing_menu")
+        <<tr("extract")
 		<<tr("about");
 }
 
@@ -53,6 +55,7 @@ QStringList SIGEN::funclist() const
 {
 	return QStringList()
 		<<tr("tracing_func")
+        <<tr("extract_func")
 		<<tr("help");
 }
 
@@ -66,10 +69,18 @@ void SIGEN::domenu(const QString &menu_name, V3DPluginCallback2 &callback, QWidg
 
         reconstruction_func(callback,parent,PARA,bmenu);
 	}
+    else if (menu_name == tr("extract"))
+    {
+
+        bool bmenu = true;
+        input_PARA PARA;
+
+        reconstruction_func2(callback,parent,PARA,bmenu);
+    }
 	else
 	{
 		v3d_msg(tr("This is a test plugin, you can use it as a demo.. "
-			"Developed by YourName, 2015-6-2"));
+            "Developed by Hidetoshi Ikeno, 2015-6-2"));
 	}
 }
 
@@ -96,6 +107,28 @@ bool SIGEN::dofunc(const QString & func_name, const V3DPluginArgList & input, V3
         PARA.channel = (paras.size() >= k+1) ? atoi(paras[k]) : 1;  k++;
         reconstruction_func(callback,parent,PARA,bmenu);
 	}
+    else if (func_name == tr("extract_func"))
+    {
+       bool bmenu = false;
+       input_PARA PARA;
+
+       vector<char*> * pinfiles = (input.size() >= 1) ? (vector<char*> *) input[0].p : 0;
+       vector<char*> * pparas = (input.size() >= 2) ? (vector<char*> *) input[1].p : 0;
+       vector<char*> infiles = (pinfiles != 0) ? * pinfiles : vector<char*>();
+       vector<char*> paras = (pparas != 0) ? * pparas : vector<char*>();
+
+       if(infiles.empty())
+       {
+           fprintf (stderr, "Need input image. \n");
+           return false;
+       }
+       else
+           PARA.inimg_file = infiles[0];
+       int k=0;
+       PARA.channel = (paras.size() >= k+1) ? atoi(paras[k]) : 1;  k++;
+       reconstruction_func2(callback,parent,PARA,bmenu);
+
+    }
     else if (func_name == tr("help"))
         {
 
@@ -274,6 +307,243 @@ void reconstruction_func(V3DPluginCallback2 &callback, QWidget *parent, input_PA
     // using Fiji
     // Fiji have to be loacted in /Applications
     
+    system("/Applications/Fiji.app/Contents/MacOS/ImageJ-macosx fileOut.ijm");
+
+    // executing MorphExtractorCL.exe on mono
+    QString strTh, strDt, strVt, strNs, strNc;
+    QString command = "mono ./MorphExtractorCL.exe -i /var/tmp/vaa3D-SIGEN -o out -b "+strTh.setNum(th)+" -t 1 -v "+strVt.setNum(vt)+" -d "+strDt.setNum(dt)+" -s "+strNs.setNum(ns)+" -a "+strNc.setNum(nc);
+    system(command.toStdString().c_str());
+
+    //    system("mono ./MorphExtractorCL.exe -i ./tmp -o out -b 128 -t -v 2 -d 10 -s 10 -a 10");
+    system("rm /var/tmp/vaa3D-SIGEN/*");
+    system("rmdir /var/tmp/vaa3D-SIGEN");
+
+    //Output
+
+
+    NeuronTree nt;
+    QString swc_name = PARA.inimg_file + "_SIGEN.swc";
+    nt.name = "execSIGEN";
+    //    writeSWC_file(swc_name.toStdString().c_str(),nt);
+
+    if (fexist("out.swc")) {
+        command = "cp out.swc "+swc_name;
+            system(command.toStdString().c_str());
+
+        system("rm out.swc");
+    } else {
+        v3d_msg(QString("Install Mono at http://www.mono-project.com/ & copy MorphExtractorCL.exe in your vaa3D directory"));
+    }
+
+    ofstream log_file;
+    QString log_name = PARA.inimg_file+"_SIGEN.txt";
+    log_file.open(log_name.toStdString().c_str());
+
+    time_t now = time(NULL);
+    struct tm *pnow = localtime(&now);
+    log_file << "Date_Time: ";
+    log_file << pnow->tm_year + 1900 << "/" << pnow->tm_mon + 1 << "/" << pnow->tm_mday <<
+                " " << pnow->tm_hour << ":" << pnow->tm_min << ":" << pnow->tm_sec << "\n";
+
+    log_file << "Input: ";
+    log_file << PARA.inimg_file.toStdString().c_str() << "\n";
+    log_file << "Output: ";
+    log_file << swc_name.toStdString().c_str();
+    log_file << "\n";
+    log_file << "Binarization_threshold: ";
+    log_file << strTh.setNum(th).toStdString().c_str();
+    log_file << "\n";
+    log_file << "Volume_threshold: ";
+    log_file << strTh.setNum(vt).toStdString().c_str();
+    log_file << "\n";
+    log_file << "Distance_threshold: ";
+    log_file << strTh.setNum(dt).toStdString().c_str();
+    log_file << "\n";
+    log_file << "Smoothing_level: ";
+    log_file << strTh.setNum(ns).toStdString().c_str();
+    log_file << "\n";
+    log_file << "Clipping_level: ";
+    log_file << strTh.setNum(nc).toStdString().c_str();
+
+    log_file.close();
+
+    if(!bmenu)
+    {
+        if(data1d) {delete []data1d; data1d = 0;}
+    }
+
+    return;
+}
+
+
+void reconstruction_func2(V3DPluginCallback2 &callback, QWidget *parent, input_PARA &PARA, bool bmenu)
+{
+    unsigned char* data1d = 0;
+    V3DLONG N,M,P,sc,c;
+    V3DLONG in_sz[4];
+    if(bmenu)
+    {
+        v3dhandle curwin = callback.currentImageWindow();
+        if (!curwin)
+        {
+            QMessageBox::information(0, "", "You don't have any image open in the main window.");
+            return;
+        }
+
+        Image4DSimple* p4DImage = callback.getImage(curwin);
+
+        if (!p4DImage)
+        {
+            QMessageBox::information(0, "", "The image pointer is invalid. Ensure your data is valid and try again!");
+            return;
+        }
+
+
+        data1d = p4DImage->getRawData();
+        N = p4DImage->getXDim();
+        M = p4DImage->getYDim();
+        P = p4DImage->getZDim();
+        sc = p4DImage->getCDim();
+
+        bool ok1;
+
+        if(sc==1)
+        {
+            c=1;
+            ok1=true;
+        }
+        else
+        {
+            c = QInputDialog::getInteger(parent, "Channel",
+                                             "Enter channel NO:",
+                                             1, 1, sc, 1, &ok1);
+        }
+
+        if(!ok1)
+            return;
+
+        in_sz[0] = N;
+        in_sz[1] = M;
+        in_sz[2] = P;
+        in_sz[3] = sc;
+
+
+        PARA.inimg_file = p4DImage->getFileName();
+    }
+    else
+    {
+        int datatype = 0;
+        if (!simple_loadimage_wrapper(callback,PARA.inimg_file.toStdString().c_str(), data1d, in_sz, datatype))
+        {
+            fprintf (stderr, "Error happens in reading the subject file [%s]. Exit. \n",PARA.inimg_file.toStdString().c_str());
+            return;
+        }
+        if(PARA.channel < 1 || PARA.channel > in_sz[3])
+        {
+            fprintf (stderr, "Invalid channel number. \n");
+            return;
+        }
+        N = in_sz[0];
+        M = in_sz[1];
+        P = in_sz[2];
+        sc = in_sz[3];
+        c = PARA.channel;
+    }
+
+    //main neuron reconstruction code
+
+    //// THIS IS WHERE THE DEVELOPERS SHOULD ADD THEIR OWN NEURON TRACING CODE
+
+    // v3d_msg(QString("Image file: %1").arg(PARA.inimg_file),bmenu);
+
+    QDialog dialog;
+    // Use a layout allowing to have a label next to each field
+    QFormLayout form(&dialog);
+
+    // Add some text above the fields
+    form.addRow(new QLabel("SIGEN Extraction"));
+
+    // Add the lineEdits with their respective labels
+    QList<QLineEdit *> fields;
+
+    QLineEdit *lineEdit1 = new QLineEdit(&dialog);
+    lineEdit1->setText("20");
+    QString label1 = QString("Binarization Th").arg(1);
+    form.addRow(label1, lineEdit1);
+    fields << lineEdit1;
+
+    QLineEdit *lineEdit2 = new QLineEdit(&dialog);
+    lineEdit2->setText("10");
+    QString label2 = QString("Distance Th").arg(2);
+    form.addRow(label2, lineEdit2);
+    fields << lineEdit2;
+
+    QLineEdit *lineEdit3 = new QLineEdit(&dialog);
+    lineEdit3->setText("1");
+    QString label3 = QString("Volume Th").arg(3);
+    form.addRow(label3, lineEdit3);
+    fields << lineEdit3;
+
+    QLineEdit *lineEdit4 = new QLineEdit(&dialog);
+    lineEdit4->setText("0");
+    QString label4 = QString("Smoothing Level").arg(4);
+    form.addRow(label4, lineEdit4);
+    fields << lineEdit4;
+
+    QLineEdit *lineEdit5 = new QLineEdit(&dialog);
+    lineEdit5->setText("0");
+    QString label5 = QString("Clipping Level").arg(5);
+    form.addRow(label5, lineEdit5);
+    fields << lineEdit5;
+
+    // Add some standard buttons (Cancel/Ok) at the bottom of the dialog
+    QDialogButtonBox buttonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel,
+                               Qt::Horizontal, &dialog);
+    form.addRow(&buttonBox);
+    QObject::connect(&buttonBox, SIGNAL(accepted()), &dialog, SLOT(accept()));
+    QObject::connect(&buttonBox, SIGNAL(rejected()), &dialog, SLOT(reject()));
+
+    // Show the dialog as modal
+    if (dialog.exec() == QDialog::Accepted) {
+        // If the user didn't dismiss the dialog, do something with the fields
+        foreach(QLineEdit * lineEdit1, fields) {
+            qDebug() << lineEdit1->text();
+        }
+    } else return;
+
+    int th=20, dt=10, vt=1, ns=0, nc=0;
+
+    th = atoi(lineEdit1->text().toStdString().c_str());
+    dt = atoi(lineEdit2->text().toStdString().c_str());
+    vt = atoi(lineEdit3->text().toStdString().c_str());
+    ns = atoi(lineEdit4->text().toStdString().c_str());
+    nc = atoi(lineEdit5->text().toStdString().c_str());
+
+    // using /var/tmp area for temporary saving of image files
+
+    system("mkdir /var/tmp/vaa3D-SIGEN");
+
+    // using Fiji in /Applications/Fiji.app/
+    // in the case of different directory, we have to change line 261
+
+    ofstream macro_file;
+
+    macro_file.open("/Applications/Fiji.app/macros/fileOut.ijm");
+
+    QString fiji_com = "open(\""+PARA.inimg_file+"\");\n";
+    macro_file << fiji_com.toStdString();
+
+    fiji_com = "run(\"Image Sequence... \", \"format=BMP name=in start=0 digits=4 save=/var/tmp/vaa3D-SIGEN/\");\n";
+    macro_file << fiji_com.toStdString();
+
+    fiji_com = "run(\"Quit\")";
+    macro_file << fiji_com.toStdString();
+
+    macro_file.close();
+
+    // using Fiji
+    // Fiji have to be loacted in /Applications
+
     system("/Applications/Fiji.app/Contents/MacOS/ImageJ-macosx fileOut.ijm");
 
     // executing MorphExtractorCL.exe on mono
