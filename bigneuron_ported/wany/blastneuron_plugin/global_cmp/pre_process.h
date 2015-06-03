36af6e8b764cb6f9b6c8c94d8341c3571d464c2c
Yinan Wan <wany1991@gmail.com>
2015-06-03 22:39:50 +0000

    blastneuron plugin done

--- bigneuron_ported/wany/blastneuron_plugin/global_cmp/pre_process.h
+++ bigneuron_ported/wany/blastneuron_plugin/global_cmp/pre_process.h
@@ -2,30 +2,12 @@
 #define __PRE_PROCESS_H__
 
 #include "basic_surf_objs.h"
-#include "prune_short_branch.h"
-#include "resampling.h"
-#include "align_axis.h"
+#include "../pre_processing/prune_short_branch.h"
+#include "../pre_processing/resampling.h"
+#include "../pre_processing/align_axis.h"
 
-NeuronTree pre_process(NeuronTree nt, double step_size)
-{
-	NeuronTree result;
-	NeuronTree pruned;
-	if (!prune_branch(nt, pruned))
-	{
-		fprintf(stderr,"Error in prune_short_branch.\n");
-		return result;
-	}
-	
-	printf("Resampling along segments\n");
-	NeuronTree resampled = resample(pruned, step_size);
-	
 
-	printf("Aligning PCA axis\n");
-	result = align_axis(resampled);
-
-	return result;
-}
-
+NeuronTree pre_process(NeuronTree nt, double step_size);
 #endif
 
 
