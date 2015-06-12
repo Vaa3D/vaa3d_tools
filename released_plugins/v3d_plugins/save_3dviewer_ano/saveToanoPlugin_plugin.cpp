679417a423736b345bda728783ff9cb7d9a19703
Hanbo Chen <cojoc.chen@gmail.com>
2015-06-12 22:04:57 +0000

    Added a new function for time stamp save

--- released_plugins/v3d_plugins/save_3dviewer_ano/saveToanoPlugin_plugin.cpp
+++ released_plugins/v3d_plugins/save_3dviewer_ano/saveToanoPlugin_plugin.cpp
@@ -17,6 +17,7 @@ using namespace std;
 Q_EXPORT_PLUGIN2(save3dviewer2linker, saveToanoPlugin);
 
 void generatorAno43Dviewer(V3DPluginCallback2 &callback, QWidget *parent);
+void generatorAno43Dviewer_timestamp(V3DPluginCallback2 &callback, QWidget *parent);
 
 controlPanel* controlPanel::panel = 0;
 
@@ -25,6 +26,7 @@ QStringList saveToanoPlugin::menulist() const
 {
 	return QStringList() 
         <<tr("save all SWC, APO, and Surface files to an ANO file")
+       <<tr("1_click_timestamp_save")
 		<<tr("about");
 }
 
@@ -40,6 +42,10 @@ void saveToanoPlugin::domenu(const QString &menu_name, V3DPluginCallback2 &callb
 	{
         generatorAno43Dviewer(callback,parent);
 	}
+    if (menu_name == tr("1_click_timestamp_save"))
+    {
+        generatorAno43Dviewer_timestamp(callback,parent);
+    }
 	else
 	{
         v3d_msg(tr("This is a plugin to save all SWC, APO, and Surface files from a 3D viewer,"
@@ -61,6 +67,122 @@ bool saveToanoPlugin::dofunc(const QString & func_name, const V3DPluginArgList &
 	else return false;
 
 	return true;
+}
+
+void generatorAno43Dviewer_timestamp(V3DPluginCallback2 &callback, QWidget *parent)
+{
+    //select the window to operate
+    QList <V3dR_MainWindow *> allWindowList = callback.getListAll3DViewers();
+    QList <V3dR_MainWindow *> selectWindowList;
+    V3dR_MainWindow * v3dwin;
+    QList<NeuronTree> * ntTreeList;
+    int winid;
+    qDebug("search for 3D windows");
+    for (V3DLONG i=0;i<allWindowList.size();i++)
+    {
+        ntTreeList = callback.getHandleNeuronTrees_Any3DViewer(allWindowList[i]);
+        if(ntTreeList->size()>0){
+            QString fname_win=callback.getImageName(allWindowList[i]);
+            if(fname_win.contains("[") && fname_win.contains("]"))
+                selectWindowList.append(allWindowList[i]);
+        }
+    }
+    qDebug("match and select 3D windows");
+    if(selectWindowList.size()<1){
+        v3d_msg("Cannot find 3D view with SWC file. Please load the SWC files you want to color in the 3D view");
+        return;
+    }else if(selectWindowList.size()>1){
+        //pop up a window to select
+
+        QStringList items;
+        for(int i=0; i<selectWindowList.size(); i++){
+            items.append(callback.getImageName(selectWindowList[i]));
+        }
+        bool ok;
+        QString selectitem = QInputDialog::getItem(parent, QString::fromUtf8("1-click neuron color"), QString::fromUtf8("Select A Window to Operate"), items, 0, false, &ok);
+        if(!ok) return;
+        for(int i=0; i<selectWindowList.size(); i++){
+            if(selectitem==callback.getImageName(selectWindowList[i]))
+            {
+                winid=i;
+                break;
+            }
+        }
+    }else{
+        winid=0;
+    }
+    v3dwin = selectWindowList[winid];
+
+    //get the file name
+    QString fname_win=callback.getImageName(v3dwin).remove("3D View [").remove("]");
+    QDateTime mytime = QDateTime::currentDateTime();
+    QString fname_out=fname_win + "_stamp_" + mytime.toString("yyyy_MM_dd_hh_mm");
+    qDebug()<<"Will save file to "<<fname_out;
+
+    //get content
+    ntTreeList=callback.getHandleNeuronTrees_Any3DViewer(v3dwin);
+    QStringList SWC_list;
+    for(int i=0; i<ntTreeList->size(); i++){
+        QString fname_swc=fname_out;
+        if(i>0){
+            fname_swc=fname_swc+"_nt"+QString::number(i);
+        }
+        fname_swc=fname_swc+".swc";
+        SWC_list.append(fname_swc);
+    }
+
+    DataLists_in_3dviewer listItem = callback.fetch_3dviewer_datafilelist(callback.getImageName(v3dwin));
+
+    QList<NeuronTree> * mTreeList;
+    mTreeList = callback.getHandleNeuronTrees_Any3DViewer(v3dwin);
+
+    QList<CellAPO> * mAPOList;
+    mAPOList = callback.getHandleAPOCellList_Any3DViewer(v3dwin);
+
+    QList<CellAPO> mAPOList_v2;
+    for (int i = 0; i < mAPOList->size();i++)
+            mAPOList_v2.push_back(mAPOList->at(i));
+
+    QString imgname = listItem.imgfile;
+    QString labelfieldname = listItem.labelfield_file;
+    QString surfacename = listItem.surface_file;
+    QStringList APO_list = listItem.pointcloud_file_list;
+
+    //start saving
+    QString filename=fname_out+".ano";
+    ofstream anofile(filename.toStdString().c_str(),ios::out | ios::app );
+    if(!anofile.is_open()){
+        v3d_msg("Error opening file to save: "+filename);
+        return;
+    }
+
+    if(SWC_list.count()>0)
+    {
+        for(V3DLONG i = 0; i < SWC_list.count(); i++)
+        {
+            NeuronTree mTree = mTreeList->at(i);
+            QString newSWCname = SWC_list.at(i);
+            writeSWC_file(newSWCname,mTree);
+            anofile << "SWCFILE=" << newSWCname.toStdString().c_str() << endl;
+        }
+
+    }
+
+    if(APO_list.count()>0)
+    {
+            QString newAPOname = fname_out + "_" + QFileInfo(APO_list.at(0)).fileName();
+            writeAPO_file(newAPOname,mAPOList_v2);
+            anofile << "APOFILE=" << newAPOname.toStdString().c_str() << endl;
+    }
+
+    if(imgname.size()>0) anofile << "RAWIMG=" << imgname.toStdString().c_str() << endl;
+    if(surfacename.size()>0) anofile << "SURFILE=" << surfacename.toStdString().c_str() << endl;
+    if(labelfieldname.size()>0) anofile << "SURFILE=" << labelfieldname.toStdString().c_str() << endl;
+
+    anofile.close();
+
+    qDebug()<<QString("The ANO file is save in:[%1]").arg(filename.toStdString().c_str());
+    return;
 }
 
 void generatorAno43Dviewer(V3DPluginCallback2 &v3d, QWidget *parent)
