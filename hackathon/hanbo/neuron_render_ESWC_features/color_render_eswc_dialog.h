e01c23be979d3c9171d8224fb331a29ae85f1ceb
Hanbo Chen <cojoc.chen@gmail.com>
2015-06-22 23:58:42 +0000

    A new plugin to render features in ESWC file. The user can select the feature to render and the data span to render :)

---
+++ hackathon/hanbo/neuron_render_ESWC_features/color_render_eswc_dialog.h
@@ -0,0 +1,46 @@
+#ifndef COLOR_RENDER_ESWC_DIALOG_H
+#define COLOR_RENDER_ESWC_DIALOG_H
+
+#include <QDialog>
+#include <QtGui>
+#include <v3d_interface.h>
+#include <vector>
+
+using namespace std;
+
+class color_render_ESWC_dialog : public QDialog
+{
+    Q_OBJECT
+public:
+    explicit color_render_ESWC_dialog(V3DPluginCallback2 * callback, V3dR_MainWindow* v3dwin, int nid);
+    void enterEvent(QEvent *e);
+
+signals:
+
+public slots:
+    void update();
+    void reset();
+    void reject();
+    void set_black();
+    void set_white();
+    void set_magenta();
+    void set_span();
+
+protected:
+    void checkwindow();
+
+public:
+    QComboBox *cb_feature;
+    QDoubleSpinBox *spin_min, *spin_max, *spin_black, *spin_white, *spin_meg;
+    QCheckBox *check_black, *check_white, *check_meg;
+    QPushButton *btn_update, *btn_quit, *btn_reset;
+
+private:
+    NeuronTree * nt;
+    int neuronid, feaNum;
+    vector<unsigned char> bk_type;
+    V3dR_MainWindow * v3dwin;
+    V3DPluginCallback2 * callback;
+};
+
+#endif // COLOR_RENDER_ESWC_DIALOG_H
