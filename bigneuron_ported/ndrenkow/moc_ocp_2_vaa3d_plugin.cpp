048b927ef9cc4b44f0cbb9c12150e90e56718149
Zhi Zhou <alan.zhizhou@gmail.com>
2015-07-10 02:44:03 +0000

    revised ocp_2_vaa3d.pro

--- bigneuron_ported/ndrenkow/moc_ocp_2_vaa3d_plugin.cpp
+++ bigneuron_ported/ndrenkow/moc_ocp_2_vaa3d_plugin.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'ocp_2_vaa3d_plugin.h'
 **
-** Created: Wed Jun 3 18:48:04 2015
+** Created: Thu Jul 9 19:43:33 2015
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.7.1)
 **
 ** WARNING! All changes made in this file will be lost!
