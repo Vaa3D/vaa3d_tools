73e65da6baad89287415683b3cbd8885a6e950a6
Hanbo Chen <cojoc.chen@gmail.com>
2015-07-16 00:30:25 +0000

    Check-in to save. Not done yet. Added manually add/delete functions.

--- hackathon/hanbo/neuron_assemble_live/assemble_neuron_live_dialog.cpp
+++ hackathon/hanbo/neuron_assemble_live/assemble_neuron_live_dialog.cpp
@@ -41,12 +41,19 @@ void assemble_neuron_live_dialog::creat(QWidget *parent)
     QGridLayout * layout_conn = new QGridLayout();
     list_link = new QListWidget();
     btn_connect = new QPushButton("connect select pair");
-    btn_manuallink = new QPushButton("add/del connection pair");
+    btn_manuallink = new QPushButton("add connection pair");
+    btn_deletelink = new QPushButton("delete connection pair");
+    check_loop = new QCheckBox("Check loop before connect"); check_loop->setChecked(true);
     layout_conn->addWidget(list_link,0,0,6,2);
     layout_conn->addWidget(btn_connect,0,2,1,1);
     layout_conn->addWidget(btn_manuallink,1,2,1,1);
+    layout_conn->addWidget(btn_deletelink,2,2,1,1);
+    layout_conn->addWidget(check_loop,3,2,1,1);
     dialog_conn->setLayout(layout_conn);
     tab->addTab(dialog_conn,tr("connect"));
+
+    connect(btn_manuallink,SIGNAL(clicked()),this,SLOT(pairMarker()));
+    connect(btn_deletelink,SIGNAL(clicked()),this,SLOT(delPair()));
 
     //neuron breaker
     QDialog * dialog_break = new QDialog(tab);
@@ -63,6 +70,8 @@ void assemble_neuron_live_dialog::creat(QWidget *parent)
     layout_break->addWidget(btn_colorloop, 4,2,1,1);
     dialog_break->setLayout(layout_break);
     tab->addTab(dialog_break,tr("break"));
+
+    connect(btn_syncmarker,SIGNAL(clicked()),this,SLOT(syncMarker()));
 
     layout->addWidget(tab,3,0,1,2);
 
@@ -192,10 +201,243 @@ void assemble_neuron_live_dialog::initNeuron(QList<NeuronTree> &ntList)
     }
 }
 
+
+void assemble_neuron_live_dialog::syncMarker()
+{
+    LandmarkList * mList = getMarkerList();
+
+    list_marker->clear();
+    for(V3DLONG i=0; i<mList->size(); i++){
+        QString tmp="marker: "+QString::number(i+1);
+        list_marker->addItem(tmp);
+    }
+
+    if(mList->size()==0){
+        v3d_msg("Not marker identified. Please define some in the 3D viewer.");
+    }
+}
+
+void assemble_neuron_live_dialog::pairMarker()
+{
+    LandmarkList * mList = getMarkerList();
+
+    if(mList->size()<=0){
+        v3d_msg("Not marker identified. Please define some in the 3D viewer.");
+
+        list_link->clear();
+        return;
+    }
+
+    //find exist links
+    QSet<QPair<V3DLONG,V3DLONG> > links;
+    QList<int> info;
+    QHash<int, int> markerTable;
+    for(int mid=0; mid<mList->size(); mid++){
+        get_marker_info(mList->at(mid), info);
+        markerTable.insert(info.at(0),mid);
+    }
+    for(int mid=0; mid<mList->size(); mid++){
+        get_marker_info(mList->at(mid), info);
+        QList<int>::iterator iter = info.begin();
+        iter++;
+        while(iter!=info.end()){
+            if(info.at(0)!=*iter && markerTable.contains(*iter)){
+                QPair<V3DLONG,V3DLONG> tmp(MIN(info.at(0), *iter),MAX(info.at(0), *iter));
+                links.insert(tmp);
+                iter++;
+            }else{
+                iter=info.erase(iter);
+                update_marker_info(mList->at(mid), info);
+            }
+        }
+    }
+
+    //ask for the new link
+    pair_marker_dialog pairDlg(mList, this);
+    if(pairDlg.exec()){ //accepted
+        QList<int> info1, info2;
+        int mid1 = pairDlg.list1->currentRow();
+        int mid2 = pairDlg.list2->currentRow();
+        get_marker_info(mList->at(mid1), info1);
+        get_marker_info(mList->at(mid2), info2);
+        if(info1.at(0)!=info2.at(0)){
+            QPair<V3DLONG,V3DLONG> tmp(MIN(info1.at(0), info2.at(0)),MAX(info1.at(0), info2.at(0)));
+            if(!links.contains(tmp)){
+                links.insert(tmp);
+                info1.push_back(info2.at(0));
+                info2.push_back(info1.at(0));
+                update_marker_info(mList->at(mid1), info1);
+                update_marker_info(mList->at(mid2), info2);
+            }
+        }
+    }else{
+        return;
+    }
+
+    //update list widget
+    list_link->clear();
+    for(QSet<QPair<V3DLONG,V3DLONG> >::iterator iter = links.begin(); iter!=links.end(); iter++){
+        V3DLONG pid1, pid2;
+        pid1=iter->first;
+        pid2=iter->second;
+        int mid1 = markerTable[pid1]+1;
+        int mid2 = markerTable[pid2]+1;
+        QString tmp;
+        if(mid1<mid2){
+            tmp = "Marker "+QString::number(mid1)+" x "+
+                    QString::number(mid2)+" (Node "+QString::number(pid1)+" x "+
+                    QString::number(pid2)+" )";
+        }else{
+            tmp = "Marker "+QString::number(mid2)+" x "+
+                    QString::number(mid1)+" (Node "+QString::number(pid2)+" x "+
+                    QString::number(pid1)+" )";
+        }
+        list_link->addItem(tmp);
+    }
+    if(list_link->count()>0){
+        list_link->setCurrentRow(0);
+    }
+
+    update3DView();
+}
+
+void assemble_neuron_live_dialog::delPair()
+{
+    if(list_link->currentRow()>=0 && list_link->currentRow()<list_link->count()){
+        QString tmp = list_link->currentItem()->text();
+        QStringList items = tmp.split(" ", QString::SkipEmptyParts);
+        int mid, pid;
+        bool check;
+        LandmarkList * mList = getMarkerList();
+        mid=items.at(1).toInt(&check)-1;
+        if(check && mid>=0 && mid<mList->size()){
+            pid=items.at(7).toInt(&check);
+            if(check){
+                QList<int> info;
+                get_marker_info(mList->at(mid), info);
+                QList<int>::iterator iter=info.begin();
+                iter++;
+                while(iter!=info.end()){
+                    if(*iter == pid){
+                        info.erase(iter);
+                    }else{
+                        iter++;
+                    }
+                }
+                update_marker_info(mList->at(mid), info);
+            }
+            qDebug()<<mid<<":"<<pid;
+        }
+        mid=items.at(3).toInt(&check)-1;
+        if(check && mid>=0 && mid<mList->size()){
+            pid=items.at(5).toInt(&check);
+            if(check){
+                QList<int> info;
+                get_marker_info(mList->at(mid), info);
+                QList<int>::iterator iter=info.begin();
+                iter++;
+                while(iter!=info.end()){
+                    if(*iter == pid){
+                        info.erase(iter);
+                    }else{
+                        iter++;
+                    }
+                }
+                update_marker_info(mList->at(mid), info);
+            }
+            qDebug()<<mid<<":"<<pid;
+        }
+
+        list_link->takeItem(list_link->currentRow());
+    }
+
+    update3DView();
+}
+
+LandmarkList * assemble_neuron_live_dialog::getMarkerList()
+{
+    V3dR_MainWindow * _3dwin = check3DWindow();
+    LandmarkList * mList = callback->getHandleLandmarkList_Any3DViewer(_3dwin);
+    if(mList->size()<=0){
+        return mList;
+    }
+
+    double dis;
+    QVector<V3DLONG> pidList, tidList, midList;
+    int nonetip_count=0, cantip_count=0;
+    for(int i=0; i<mList->size(); i++){ //find new markers
+        QList<int> info;
+        get_marker_info(mList->at(i),info);
+        if(nodes.contains(info.at(0)))
+            continue;
+        V3DLONG pid=(*nodes.begin())->n;
+        double mdis=NTDIS(**(nodes.begin()),mList->at(i));
+
+        for(NodeIter iter = nodes.begin(); iter != nodes.end(); iter++){
+            NOI* node = iter.value();
+            dis=NTDIS(*node,mList->at(i));
+            if(dis<mdis){
+                mdis=dis;
+                pid=node->n;
+            }
+        }
+
+        //to-do, search tips
+        V3DLONG tid=pid;
+        //tid=matchfunc->search_tip0(nid);
+//        if(tid!=pid){
+//            nonetip_count++;
+//            if(tid>=0) cantip_count++;
+//        }
+
+        pidList.append(pid);
+        tidList.append(tid);
+        midList.append(i);
+    }
+    if(cantip_count>0){
+        QString str_msg = QString::number(pidList.size()) + " new markers were identified.\n" +
+                QString::number(nonetip_count) + " of them are not eligible border tips.\n" +
+                QString::number(cantip_count) + " of them can be corrected.\n Do you want to correct it?";
+        QMessageBox::StandardButton reply;
+        reply = QMessageBox::question(this, "Match Border Tips", str_msg,
+                                        QMessageBox::Yes|QMessageBox::No);
+        if(reply==QMessageBox::Yes){
+            for(int i=0; i<pidList.size(); i++){
+                pidList[i]=tidList.at(i);
+            }
+        }
+    }
+
+    //update
+    LocationSimple *p = 0;
+    for(int j=0; j<pidList.size(); j++){
+        int i=midList.at(j);
+        V3DLONG pid=pidList.at(j);
+        p = (LocationSimple *)&(mList->at(i));
+        p->name=QString::number(pid).toStdString();
+        p->comments="";
+        p->color.r = 0;
+        p->color.g = 128;
+        p->color.b = 0;
+        p->x=nodes[pid]->x;
+        p->y=nodes[pid]->y;
+        p->z=nodes[pid]->z;
+    }
+
+    callback->update_3DViewer(_3dwin);
+    View3DControl * _3dcontrol = callback->getView3DControl_Any3DViewer(_3dwin);
+    if(_3dcontrol){
+        _3dcontrol->updateLandmark();
+    }
+
+    return mList;
+}
+
 void assemble_neuron_live_dialog::setColor(int i)
 {
     updateColor();
 }
+
 
 void assemble_neuron_live_dialog::updateDisplay()
 {
@@ -205,6 +447,18 @@ void assemble_neuron_live_dialog::updateDisplay()
     }
 
     update3DWindow();
+}
+
+void assemble_neuron_live_dialog::update3DView()
+{
+    V3dR_MainWindow * _3dwin = check3DWindow();
+    if(_3dwin){
+        callback->update_3DViewer(_3dwin);
+        View3DControl * _3dcontrol = callback->getView3DControl_Any3DViewer(_3dwin);
+        if(_3dcontrol){
+            _3dcontrol->updateLandmark();
+        }
+    }
 }
 
 v3dhandle assemble_neuron_live_dialog::updateImageWindow()
@@ -372,4 +626,97 @@ void assemble_neuron_live_dialog::updateColor()
 void assemble_neuron_live_dialog::updateROIWindow()
 {
 
+}
+
+//~~~~~~~~~~pair dialog~~~~~~~~~~~
+pair_marker_dialog::pair_marker_dialog(LandmarkList * mList,QWidget *parent)
+{
+    QGridLayout * layout = new QGridLayout(this);
+
+    QLabel *label1= new QLabel("Marker 1:");
+    QLabel *label2= new QLabel("Marker 2:");
+    list1 = new QListWidget();
+    list2 = new QListWidget();
+    btn_yes = new QPushButton("Pair Select");
+    btn_no = new QPushButton("Cancel");
+
+    layout->addWidget(label1, 0,0,1,1);
+    layout->addWidget(label2, 0,1,1,1);
+    layout->addWidget(list1, 1,0,1,1);
+    layout->addWidget(list2, 1,1,1,1);
+    layout->addWidget(btn_yes, 2,0,1,1);
+    layout->addWidget(btn_no, 2,1,1,1);
+
+    for(V3DLONG i=0; i<mList->size(); i++){
+        QList<int> info;
+        get_marker_info(mList->at(i), info);
+        QString tmp="Marker "+QString::number(i+1)+"; Node "+QString::number(info.at(0));
+        list1->addItem(tmp);
+        list2->addItem(tmp);
+    }
+
+    if(mList->size()>0){
+        list1->setCurrentRow(0);
+        list2->setCurrentRow(0);
+    }
+
+    connect(btn_yes,SIGNAL(clicked()),this,SLOT(accept()));
+    connect(btn_no,SIGNAL(clicked()),this,SLOT(reject()));
+}
+
+//~~~~~~~~~~functions~~~~~~~~~~~~~
+
+void update_marker_info(const LocationSimple& mk, QList<int> & info) //info[0]=node id, info[i]=point id to connect
+{
+    LocationSimple *p;
+    p = (LocationSimple *)&mk;
+    QString tmp;
+    for(int i=1; i<info.size(); i++)
+        tmp+=QString::number(info.at(i)) + " ";
+    p->comments=tmp.toStdString();
+    if(info.size()>0)
+        p->name=QString::number(info.at(0)).toStdString();
+    else
+        p->name="-1";
+}
+
+void update_marker_info(const LocationSimple& mk, QList<int> & info, int* color) //info[0]=neuron id, info[1]=point id, info[2]=matching marker, info[3]=marker name/id
+{
+    LocationSimple *p;
+    p = (LocationSimple *)&mk;
+    QString tmp;
+    for(int i=1; i<info.size(); i++)
+        tmp+=QString::number(info.at(i)) + " ";
+    p->comments=tmp.toStdString();
+    if(info.size()>0)
+        p->name=QString::number(info.at(0)).toStdString();
+    else
+        p->name="-1";
+    p->color.r = color[0];
+    p->color.g = color[1];
+    p->color.b = color[2];
+}
+
+bool get_marker_info(const LocationSimple& mk, QList<int> & info) //info[0]=neuron id, info[1]=point id, info[2]=matching marker, info[3]=marker name
+{
+    info.clear();
+    info.push_back(-1);
+    QString tmp;
+    bool check;
+    int val;
+    tmp=QString::fromStdString(mk.name);
+    val=tmp.toInt(&check, 10);
+    if(!check)
+        return false;
+    else
+        info[0]=val;
+    tmp=QString::fromStdString(mk.comments);
+    QStringList items = tmp.split(" ", QString::SkipEmptyParts);
+    for(int i=0; i<items.size(); i++){
+        val=items[i].toInt(&check, 10);
+        if(check)
+            info.push_back(val);
+    }
+
+    return true;
 }
--- hackathon/hanbo/neuron_assemble_live/assemble_neuron_live_dialog.h
+++ hackathon/hanbo/neuron_assemble_live/assemble_neuron_live_dialog.h
@@ -23,6 +23,8 @@ typedef struct NOI{
     QSet<NOI *> conn; //connections from NOI
 }NOI;
 
+typedef QHash<V3DLONG, NOI*>::iterator NodeIter;
+
 class assemble_neuron_live_dialog : public QDialog
 {
     Q_OBJECT
@@ -36,21 +38,27 @@ private:
     V3DLONG noffset;
     Image4DSimple * p_img4d;
     
-    QPushButton * btn_link, *btn_loop, *btn_manuallink, *btn_connect,
+    QPushButton * btn_link, *btn_loop, *btn_manuallink, *btn_deletelink, *btn_connect,
         *btn_syncmarker, *btn_break, * btn_save, * btn_quit, * btn_colorloop;
     QTabWidget * tab;
     QListWidget * list_marker, * list_link;
     QComboBox * cb_loop, * cb_color;
+    QCheckBox * check_loop;
 
 signals:
     
 public slots:
     void setColor(int i);
+    void syncMarker();
+    void pairMarker();
+    void delPair();
 
 private:
     void creat(QWidget *parent);
     void initNeuron(QList<NeuronTree> &ntList_in);
+    LandmarkList * getMarkerList();
     void updateDisplay();
+    void update3DView();
     v3dhandle getImageWindow();
     v3dhandle checkImageWindow(); //if window not opened, open it. return the handle
     v3dhandle updateImageWindow(); //
@@ -60,5 +68,22 @@ private:
     void updateROIWindow();
     void updateColor();
 };
+
+class pair_marker_dialog : public QDialog
+{
+    Q_OBJECT
+public:
+    pair_marker_dialog(LandmarkList * mList,QWidget *parent = 0);
+    QListWidget * list1, * list2;
+    QPushButton * btn_yes, *btn_no;
+};
+
+#define NTDIS(a,b) (((a).x-(b).x)*((a).x-(b).x)+((a).y-(b).y)*((a).y-(b).y)+((a).z-(b).z)*((a).z-(b).z))
+#define NTDOT(a,b) ((a).x*(b).x+(a).y*(b).y+(a).z*(b).z)
+#define NTNORM(a) (sqrt((a).x*(a).x+(a).y*(a).y+(a).z*(a).z))
+
+void update_marker_info(const LocationSimple& mk, QList<int>& info); //info[0]=point id, info[0]=matching point id
+void update_marker_info(const LocationSimple& mk, QList<int>& info, int* color);
+bool get_marker_info(const LocationSimple& mk, QList<int>& info);
 
 #endif // ASSEMBEL_NEURON_LIVE_DIALOG_H
