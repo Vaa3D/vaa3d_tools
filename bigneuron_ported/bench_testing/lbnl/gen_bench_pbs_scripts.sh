8a249416bb4a2e52b69a951135e437fbaf115dcc
Zhi Zhou <alan.zhizhou@gmail.com>
2015-08-18 16:31:08 +0000

    add two scripts to generate text jobs and pbs script

---
+++ bigneuron_ported/bench_testing/lbnl/gen_bench_pbs_scripts.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+#This is a shell program to batch reconstruct images using 21 different methods.
+#
+
+function write_psb_script {
+
+  outputScript=$1;
+  inputfolder=$2;
+  exefilename=$3;
+  jobnumbers=$4;	
+  echo "#PBS -l walltime=3:00:00" >> $outputScript;
+  echo "#PBS -l nodes=30" >> $outputScript;
+  echo "#PBS -q regular" >> $outputScript;
+
+  echo "cd $inputfolder" >>  $outputScript;
+  echo "aprun -n $jobnumbers  ./$exefilename" >>  $outputScript;
+}
+
+				
+#copy the names
+jobnumbers=$1
+jobScriptFile=$2
+inputexefolder=$3
+exefilename=$4
+
+
+
+#generate the batch script configuration
+if [ -f $jobScriptFile ]; then
+  rm $jobScriptFile;
+fi;
+
+write_psb_script $jobScriptFile $inputexefolder $exefilename $jobnumbers
+
