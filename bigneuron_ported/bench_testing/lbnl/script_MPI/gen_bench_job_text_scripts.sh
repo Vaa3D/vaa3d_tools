ea5ee6d6d9bb6d2273233bcfd9b47097ec6926d3
Zhi Zhou <alan.zhizhou@gmail.com>
2015-08-24 17:55:02 +0000

    fixed a typo in the script for 24th method

--- bigneuron_ported/bench_testing/lbnl/script_MPI/gen_bench_job_text_scripts.sh
+++ bigneuron_ported/bench_testing/lbnl/script_MPI/gen_bench_job_text_scripts.sh
@@ -164,7 +164,7 @@ function write_neuron_tracing_command {
 #RegMST
 
   if [ $METHOD == "24" -o $METHOD == "-1" ]; then
-    echo "./start_vaa3d.sh -x RegMST -f tracing_func -i $inimgfileTracing -p $vaa3dProgramPath/filter_banks/oof_fb_3d_scale_1_2_3_5_size_13_weigths_cpd_rank_49.txt $vaa3dProgramPath/filter_banks/oof_fb_3d_scale_1_2_3_5_size_13_weigths_cpd_rank_49.txt $vaa3dProgramPath/filter_banks/proto_filter_AC_lap_633_822_sep_cpd_rank_49.txt $vaa3dProgramPath/filter_banks/proto_filter_AC_lap_633_822_weigths_cpd_rank_49.txt 1 2$vaa3dProgramPath/trained_models/model_S/Regressor_ac_0.cfg $vaa3dProgramPath/trained_models/model_S/Regressor_ac_1.cfg 21 200 ;mv  $inimgfileTracing*_tubularity_model_S.v3draw_MST_Tracing_Ws_21_th_200.swc $finalfileFolder ;rm  $inimgfileTracing*_tubularity_model_S.v3draw" >> $outputScript; 
+    echo "./start_vaa3d.sh -x RegMST -f tracing_func -i $inimgfileTracing -p $vaa3dProgramPath/filter_banks/oof_fb_3d_scale_1_2_3_5_size_13_weigths_cpd_rank_49.txt $vaa3dProgramPath/filter_banks/oof_fb_3d_scale_1_2_3_5_size_13_weigths_cpd_rank_49.txt $vaa3dProgramPath/filter_banks/proto_filter_AC_lap_633_822_sep_cpd_rank_49.txt $vaa3dProgramPath/filter_banks/proto_filter_AC_lap_633_822_weigths_cpd_rank_49.txt 1 2 $vaa3dProgramPath/trained_models/model_S/Regressor_ac_0.cfg $vaa3dProgramPath/trained_models/model_S/Regressor_ac_1.cfg 21 200 ;mv  $inimgfileTracing*_tubularity_model_S.v3draw_MST_Tracing_Ws_21_th_200.swc $finalfileFolder ;rm  $inimgfileTracing*_tubularity_model_S.v3draw" >> $outputScript; 
   fi;
 }
 
