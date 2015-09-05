7c0a8877afad21b715f21ae3154046a573545827
Zhihao Zheng <zhihaozh@brandeis.edu>
2015-09-05 04:24:04 +0000

    update matlab so

--- bigneuron_ported/pengyu_zhihao/3DTraceSWC/matlab_lib/libvaa_port_test.h
+++ bigneuron_ported/pengyu_zhihao/3DTraceSWC/matlab_lib/libvaa_port_test.h
@@ -1,6 +1,6 @@
 /*
  * MATLAB Compiler: 6.0 (R2015a)
- * Date: Tue Sep  1 11:34:02 2015
+ * Date: Sat Sep  5 00:10:10 2015
  * Arguments: "-B" "macro_default" "-B" "csharedlib:libvaa_port_test" "-W"
  * "lib:libvaa_port_test" "-T" "link:lib" "vaa3d_trace3D" 
  */
