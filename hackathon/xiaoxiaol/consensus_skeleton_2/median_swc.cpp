030e0663722511cea6a614be255bb12ad5a90bfe
Xiaoxiao Liu <liuxiaoxiao@gmail.com>
2015-09-14 21:24:34 +0000

    WIP: include all three neuron distance metrics.

--- hackathon/xiaoxiaol/consensus_skeleton_2/median_swc.cpp
+++ hackathon/xiaoxiaol/consensus_skeleton_2/median_swc.cpp
@@ -4,25 +4,66 @@
 #include  <iostream>
 
 int median_swc(vector<NeuronTree> nt_list){
-    int idx = -1;
+    int idx1 = -1;
+    int idx2 = -1;
+    int idx3 = -1;
     //calculate pair-wise distance, pick the one has the min sum distance
-    double min_dis_sum = DBL_MAX;
+    double dis_sum_1[nt_list.size()];
+    double dis_sum_2[nt_list.size()];
+    double dis_sum_3[nt_list.size()];
+    for (int i = 0; i < nt_list.size(); i++){
+        dis_sum_1[i] =0 ;
+        dis_sum_2[i] =0 ;
+        dis_sum_3[i] =0 ;
+    }
+
     for (int i = 0; i < nt_list.size()-1; i++){
-        double dis_sum = 0;
         for (int j = i+1; j < nt_list.size(); j++)
         {
-            cout <<"\nComputing neuron distance between " <<i<<"  and "<<j <<":"<<endl;
+
             NeuronDistSimple tmp_score = neuron_score_rounding_nearest_neighbor(&nt_list[i], &nt_list[j],false);
-            dis_sum += tmp_score.dist_allnodes;
+            cout <<"\n\nComputing neuron distance between " <<i<<"  and "<<j <<":"<<endl;
+            cout<<"entire-structure-average = "<<tmp_score.dist_allnodes <<endl;
+            cout<<"differen-structure-average = "<<tmp_score.dist_apartnodes<<endl;
+            cout<<"percent of different-structure = "<<tmp_score.percent_apartnodes<<endl<<endl;
+
+            dis_sum_1[i] += tmp_score.dist_allnodes;
+            dis_sum_1[j] += tmp_score.dist_allnodes;
+
+            dis_sum_2[i] += tmp_score.dist_apartnodes;
+            dis_sum_2[j] += tmp_score.dist_apartnodes;
+
+            dis_sum_3[i] += tmp_score.dist_max;
+            dis_sum_3[j] += tmp_score.dist_max;
         }
-        if (min_dis_sum > dis_sum)
-        {
-            min_dis_sum = dis_sum;
-            idx = i;
-        }
+    }
+
+    double min_dis_sum_1 = DBL_MAX;
+    double min_dis_sum_2 = DBL_MAX;
+    double min_dis_sum_3 = DBL_MAX;
+    for (int i = 0; i < nt_list.size(); i++){
+       if (dis_sum_1[i] < min_dis_sum_1)
+       {
+               idx1 = i;
+               min_dis_sum_1 = dis_sum_1[i];
+       }
+       if (dis_sum_2[i] < min_dis_sum_2)
+       {
+               idx2 = i;
+               min_dis_sum_2 = dis_sum_2[i];
+       }
+       if (dis_sum_3[i] < min_dis_sum_3)
+       {
+               idx3 = i;
+               min_dis_sum_3 = dis_sum_3[i];
+       }
 
     }
-    return idx;
+    cout<<"Min total entire-structure-average = "<< min_dis_sum_1<< ", median swc id:" <<idx1 << endl;
+    cout<<"Min total differen-structure-average = "<<min_dis_sum_2<< ", median swc id:" <<idx2 << endl;
+    cout<<"Min total  percent of different-structure ="<< min_dis_sum_3 << ", median swc id:" << idx3 << endl;
+
+    return idx1;
 }
 
 
@@ -31,6 +72,9 @@ int median_swc(vector<NeuronTree> nt_list){
 NeuronTree average_node_position( NeuronTree median_neuron,vector<NeuronTree> nt_list)
 {
      NeuronTree median_adjusted;
+     //for (int i = 0; i <median_neurn)
+
+
 
      return median_adjusted;
 }
