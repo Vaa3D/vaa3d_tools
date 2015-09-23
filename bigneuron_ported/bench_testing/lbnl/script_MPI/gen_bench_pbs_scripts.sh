caafc011979f2c0b79012dd398e1012ce11e4a9b
Zhi Zhou <alan.zhizhou@gmail.com>
2015-09-23 18:40:39 +0000

    add jobpernode option on edison

--- bigneuron_ported/bench_testing/lbnl/script_MPI/gen_bench_pbs_scripts.sh
+++ bigneuron_ported/bench_testing/lbnl/script_MPI/gen_bench_pbs_scripts.sh
@@ -9,14 +9,21 @@ function write_psb_script {
   inputfolder=$2;
   exefilename=$3;
   jobnumbers=$4;
-  nodenumbers=$5;	
+  nodenumbers=$5;
+  jobpernode=$6;
+	
   echo "#PBS -l walltime=1:00:00" >> $outputScript;
   echo "#PBS -l nodes=$nodenumbers" >> $outputScript;
  # echo "#PBS -l mppwidth=1920" >> $outputScript;
   echo "#PBS -q regular" >> $outputScript;
 
   echo "cd $inputfolder" >>  $outputScript;
-  echo "aprun -n $jobnumbers ./$exefilename" >>  $outputScript;
+  if [ $jobpernode == "-1" ]; then
+        echo "aprun -n $jobnumbers ./$exefilename" >>  $outputScript;
+  else
+        echo "aprun -n $jobnumbers -N $jobpernode -S $jobpernode  ./$exefilename" >>  $outputScript;
+  fi
+
 }
 
 				
@@ -27,6 +34,12 @@ jobScriptFile=$3
 inputexefolder=$4
 exefilename=$5
 
+if [ $# -ge 6 ]; then
+        jobpernode=$6
+else
+        jobpernode="-1"
+fi
+
 
 
 #generate the batch script configuration
@@ -34,5 +47,5 @@ if [ -f $jobScriptFile ]; then
   rm $jobScriptFile;
 fi;
 
-write_psb_script $jobScriptFile $inputexefolder $exefilename $jobnumbers $nodenumbers
+write_psb_script $jobScriptFile $inputexefolder $exefilename $jobnumbers $nodenumbers $jobpernode
 
