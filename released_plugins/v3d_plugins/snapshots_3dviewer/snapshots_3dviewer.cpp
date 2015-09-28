4614e24b425c8f6968d204e788b842f15c6b9e7e
Xiaoxiao Liu <liuxiaoxiao@gmail.com>
2015-09-28 06:27:05 +0000

    Add user inputs for snapshots_3dviewer plugin.

--- released_plugins/v3d_plugins/snapshots_3dviewer/snapshots_3dviewer.cpp
+++ released_plugins/v3d_plugins/snapshots_3dviewer/snapshots_3dviewer.cpp
@@ -13,7 +13,6 @@ void snapShots3Dviewer(V3DPluginCallback2 & v3d, QWidget * parent);
 
 //plugin funcs
 const QString title = "Saving Snapshots From 3D Viewer";
-controlPanel* controlPanel::m_pLookPanel = 0;
 QStringList SnapShots_3Dviewer::menulist() const
 {
 	return QStringList()
@@ -30,80 +29,6 @@ void SnapShots_3Dviewer::domenu(const QString & menu_name,	V3DPluginCallback2 & 
 
 void SnapShots_3Dviewer(V3DPluginCallback2 & v3d, QWidget * parent)
 {
-}
-
-controlPanel::controlPanel(V3DPluginCallback2 &_v3d, QWidget *parent) :
-	QDialog(parent), m_v3d(_v3d)
-{
-	m_pLookPanel = this;
-	m_lframeind = 0;
-
-	m_pLineEdit_fps = new QLineEdit(QObject::tr("10"));
-	m_pLineEdit_filepath = new QLineEdit();
-	QPushButton *pPushButton_start = new QPushButton(QObject::tr("start"));
-	QPushButton *pPushButton_stop = new QPushButton(QObject::tr("stop"));
-	QPushButton *pPushButton_openFileDlg_output = new QPushButton(QObject::tr("..."));
-
-	QGridLayout *pGridLayout = new QGridLayout();
-	pGridLayout->addWidget(new QLabel(QObject::tr("choose fps:")),1,1);
-	pGridLayout->addWidget(m_pLineEdit_fps,1,2);
-	pGridLayout->addWidget(new QLabel(QObject::tr("choose output dir:")),2,1);
-	pGridLayout->addWidget(m_pLineEdit_filepath,2,2);
-	pGridLayout->addWidget(pPushButton_openFileDlg_output,2,3);
-	pGridLayout->addWidget(pPushButton_start,3,1);
-	pGridLayout->addWidget(pPushButton_stop,3,2);
-
-	setLayout(pGridLayout);
-	setWindowTitle(QString("make movie"));
-
-	connect(pPushButton_start, SIGNAL(clicked()), this, SLOT(_slot_start()));
-	connect(pPushButton_stop, SIGNAL(clicked()), this, SLOT(_slot_stop()));
-	connect(pPushButton_openFileDlg_output, SIGNAL(clicked()), this, SLOT(_slots_openFileDlg_output()));
-
-	m_pTimer = new QTimer(this);
-	connect(m_pTimer, SIGNAL(timeout()), this, SLOT(_slot_timerupdate()));
-}
-controlPanel::~controlPanel()
-{
-	m_pLookPanel = 0;
-}
-
-void controlPanel::_slot_start()
-{
-	if(m_pLineEdit_filepath->text().isEmpty() || !QDir(m_pLineEdit_filepath->text()).exists())
-	{
-		v3d_msg("Output dir is empty or do not exist!");
-		return;
-	}
-
-	long interval = 1.0 / m_pLineEdit_fps->text().toLong() * 1000;
-	m_pTimer->start(interval);
-}
-void controlPanel::_slot_stop()
-{
-	m_pTimer->stop();
-}
-void controlPanel::_slots_openFileDlg_output()
-{
-	QFileDialog d(this);
-	d.setWindowTitle(tr("Choose output dir:"));
-	d.setFileMode(QFileDialog::Directory);
-	if(d.exec())
-	{
-		QString selectedFile=(d.selectedFiles())[0];
-		m_pLineEdit_filepath->setText(selectedFile);
-	}
-}
-void controlPanel::_slot_timerupdate()
-{
-	QString outputdir = m_pLineEdit_filepath->text();
-
-	v3dhandle curwin = m_v3d.currentImageWindow();
-	QString BMPfilename = outputdir + QString("/%1").arg(m_lframeind);
-	m_v3d.screenShot3DWindow(curwin, BMPfilename);
-
-	printf("%d\n", m_lframeind);
-	m_lframeind++;
 }
 
 void snapShots3Dviewer(V3DPluginCallback2 & v3d, QWidget * parent)
@@ -125,9 +50,20 @@ void snapShots3Dviewer(V3DPluginCallback2 & v3d, QWidget * parent)
     QString(QObject::tr("Choose the output directory, where the snapshots would be saved.")));
 
 
-    float x_rot = 90;
-    float y_rot = 0 ;
-    float z_rot = 90;
+    float x_rot = QInputDialog::getDouble(parent, "Roation about x axis",
+                                 "Rotation about x :",
+                                 90, -180, 180);
+
+
+    float  y_rot = QInputDialog::getDouble(parent, "Roation about y axis",
+                                 "Rotation about y :",
+                                0, -180, 180);
+
+
+
+    float z_rot = QInputDialog::getDouble(parent, "Roation about z axis",
+                                 "Rotation about z :",
+                                 90, -180, 180);
 
     for(int i = 0; i <cc.swc_file_list.size(); i++)
     {
