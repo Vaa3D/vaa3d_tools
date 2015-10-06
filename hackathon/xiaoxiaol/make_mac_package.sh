44fd95bf326ba73d20286b0196656aabe4402025
Xiaoxiao Liu <liuxiaoxiao@gmail.com>
2015-10-06 06:13:23 +0000

    update mac packaging scripts.

--- hackathon/xiaoxiaol/make_mac_package.sh
+++ hackathon/xiaoxiaol/make_mac_package.sh
@@ -23,19 +23,29 @@ rm -rf plugins
 cp -r  ~/work/v3d/v3d_external/bin/plugins ./
 
 cd ~/work/bin/vaa3d/
-make package
-rm v3d/Mac_Fat/Vaa3d.app/Contents/MacOS/plugins/image_stitching/istitch/libimageStitch.dylib
-rm v3d/Mac_Fat/Vaa3d.app/Contents/MacOS/plugins/image_segmentation/lobeseger/liblobeseg.dylib
-rm v3d/Mac_Fat/Vaa3d.app/Contents/MacOS/plugins/refExtract/librefExtract.dylib
-rm v3d/Mac_Fat/Vaa3d.app/Contents/MacOS/plugins/image_registration/SSD_registration/libplugin_PQ_imagereg.dylib
-rm v3d/Mac_Fat/Vaa3d.app/Contents/MacOS/plugins/image_blending/blend_multiscanstacks/libblend_multiscanstacks.dylib
-rm v3d/Mac_Fat/Vaa3d.app/Contents/MacOS/plugins/FlyWorkstation_utilities/clonalSelect/libclonalselect.dylib
-rm v3d/Mac_Fat/Vaa3d.app/Contents/MacOS/plugins/image_stitching/ifusion/libifusion.dylib
+make DeployPlugins
+
+
+#make package
+#rm v3d/Mac_Fat/Vaa3d.app/Contents/MacOS/plugins/image_stitching/istitch/libimageStitch.dylib
+#rm v3d/Mac_Fat/Vaa3d.app/Contents/MacOS/plugins/image_segmentation/lobeseger/liblobeseg.dylib
+#rm v3d/Mac_Fat/Vaa3d.app/Contents/MacOS/plugins/refExtract/librefExtract.dylib
+#rm v3d/Mac_Fat/Vaa3d.app/Contents/MacOS/plugins/image_registration/SSD_registration/libplugin_PQ_imagereg.dylib
+#rm v3d/Mac_Fat/Vaa3d.app/Contents/MacOS/plugins/image_blending/blend_multiscanstacks/libblend_multiscanstacks.dylib
+#rm v3d/Mac_Fat/Vaa3d.app/Contents/MacOS/plugins/FlyWorkstation_utilities/clonalSelect/libclonalselect.dylib
+#rm v3d/Mac_Fat/Vaa3d.app/Contents/MacOS/plugins/image_stitching/ifusion/libifusion.dylib
 
 #otool -L /Users/xiaoxiaoliu/work/bin/vaa3d/v3d/Mac_Fat/Vaa3d.app/Contents/MacOS/plugins/image_filters/anisotropic_filter/anisodiff_littlequick/libanisodiff_littlequick_debug.dylib
 
 
-# copy vaa3d executable and plugins to the template package folder
+# copy vaa3d executable and plugins to the template package folder: ~/Downloads/Vaa3d_V3.100_MacOSX10.9_64bit/
+cp -r /Users/xiaoxiaoliu/work/bin/vaa3d/v3d/Mac_Fat/Vaa3d.app ~/Downloads/Vaa3d_V3.100_MacOSX10.9_64bit/
+
+mv ~/Downloads/Vaa3d_V3.100_MacOSX10.9_64bit/Vaa3d.app/Contents/MacOS/plugins  ~/Downloads/Vaa3d_V3.100_MacOSX10.9_64bit/
+
+#run testing scripts
+cp ~/work/v3d/v3d_external/testing ~/Downloads/Vaa3d_V3.100_MacOSX10.9_64bit/
+cd testing
+./test_plugin_installation.sh
 
 
-# rm redundant  plugins in Vaa3d.app
