6e92941b451373f120cbc5554bf81b29fe93d255
Zhi Zhou <alan.zhizhou@gmail.com>
2015-10-26 18:32:57 +0000

    add apical and basal diameter information

--- hackathon/zhi/branch_order_analysis/branch_order_analysis_plugin.cpp
+++ hackathon/zhi/branch_order_analysis/branch_order_analysis_plugin.cpp
@@ -108,6 +108,9 @@ void branch_order_analysis::domenu(const QString &menu_name, V3DPluginCallback2 
         QList<NeuronSWC> list = nt.listNeuron;
         vector<MyMarker*> swc_file = readSWC_file(fileOpenName.toStdString());
 
+        int branchOrder_apical = 0;
+        int branchOrder_basal = 0;
+
         for (int i=0;i<list.size();i++)
         {
             int parent_node = getParent(i,nt);
@@ -116,23 +119,70 @@ void branch_order_analysis::domenu(const QString &menu_name, V3DPluginCallback2 
             else if (childs[parent_node].size()<=1)
                 swc_file[i]->type =  swc_file[parent_node]->type;
             else if (childs[parent_node].size()>1)
+            {
                 swc_file[i]->type =  swc_file[parent_node]->type + 1;
+            }
         }
 
-       QString fileDefaultName = fileOpenName+QString("_ordered.swc");
-       //write new SWC to file
-       QString fileSaveName = QFileDialog::getSaveFileName(0, QObject::tr("Save File"),
-               fileDefaultName,
-               QObject::tr("Supported file (*.swc)"
-                   ";;Neuron structure	(*.swc)"
-                   ));
+        for (int i=0;i<list.size();i++)
+        {
+            if(list.at(i).type == 3 && swc_file[i]->type > branchOrder_basal)
+                branchOrder_basal = swc_file[i]->type;
+            else if (list.at(i).type == 4 && swc_file[i]->type > branchOrder_apical)
+                branchOrder_apical = swc_file[i]->type;
 
-       saveSWC_file(fileSaveName.toStdString(), swc_file);
+        }
 
-	}
-	else if (menu_name == tr("menu2"))
-	{
-		v3d_msg("To be implemented.");
+    //    v3d_msg(QString("apical is %1, basal is %2").arg(branchOrder_apical).arg(branchOrder_basal));
+        double *branchapical_mean = new double[branchOrder_apical];
+        QString disp_text = "branch orders vs. diameters (apical)\n\n";
+        for (int j = 0; j < branchOrder_apical; j++)
+        {
+            int count = 0;
+            double radius_sum = 0;
+            for (int i = 0;i < list.size();i++)
+            {
+                if(list.at(i).type == 4 &&  swc_file[i]->type == j+1)
+                {
+                    double radius_node = list.at(i).radius;
+                    radius_sum += radius_node;
+                    count++;
+                }
+            }
+            branchapical_mean[j] = 2*radius_sum/count;
+            disp_text += "Branch order = " + QString::number (j+1)  + ", average diameter = "    + QString::number(branchapical_mean[j]) + ";\n";
+        }
+
+        double *branchbasal_mean = new double[branchOrder_basal];
+        disp_text += "\n\n\nbranch orders vs. diameters (basal)\n\n";
+        for (int j = 0; j < branchOrder_basal; j++)
+        {
+            int count = 0;
+            double radius_sum = 0;
+            for (int i = 0;i < list.size();i++)
+            {
+                if(list.at(i).type == 3 &&  swc_file[i]->type == j+1)
+                {
+                    double radius_node = list.at(i).radius;
+                    radius_sum += radius_node;
+                    count++;
+                }
+            }
+            branchbasal_mean[j] = 2*radius_sum/count;
+            disp_text += "Branch order = " + QString::number (j+1)  + ", average diameter = "    + QString::number(branchbasal_mean[j]) + ";\n";
+        }
+
+        v3d_msg(disp_text);
+
+//       QString fileDefaultName = fileOpenName+QString("_ordered.swc");
+//       //write new SWC to file
+//       QString fileSaveName = QFileDialog::getSaveFileName(0, QObject::tr("Save File"),
+//               fileDefaultName,
+//               QObject::tr("Supported file (*.swc)"
+//                   ";;Neuron structure	(*.swc)"
+//                   ));
+
+//       saveSWC_file(fileSaveName.toStdString(), swc_file);
 	}
 	else
 	{
