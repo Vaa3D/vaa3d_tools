3e7e277b40049cdbdb3b7b9409d9f98f4ccd17e5
Zhi Zhou <alan.zhizhou@gmail.com>
2015-11-16 18:42:48 +0000

    add multi-windows display

--- released_plugins/v3d_plugins/neuron_tile_display/neuron_tile_display_plugin.cpp
+++ released_plugins/v3d_plugins/neuron_tile_display/neuron_tile_display_plugin.cpp
@@ -15,6 +15,8 @@ QStringList neuron_tile_display::menulist() const
 	return QStringList() 
 		<<tr("neuron_tile_display")
         <<tr("neuron_tile_XY")
+        <<tr("neuron_multi_windows")
+        <<tr("neuron_multi_windows_under_one_folder")
 		<<tr("about");
 }
 
@@ -24,6 +26,35 @@ QStringList neuron_tile_display::funclist() const
 		<<tr("help");
 }
 
+QStringList importFileList_addnumbersort(const QString & curFilePath, int method_code)
+{
+    QStringList myList;
+    myList.clear();
+
+    // get the image files namelist in the directory
+    QStringList imgSuffix;
+    if (method_code ==1)
+        imgSuffix<<"*.swc"<<"*.eswc"<<"*.SWC"<<"*.ESWC";
+    else if (method_code ==2)
+        imgSuffix<<"*.marker";
+
+    QDir dir(curFilePath);
+    if (!dir.exists())
+    {
+        qWarning("Cannot find the directory");
+        return myList;
+    }
+
+    foreach (QString file, dir.entryList(imgSuffix, QDir::Files, QDir::Name))
+    {
+        myList += QFileInfo(dir, file).absoluteFilePath();
+    }
+
+    // print filenames
+    foreach (QString qs, myList)  qDebug() << qs;
+
+    return myList;
+}
 void neuron_tile_display::domenu(const QString &menu_name, V3DPluginCallback2 &callback, QWidget *parent)
 {
 	if (menu_name == tr("neuron_tile_display"))
@@ -33,6 +64,121 @@ void neuron_tile_display::domenu(const QString &menu_name, V3DPluginCallback2 &c
     else if (menu_name == tr("neuron_tile_XY"))
     {
         doxytile(callback, parent);
+    }
+    else if (menu_name == tr("neuron_multi_windows"))
+    {
+        QStringList swcList;
+        swcList = QFileDialog::getOpenFileNames(0, QObject::tr("Open File"),
+                "",
+                QObject::tr("Supported file (*.swc *.eswc)"
+                    ";;Neuron structure	(*.swc)"
+                    ";;Extended neuron structure (*.eswc)"
+                    ));
+        if(swcList.isEmpty())
+            return;
+
+        bool ok1, ok2, ok3, ok4;
+        unsigned int col=1, row=1, xRez=1, yRez=1;
+        col = QInputDialog::getInteger(parent, "",
+                                      "number of displayed windows per row:",
+                                      1, 1, 1000, 1, &ok1);
+
+        if(ok1)
+        {
+            row = QInputDialog::getInteger(parent, "",
+                                          "number of displayed windows per column:",
+                                          1, 1, 1000, 1, &ok2);
+        }
+        else
+            return;
+
+        if(ok2)
+        {
+            xRez = QInputDialog::getInteger(parent, " ",
+                                          "xRez:",
+                                          1, 1, 10000, 1, &ok3);
+        }
+        else
+            return;
+
+        if(ok3)
+        {
+            yRez = QInputDialog::getInteger(parent, " ",
+                                          "yRez:",
+                                          1, 1, 10000, 1, &ok4);
+        }
+        else
+            return;
+
+
+        V3dR_MainWindow * new3DWindow = NULL;
+        int offsety = -1;
+        for(V3DLONG i = 0; i < swcList.size(); i++)
+        {
+            QString curPathSWC = swcList.at(i);
+            new3DWindow = callback.open3DViewerForSingleSurfaceFile(curPathSWC);
+            if( (i%col)*xRez ==0)
+                offsety++;
+            callback.moveWindow(new3DWindow,(i%col)*xRez,offsety*yRez);
+            callback.resizeWindow(new3DWindow,xRez,yRez);
+//            v3d_msg(QString("offsetx: %1, offsety:%2").arg((i%col)*xRez).arg(offsety*yRez));
+        }
+
+    }
+    else if (menu_name == tr("neuron_multi_windows_under_one_folder"))
+    {
+        QString m_InputfolderName = QFileDialog::getExistingDirectory(parent, QObject::tr("Choose the directory including all swc files "),
+                                                                      QDir::currentPath(),
+                                                                      QFileDialog::ShowDirsOnly);
+
+
+        bool ok1, ok2, ok3, ok4;
+        unsigned int col=1, row=1, xRez=1, yRez=1;
+        col = QInputDialog::getInteger(parent, "",
+                                      "number of displayed windows per row:",
+                                      1, 1, 1000, 1, &ok1);
+
+        if(ok1)
+        {
+            row = QInputDialog::getInteger(parent, "",
+                                          "number of displayed windows per column:",
+                                          1, 1, 1000, 1, &ok2);
+        }
+        else
+            return;
+
+        if(ok2)
+        {
+            xRez = QInputDialog::getInteger(parent, " ",
+                                          "xRez:",
+                                          1, 1, 10000, 1, &ok3);
+        }
+        else
+            return;
+
+        if(ok3)
+        {
+            yRez = QInputDialog::getInteger(parent, " ",
+                                          "yRez:",
+                                          1, 1, 10000, 1, &ok4);
+        }
+        else
+            return;
+        QStringList swcList = importFileList_addnumbersort(m_InputfolderName, 1);
+
+        V3dR_MainWindow * new3DWindow = NULL;
+        int offsety = -1;
+        for(V3DLONG i = 0; i < swcList.size(); i++)
+        {
+
+            QString curPathSWC = swcList.at(i);
+            new3DWindow = callback.open3DViewerForSingleSurfaceFile(curPathSWC);
+            if( (i%col)*xRez ==0)
+                offsety++;
+            callback.moveWindow(new3DWindow,(i%col)*xRez,offsety*yRez);
+            callback.resizeWindow(new3DWindow,xRez,yRez);
+        }
+
     }
 	else
 	{
