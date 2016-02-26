73c3718d3b7a62d0407e9f53de3c635cbb64f4c1
Xiaoxiao Liu <liuxiaoxiao@gmail.com>
2016-02-26 15:54:37 -0800

    BugFix: Mismatched segment type in the final csv report (profile_swc).

--- released_plugins/v3d_plugins/neuron_image_profiling/profile_swc.cpp
+++ released_plugins/v3d_plugins/neuron_image_profiling/profile_swc.cpp
@@ -690,16 +690,16 @@ QList<IMAGE_METRICS> intensity_profile(NeuronTree neuronTree, Image4DSimple * im
     QList <QList<NeuronSWC> > neuronSWC_lists;
     std::map<int,int> mapTypeToId;
     int pre_type = neuronSWCs[0].type;
-    int count = 0;
-    int j = pre_type;
+    int count = 0;//numer fo different segment types
+    int current_type = pre_type;
     for (V3DLONG i = 0 ; i < neuronSWCs.size() ; i++)
     {
-        j = neuronSWCs[i].type;
+        current_type = neuronSWCs[i].type;
 
-        if (j != pre_type)
+        if (current_type != pre_type)
         {// not in map yet, add into map
             if (!neuronSWC_sameType.isEmpty()){
-                if ( mapTypeToId.count(j) == 0   )
+                if ( mapTypeToId.count(pre_type) == 0   )
                 {
                     neuronSWC_lists.push_back(neuronSWC_sameType);
                     mapTypeToId[pre_type] = count;
@@ -709,41 +709,41 @@ QList<IMAGE_METRICS> intensity_profile(NeuronTree neuronTree, Image4DSimple * im
                 }
                 else
                 {
-                    int jj = mapTypeToId[j];
-
+                    int jj = mapTypeToId[current_type];
                     neuronSWC_lists[jj].append(neuronSWC_sameType);;
                 }
                 neuronSWC_sameType.clear();
             }
 
-            pre_type = j;
+            pre_type = current_type;
 
         }
 
         neuronSWC_sameType.push_back(neuronSWCs.at(i));
+
 
     }
 
 
     //at the end
     if (!neuronSWC_sameType.isEmpty()){
-        if ( mapTypeToId.count(j) == 0)
+        if ( mapTypeToId.count(current_type) == 0)
         {
             neuronSWC_lists.push_back(neuronSWC_sameType);
-            mapTypeToId[j] = count;
-           // cout<<"last: map "<<j<<" to "<<count<<endl;
+            mapTypeToId[current_type] = count;
+           // cout<<"last: map "<<current_type<<" to "<<count<<endl;
             count++ ;
         }
         else
         {
-            int jj = mapTypeToId[j];
+            int jj = mapTypeToId[current_type];
             neuronSWC_lists[jj].append(neuronSWC_sameType);;
         }
     }
 
     //collect metrics
     cout<< "Profile " << count<< " different segment types" <<endl;
-    for (j = 0; j < count; j++)
+    for (int j = 0; j < count; j++)
     {
         if (!neuronSWC_lists[j].isEmpty() )
         {
