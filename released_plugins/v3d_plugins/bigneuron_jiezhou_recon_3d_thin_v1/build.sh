daf65c730b5556e0a70cd18732a02d7c35337300
Zhi Zhou <alan.zhizhou@gmail.com>
2016-05-09 14:40:38 -0700

    updated build.sh in bigneuron_jiezhou_recon_3d_thin_v1 folder

--- released_plugins/v3d_plugins/bigneuron_jiezhou_recon_3d_thin_v1/build.sh
+++ released_plugins/v3d_plugins/bigneuron_jiezhou_recon_3d_thin_v1/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #snake
 if [ ! -d ../bigneuron_zz_snake_tracing/ITK_include ]; then
-	unzip ITK_include.zip		
+	unzip ../bigneuron_zz_snake_tracing/ITK_include.zip		
+	mv ITK_include ../bigneuron_zz_snake_tracing/
 fi	
 qmake
 make
