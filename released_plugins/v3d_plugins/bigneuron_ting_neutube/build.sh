ee8c9831020421541a846954c25e10ced575f073
Zhi Zhou <alan.zhizhou@gmail.com>
2016-05-09 15:26:00 -0700

    added build scrpt in neutube tracing

---
+++ released_plugins/v3d_plugins/bigneuron_ting_neutube/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#snake
+if [ ! -d NeuTu ]; then
+	wget https://www.dropbox.com/s/g2alfleyp6d9dfc/NeuTu.zip?dl=0
+	unzip NeuTu.zip		
+	cd NeuTu/neurolabi
+	sh buildlib.sh
+	cd ..
+	cd ..
+fi
+qmake
+make
+
+
+
