d8123cc08455293ab3b1a3d0ac239bb2280d9878
Xiaoxiao Liu <liuxiaoxiao@gmail.com>
2016-05-12 11:15:21 -0700

    Working on mac package release. Temperoarily disable terastitcher plugin.

--- hackathon/xiaoxiaol/make_mac_package.sh
+++ hackathon/xiaoxiaol/make_mac_package.sh
@@ -15,6 +15,7 @@ make DeployPlugins
 
 cp -r $cmake_build_dir/v3d/Mac_Fat/Vaa3d.app $package_dir/
 
+rm -rf $package_dir/plugins
 mv $package_dir/Vaa3d.app/Contents/MacOS/plugins  $package_dir/
 
 #run testing scripts
