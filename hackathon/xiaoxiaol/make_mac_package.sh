31e14b7f87d5ce79c6ae6855b61d238208d4b16b
Xiaoxiao Liu <liuxiaoxiao@gmail.com>
2016-06-10 13:22:15 -0700

    Add commens to the mac build scripts.

--- hackathon/xiaoxiaol/make_mac_package.sh
+++ hackathon/xiaoxiaol/make_mac_package.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
-cmake_build_dir=/Users/xiaoxiaoliu/work/src/v3d_external/build_macosx-x86_64
-qmake_build_dir=/Users/xiaoxiaoliu/work/v3d/v3d_external/bin
-package_dir=/Users/xiaoxiaoliu/Downloads/Vaa3d_V3.20_MacOSX10.9_64bit
+cmake_build_dir="/Users/xiaoxiaoliu/work/src/v3d_external/build_macosx-x86_64"
+qmake_build_dir="/Users/xiaoxiaoliu/work/v3d/v3d_external/bin"
+package_dir="/Users/xiaoxiaoliu/Downloads/Vaa3d_V3.20_MacOSX10.9_64bit"
 
+# use cmake to build the main v3d executable
+cd $cmake_build_dir
+make -j8
+
+# copy qmake built plugins to the cmake build older
 cd $qmake_build_dir
 find . -type d -empty -delete
 cd $cmake_build_dir/v3d/Mac_Fat/Vaa3d.app/Contents/MacOS
 rm -rf plugins
 cp -r  $qmake_build_dir/plugins ./
 
+
+# mac deploy all plugins: the change the qt dependency path
 cd $cmake_build_dir
 make DeployPlugins
 
-
+# copy the executables and plugins into the pre-existing package
 cp -r $cmake_build_dir/v3d/Mac_Fat/Vaa3d.app $package_dir/
-
 rm -rf $package_dir/plugins
 mv $package_dir/Vaa3d.app/Contents/MacOS/plugins  $package_dir/
 
