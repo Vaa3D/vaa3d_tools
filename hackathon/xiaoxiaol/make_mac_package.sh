1e1c676073378d87d6773921a5818d63e27e68bb
Xiaoxiao Liu <liuxiaoxiao@gmail.com>
2016-07-05 20:40:40 -0700

    mac package script updating.

--- hackathon/xiaoxiaol/make_mac_package.sh
+++ hackathon/xiaoxiaol/make_mac_package.sh
@@ -20,9 +20,9 @@ cd $cmake_build_dir
 make DeployPlugins
 
 # copy the executables and plugins into the pre-existing package
-cp -r $cmake_build_dir/v3d/Mac_Fat/Vaa3d.app $package_dir/
+cp -rf $cmake_build_dir/v3d/Mac_Fat/Vaa3d.app $package_dir/
 rm -rf $package_dir/plugins
-mv $package_dir/Vaa3d.app/Contents/MacOS/plugins  $package_dir/
+cp -r $package_dir/Vaa3d.app/Contents/MacOS/plugins  $package_dir/
 
 #run testing scripts
 #cp ~/work/v3d/v3d_external/testing ~/Downloads/Vaa3d_V3.100_MacOSX10.9_64bit/
