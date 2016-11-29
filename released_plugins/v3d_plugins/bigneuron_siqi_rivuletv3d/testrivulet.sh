c0e728d4ec757277f4fa6500c363047fd4e18e73
siqi <lsqshr@gmail.com>
2016-11-29 17:56:56 +1100

    Update test script

--- released_plugins/v3d_plugins/bigneuron_siqi_rivuletv3d/testrivulet.sh
+++ released_plugins/v3d_plugins/bigneuron_siqi_rivuletv3d/testrivulet.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-export V3DPATH=../../../../v3d_external
-export LD_LIBRARY_PATH=$V3DPATH/v3d_main/common_lib/lib
-export vaa3d=$V3DPATH/bin/vaa3d; # Assume vaa3d
+export VAA3DPATH=../../../../v3d_external
+export LD_LIBRARY_PATH=$VAA3DPATH/v3d_main/common_lib/lib
+export vaa3d=$VAA3DPATH/bin/vaa3d; # Assume vaa3d
 qmake;
-make -j4;
+make -j8;
 echo "Build Finish"
 
 echo "vaa3d -x Rivulet -f tracing_func -i <inimg_file> -p <channel> <threshold>"
 $vaa3d -x Rivulet -f tracing_func -i test/test.tif -o fuck.swc -p 1 1;
-# $vaa3d -v -i fuck.swc;
