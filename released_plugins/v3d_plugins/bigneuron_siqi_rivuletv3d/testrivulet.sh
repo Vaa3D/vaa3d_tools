422dfdb3f2e655f7e2984b311b352bb95e2edc9d
Siqi Liu <lsqshr@gmail.com>
2016-12-02 18:18:20 +1100

    Use v3d official neuron tracing data

--- released_plugins/v3d_plugins/bigneuron_siqi_rivuletv3d/testrivulet.sh
+++ released_plugins/v3d_plugins/bigneuron_siqi_rivuletv3d/testrivulet.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+mkdir -p test_data;
+export TESTIMGZIP=./test_data/peng-v3d-testdata.zip;
+export TESTIMG=./test_data/v3d-testdata//neuron01.tif;
+export TESTURL=https://github.com/Vaa3D/Vaa3D_Data/releases/download/data_v1.0/peng-v3d-testdata.zip;
+if [ ! -f $TESTIMG ];
+then
+  rm -rf test_data/*;
+  echo "Downloading test image from $TESTURL";
+  wget -P ./test_data/ $TESTURL;
+  unzip $TESTIMGZIP -d ./test_data;
+fi
+
 export VAA3DPATH=../../../../v3d_external
 export LD_LIBRARY_PATH=$VAA3DPATH/v3d_main/common_lib/lib
 export vaa3d=$VAA3DPATH/bin/vaa3d; # Assume vaa3d
@@ -7,4 +19,4 @@ make -j8;
 echo "Build Finish"
 
 echo "vaa3d -x Rivulet -f tracing_func -i <inimg_file> -p <channel> <threshold>"
-$vaa3d -x Rivulet -f tracing_func -i test/test.tif -o fuck.swc -p 1 1;
+$vaa3d -x Rivulet -f tracing_func -i $TESTIMG -o $TESTIMG.r2.swc -p 1 10;
