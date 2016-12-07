fa04d9dae6eb843f0fa527c56ad802b2e52f2a1a
Brian Long <brianl@alleninstitute.org>
2016-12-07 14:30:35 -0800

    S2: bump

--- hackathon/brl/s2/stackAnalyzer.cpp
+++ hackathon/brl/s2/stackAnalyzer.cpp
@@ -1527,7 +1527,7 @@ void StackAnalyzer::SubtractiveTracing(QString latestString,QString imageSaveStr
             {
                 arg_para.push_back("1");
                 arg_para.push_back("1");
-                full_plugin_name = "neuTube"; //old verson "NEUTUBE"
+                full_plugin_name = "neuTube";//old verson "NEUTUBE", new version "neuTube"
                 func_name =  "neutube_trace";
                 qDebug()<<"starting neutube";
             }
