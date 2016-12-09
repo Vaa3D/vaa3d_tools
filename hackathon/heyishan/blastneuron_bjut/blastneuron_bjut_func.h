ca5653832d36948f49ed96bcef56e8afbfd67798
helanfeixue <497174789@qq.com>
2016-12-09 11:13:50 +0800

    added a miss file

--- hackathon/heyishan/blastneuron_bjut/blastneuron_bjut_func.h
+++ hackathon/heyishan/blastneuron_bjut/blastneuron_bjut_func.h
@@ -2,10 +2,16 @@
 #define BLASTNEURON_BJUT_FUNC_H
 
 #include<v3d_interface.h>
+#include<vector>
+#include "my_surf_objs.h"
+#include "neuron_tree_align.h"
+#include"v3d_message.h"
 
-bool blast_bjut_func(const V3DPluginArgList & input, V3DPluginArgList & output);
-void sort_main();
-void blastneuron_main();
+
+bool pre_process_func(const V3DPluginArgList & input, V3DPluginArgList & output);
+bool resampling_main(const V3DPluginArgList & input, V3DPluginArgList & output);
+bool blastneuron_main(const V3DPluginArgList & input, V3DPluginArgList & output);
+bool prune_alignment(const V3DPluginArgList & input, V3DPluginArgList & output);
 void printHelp();
 
 #endif // BLASTNEURON_BJUT_FUNC_H
