ea45dfb0b1fd3fc36b0151c8e394f1e55f40a130
MKRangers <hsienchik@alleninstitute.org>
2017-01-03 18:23:14 -0800

    Tip finder modification 1

--- hackathon/MK/Tip_marker_finder/tip_marker_func.cpp
+++ hackathon/MK/Tip_marker_finder/tip_marker_func.cpp
@@ -36,8 +36,8 @@ bool SpecDialog(V3DPluginCallback2 &callback, QWidget *parent)
 
     vector<string> items;
     items.push_back("Total number of z slices in the image stack");
-    items.push_back("Number of top z slices to be included");
-    items.push_back("Number of bottom z slices to be included");
+    items.push_back("Specify the bottom plane number of the top section to look into: ");
+    items.push_back("Specify the top plane number of the bottom section to look into: ");
     CommonDialog dialog(items);
     dialog.setWindowTitle(title);
     if (dialog.exec()!=QDialog::Accepted)
@@ -45,8 +45,8 @@ bool SpecDialog(V3DPluginCallback2 &callback, QWidget *parent)
 
     int top=NULL, bottom=NULL, num=NULL;
     dialog.get_num("Total number of z slices in the image stack", num);
-    dialog.get_num("Number of top z slices to be included", top);
-    dialog.get_num("Number of bottom z slices to be included", bottom);
+    dialog.get_num("Specify the bottom plane number of the top section to look into: ", top);
+    dialog.get_num("Specify the top plane number of the bottom section to look into: ", bottom);
     if (num == NULL || top == NULL || bottom == NULL)
     {
         v3d_msg("Parameter(s) not specified. Failed to generate marker file.");
@@ -62,13 +62,17 @@ bool SpecDialog(V3DPluginCallback2 &callback, QWidget *parent)
         ImageMarker t;
         if (childs[i].size() == 0)
         {
-            z_coord = int(nt.listNeuron.at(i).z);
-            if ((z_coord > (num - top)) || (z_coord <= bottom))
+            if (nt.listNeuron.at(i).type == 3 || nt.listNeuron.at(i).type == 4)
             {
-                t.x = nt.listNeuron.at(i).x;
-                t.y = nt.listNeuron.at(i).y;
-                t.z = nt.listNeuron.at(i).z;
-                bifur_marker.append(t);
+                z_coord = int(nt.listNeuron.at(i).z);
+                if ((z_coord >= bottom) || (z_coord <= top))
+                {
+                    t.x = nt.listNeuron.at(i).x;
+                    t.y = nt.listNeuron.at(i).y;
+                    t.z = nt.listNeuron.at(i).z;
+                    bifur_marker.append(t);
+                    printf("%d\n", nt.listNeuron.at(i).type);
+                }
             }
         }
     }
