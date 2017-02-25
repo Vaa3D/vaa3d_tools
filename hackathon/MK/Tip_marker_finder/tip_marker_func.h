3c8d993079af7e85544f37d4b8de0f256ade0c24
MKRangers <hsienchik@alleninstitute.org>
2017-02-24 17:54:13 -0800

    Tip_marker_finder3.0 new UI, crash fix

--- hackathon/MK/Tip_marker_finder/tip_marker_func.h
+++ hackathon/MK/Tip_marker_finder/tip_marker_func.h
@@ -2,9 +2,34 @@
 #define TIP_MARKER_FUNC_H
 
 #include <v3d_interface.h>
+#include <qdialog.h>
+#include <qstringlist.h>
+#include "qlineedit.h"
+#include "qpushbutton.h"
 
-bool SpecDialog(V3DPluginCallback2 &callback, QWidget *parent);
-bool SpecDialog_all(V3DPluginCallback2 &callback, QWidget *parent);
+bool TipProcessor(QStringList input, int menu);
 
+namespace Ui {
+class TipMarkerFinderUI;
+}
+
+class TipMarkerFinderUI : public QDialog
+{
+    Q_OBJECT
+
+public:
+	V3DPluginCallback2* callback;
+    TipMarkerFinderUI(QWidget* parent, V3DPluginCallback2* callback, int menu);
+	QStringList inputs;
+	
+    ~TipMarkerFinderUI();
+
+public slots:
+	bool okClicked();
+	void filePath();
+
+private:
+    Ui::TipMarkerFinderUI* ui;
+};
 
 #endif // TIP_MARKER_FUNC_H
