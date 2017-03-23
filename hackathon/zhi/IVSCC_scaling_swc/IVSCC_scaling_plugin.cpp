13784817c29c119a2f13e94feaa68792e1c27d85
Zhi Zhou <alan.zhizhou@gmail.com>
2017-03-23 16:13:16 -0700

    added a menu to process fmost swc file

--- hackathon/zhi/IVSCC_scaling_swc/IVSCC_scaling_plugin.cpp
+++ hackathon/zhi/IVSCC_scaling_swc/IVSCC_scaling_plugin.cpp
@@ -22,6 +22,7 @@ QStringList IVSCC_scaling::funclist() const
 {
 	return QStringList()
         <<tr("scale")
+        <<tr("fmost_scale")
 		<<tr("help");
 }
 
@@ -111,7 +112,75 @@ bool IVSCC_scaling::dofunc(const QString & func_name, const V3DPluginArgList & i
           nt_scaled.hashNeuron = hashNeuron;
 
           writeSWC_file(outswc_file,nt_scaled);
-	}
+    }else if (func_name == tr("fmost_scale"))
+    {
+          cout<<"Welcome to fMOST swc scaling processing plugin"<<endl;
+          if(infiles.empty())
+          {
+              cerr<<"Need input swc file"<<endl;
+              return false;
+          }
+
+          QString  inswc_file =  infiles[0];
+          QString  outswc_file =  outfiles[0];
+
+          double x_scale, y_scale, z_scale, r_scale;
+          if (input.size()>=2)
+          {
+
+              vector<char*> paras = (*(vector<char*> *)(input.at(1).p));
+              if(paras.size() >= 4)
+              {
+                  cout<<paras.size()<<endl;
+                  x_scale = atof(paras.at(0));
+                  y_scale = atof(paras.at(1));
+                  z_scale = atof(paras.at(2));
+                  r_scale = atof(paras.at(3));
+              }
+              else
+              {
+                  cerr<<"Need four scaling factors"<<endl;
+                  return false;
+              }
+          }
+          else
+          {
+              cerr<<"Need four scaling factors"<<endl;
+              return false;
+          }
+
+          cout<<"inswc_file = "<<inswc_file.toStdString().c_str()<<endl;
+          cout<<"outswc_file = "<<outswc_file.toStdString().c_str()<<endl;
+
+          NeuronTree nt = readSWC_file(inswc_file);
+          QList<NeuronSWC> list = nt.listNeuron;
+          double max_y = -100000000, max_z = -100000000;
+          double min_y = 100000000,min_z = 100000000;
+
+          for(V3DLONG i = 0; i < list.size();i++)
+          {
+              nt.listNeuron[i].x = x_scale *  nt.listNeuron.at(i).x;
+              nt.listNeuron[i].y = y_scale *  nt.listNeuron.at(i).y;
+              nt.listNeuron[i].z = z_scale *  nt.listNeuron.at(i).z;
+              nt.listNeuron[i].r = r_scale *  nt.listNeuron.at(i).r;
+
+              if(nt.listNeuron[i].y >= max_y) max_y = nt.listNeuron[i].y;
+              if(nt.listNeuron[i].y <= min_y) min_y = nt.listNeuron[i].y;
+
+              if(nt.listNeuron[i].z >= max_z) max_z = nt.listNeuron[i].z;
+              if(nt.listNeuron[i].z <= min_z) min_z = nt.listNeuron[i].z;
+
+
+          }
+          for(V3DLONG i = 0; i < nt.listNeuron.size();i++)
+          {
+              nt.listNeuron[i].y = max_y + min_y - nt.listNeuron[i].y;
+              nt.listNeuron[i].z = max_z + min_z - nt.listNeuron[i].z;
+
+          }
+
+          writeSWC_file(outswc_file,nt);
+    }
 	else if (func_name == tr("help"))
 	{
         cout<<"Usage : v3d -x dllname -f scale -i <inswc_folder> -o <outswc_file> -p <x_factor> <y_factor> <z_factor> <radius_factor>"<<endl;
