e20ae37b5db9deaad61139e344e8171de3467369
helanfeixue <497174789@qq.com>
2017-06-23 12:20:19 +0800

    create a new plugin for matching neuron substructure

---
+++ hackathon/heyishan/region_match/match_swc.h
@@ -0,0 +1,10 @@
+#ifndef MATCH_SWC_H
+#define MATCH_SWC_H
+
+#include "v3d_message.h"
+#include <vector>
+#include "basic_surf_objs.h"
+#include <iostream>
+void match_swc(const NeuronTree &nt1, const NeuronTree &nt2);
+
+#endif // MATCH_SWC_H
