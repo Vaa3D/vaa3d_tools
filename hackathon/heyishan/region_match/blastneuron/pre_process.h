3860a522b760303b9edefa22e26c748c79d71d56
helanfeixue <497174789@qq.com>
2017-06-26 15:57:37 +0800

    add a preprocess file

--- hackathon/heyishan/region_match/blastneuron/pre_process.h
+++ hackathon/heyishan/region_match/blastneuron/pre_process.h
@@ -1,4 +1,12 @@
 #ifndef PRE_PROCESS_H
 #define PRE_PROCESS_H
+#include "basic_surf_objs.h"
+#include "../neuron_utilities/resample_swc.h"
+#include "../neuron_utilities/sort_swc.h"
+NeuronTree pre_process(NeuronTree nt, double step_size, double prune_size);
+int pca1(double *, int, int, double * , double *, double *);
+NeuronTree align_axis(NeuronTree input);
+
+
 
 #endif // PRE_PROCESS_H
