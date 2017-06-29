d1c53b1a53b577a60e479377fc71dcda2a663490
helanfeixue <497174789@qq.com>
2017-06-29 19:38:02 +0800

    create a new plugin for pattern search and build a inital framework

---
+++ hackathon/heyishan/pattern_search/pattern_search.pro
@@ -0,0 +1,15 @@
+
+TEMPLATE	= lib
+CONFIG	+= qt plugin warn_off
+#CONFIG	+= x86_64
+VAA3DPATH = /home/hys/v3d_external
+INCLUDEPATH	+= $$VAA3DPATH/v3d_main/basic_c_fun
+INCLUDEPATH	+= $$VAA3DPATH/v3d_main/common_lib/include
+
+HEADERS	+= pattern_search_plugin.h
+SOURCES	+= pattern_search_plugin.cpp
+SOURCES	+= $$VAA3DPATH/v3d_main/basic_c_fun/v3d_message.cpp
+SOURCES	+= $$VAA3DPATH/v3d_main/basic_c_fun/basic_surf_objs.cpp
+
+TARGET	= $$qtLibraryTarget(pattern_search)
+DESTDIR	= $$VAA3DPATH/bin/plugins/pattern_search/
---
+++ hackathon/heyishan/pattern_search/pattern_search_plugin.cpp
@@ -0,0 +1,235 @@
+/* pattern_search_plugin.cpp
+ * finding substructure in a whole neuron with certain morpological pattern.
+ * 2017-6-29 : by He Yishan
+ */
+ 
+#include "v3d_message.h"
+#include <vector>
+#include "basic_surf_objs.h"
+#include <iostream>
+#include "pattern_search_plugin.h"
+#include "pattern_match.h"
+
+#define VOID 1000000000
+
+
+Q_EXPORT_PLUGIN2(pattern_search, pattern_search);
+
+using namespace std;
+
+struct input_PARA
+{
+    QString inimg_file;
+    V3DLONG channel;
+    NeuronTree nt_search;
+    NeuronTree nt_pattern;
+};
+
+struct Boundary
+{
+    double minx;
+    double miny;
+    double minz;
+    double maxx;
+    double maxy;
+    double maxz;
+};
+
+void ml_func(V3DPluginCallback2 &callback, QWidget *parent, input_PARA &PARA, bool bmenu);
+bool pattern_analysis(const NeuronTree &nt,const NeuronTree &boundary,NeuronTree & consensus, int & boundary_length);
+bool get_subtrees(const NeuronTree &nt, vector<NeuronTree> &sub_trees, int boundary_length, vector<vector<V3DLONG> >p_to_tree);
+ 
+QStringList pattern_search::menulist() const
+{
+	return QStringList() 
+		<<tr("tracing_menu")
+		<<tr("about");
+}
+
+QStringList pattern_search::funclist() const
+{
+	return QStringList()
+		<<tr("tracing_func")
+		<<tr("help");
+}
+
+void pattern_search::domenu(const QString &menu_name, V3DPluginCallback2 &callback, QWidget *parent)
+{
+	if (menu_name == tr("tracing_menu"))
+	{
+        bool bmenu = true;
+        input_PARA PARA;
+        ml_func(callback,parent,PARA,bmenu);
+
+	}
+	else
+	{
+		v3d_msg(tr("finding substructure in a whole neuron with certain morpological pattern.. "
+			"Developed by He Yishan, 2017-6-29"));
+	}
+}
+
+bool pattern_search::dofunc(const QString & func_name, const V3DPluginArgList & input, V3DPluginArgList & output, V3DPluginCallback2 & callback,  QWidget * parent)
+{
+	if (func_name == tr("tracing_func"))
+	{
+        bool bmenu = false;
+        printf("dofunc can't' work yet.\n");
+        return true;
+	}
+    else if (func_name == tr("help"))
+    {
+		printf("**** Usage of pattern_search tracing **** \n");
+		printf("vaa3d -x pattern_search -f tracing_func -i <inimg_file> -p <channel> <swc_file> <marker_file> <other parameters>\n");
+        printf("inimg_file       The input image\n");
+        printf("channel          Data channel for tracing. Start from 1 (default 1).\n");
+        printf("swc_file         SWC file path.\n");
+        printf("marker_file      Marker file path.\n");
+
+        printf("outswc_file      Will be named automatically based on the input image file name, so you don't have to specify it.\n");
+        printf("outmarker_file   Will be named automatically based on the input image file name, so you don't have to specify it.\n\n");
+
+	}
+	else return false;
+
+	return true;
+}
+
+
+void ml_func(V3DPluginCallback2 &callback, QWidget *parent, input_PARA &PARA, bool bmenu)
+{
+    if(bmenu)
+    {
+        PARA.nt_search = readSWC_file("original_vr_neuron.swc");
+        PARA.nt_pattern = readSWC_file("areaofinterest.swc");
+    }
+    else
+    {
+        printf("In to dofunc.\n");
+    }
+    cout<< "******************Welcome To Pattern Search*********************"<<endl;
+
+    int boundary_length;
+    NeuronTree pt_consensus;
+    if(!pattern_analysis(PARA.nt_search,PARA.nt_pattern,pt_consensus,boundary_length))
+    {
+        cout<<"something wrong is in pattern_analysis"<<endl;
+    }
+    vector<NeuronTree> sub_trees;
+    vector<vector<V3DLONG> > p_to_tree;
+    get_subtrees(PARA.nt_search,sub_trees,boundary_length,p_to_tree);
+    double step_radio= 0.25;
+    int search_step = boundary_length * step_radio;
+    vector<NeuronTree> selected_trees;
+    pattern_match(sub_trees,pt_consensus,selected_trees,search_step);
+
+    //Output
+    cout<<"selected_tree_size="<<selected_trees.size()<<endl;
+    cout<<p_to_tree.size()<<endl;
+    vector<V3DLONG> result_points;
+    for(int i=0; i<selected_trees.size();i++)
+    {
+        V3DLONG id_tree=selected_trees[i];
+        cout<<"id_tree="<<id_tree<<endl;
+        vector<V3DLONG> ps_tree=p_to_tree[id_tree];
+        cout<<"ps_tree_size="<<ps_tree.size()<<endl;
+        for(int j=0;j<ps_treee.size();j++)
+        {
+            result_points.push_back(ps_tree[j]);
+        }
+    }
+    cout<<"result_points.size="<<result_points.size()<<endl;
+    vector<V3DLONG> result_points_set = result_points; // Could be better
+
+    //QList <NeuronSWC> list_pattern = PARA.nt_boundary.listNeuron;
+    QList <NeuronSWC> list_search = PARA.nt_search.listNeuron;
+
+    for(V3DLONG i =0; i<result_points_set.size();i++)
+    {
+        V3DLONG id=result_points_set[i];
+        list_search[id].type =7;    // type=7 means the color is green
+    }
+    export_list2file(list_search,"updated_vr_neuron.swc","updated_vr_neuron.swc");
+
+    return;
+}
+
+bool pattern_analysis(const NeuronTree &nt, const NeuronTree &boundary,NeuronTree & consensus, int & boundary_length)
+{
+    V3DLONG boundary_size = boundary.listNeuron.size();
+    V3DLONG nt_size= nt.listNeuron.size();
+    if(boundary_size==0 || nt_size==0)
+    {
+        v3d_msg("Input is empty, please retry");
+        return false;
+    }
+
+    // find min max boundary of each areaofinterest
+    Boundary temp;
+    vector<Boundary> v_boundary;
+    for(V3DLONG i=0;i<boundary_size;i++)
+    {
+
+       if(boundary.listNeuron[i].pn<0)
+       {
+            temp.minx=VOID; temp.miny=VOID;temp.minz=VOID;
+            temp.maxx=0;temp.maxy=0;temp.maxz=0;
+        }
+       temp.minx=min(boundary.listNeuron[i].x,temp.minx);
+       temp.miny=min(boundary.listNeuron[i].y,temp.miny);
+       temp.minz=min(boundary.listNeuron[i].z,temp.minz);
+       temp.maxx=max(boundary.listNeuron[i].x,temp.maxx);
+       temp.maxy=max(boundary.listNeuron[i].y,temp.maxy);
+       temp.maxz=max(boundary.listNeuron[i].z,temp.maxz);
+
+       if(i==boundary_size-2)
+       {
+           temp.minx=min(boundary.listNeuron[i+1].x,temp.minx);
+           temp.miny=min(boundary.listNeuron[i+1].y,temp.miny);
+           temp.minz=min(boundary.listNeuron[i+1].z,temp.minz);
+           temp.maxx=max(boundary.listNeuron[i+1].x,temp.maxx);
+           temp.maxy=max(boundary.listNeuron[i+1].y,temp.maxy);
+           temp.maxz=max(boundary.listNeuron[i+1].z,temp.maxz);
+           v_boundary.push_back(temp);
+           i++;
+       }
+       else if(boundary.listNeuron[i+1].pn==-1)
+       {
+           v_boundary.push_back(temp);
+       }
+    }
+
+    // push points in v_boundary into v_area;
+    vector<NeuronTree> v_area(v_boundary.size());
+   for(V3DLONG i=0;i<nt.listNeuron.size();i++)
+   {
+       NeuronSWC curr = nt.listNeuron[i];
+       for(int j=0; j<v_boundary.size();j++)
+       {
+           Boundary b=v_boundary[j];
+           if(curr.x>b.minx && curr.y>b.miny && curr.z>b.minz &&curr.x<b.maxx && curr.y<b.maxy&&curr.z<b.maxz)
+           {
+               v_area[j].listNeuron.push_back(curr);
+               break;
+           }
+       }
+    }
+
+   // calculate boundary_length of each of v_boundary
+
+}
+
+bool get_subtrees(const NeuronTree &nt, vector<NeuronTree> &sub_trees, int boundary_length, vector<vector<V3DLONG> >p_to_tree)
+{
+
+}
+
+
+
+
+
+
+
+
+
+
