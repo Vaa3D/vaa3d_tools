a4117167fa48ec5e766ec048a7f89c867ce1b513
MKRangers <hsienchik@alleninstitute.org>
2017-11-08 18:07:41 -0800

    DatasetGenerator1.0: UI construction 11/8

--- hackathon/MK/DatasetGenerator/datasetGeneratorUI.cpp
+++ hackathon/MK/DatasetGenerator/datasetGeneratorUI.cpp
@@ -17,7 +17,16 @@ DatasetGeneratorUI::DatasetGeneratorUI(QWidget* parent, V3DPluginCallback2* call
 	ui->treeView->hideColumn(3);
 	ui->treeView->setCurrentIndex(dirModel->index(QDir::currentPath()));
 
+	ui->checkBox_4->setEnabled(false);
+	ui->checkBox_5->setEnabled(false);
+	ui->checkBox_6->setEnabled(false);
+
 	this->show();
+}
+
+DatasetGeneratorUI::~DatasetGeneratorUI()
+{
+	delete ui;
 }
 
 void DatasetGeneratorUI::selectClicked()
@@ -25,7 +34,69 @@ void DatasetGeneratorUI::selectClicked()
 	QModelIndexList indexList = ui->treeView->selectionModel()->selectedIndexes();
 	QModelIndex selectedEntry = *(indexList.begin());
 	QString wholePath = this->dirModel->fileInfo(selectedEntry).absoluteFilePath();
-	ui->lineEdit_7->setText(wholePath);
-	qDebug() << wholePath;
+
+	QObject* signalSender = sender();
+	QString pushButtonName = signalSender->objectName();
+	if (pushButtonName == "pushButton_2") ui->lineEdit_7->setText(wholePath);
+	else if (pushButtonName == "pushButton_3") ui->lineEdit_8->setText(wholePath);
+
+
+	
+	
+}
+
+void DatasetGeneratorUI::checkboxToggled(bool checked)
+{
+	QObject* signalSender = sender();
+	QString checkBoxName = signalSender->objectName();
+	
+	if (checked == true)
+	{
+		if (checkBoxName == "checkBox_2")
+		{
+			ui->checkBox_3->setEnabled(false);
+			ui->checkBox_5->setEnabled(false);
+			ui->checkBox_6->setEnabled(false);
+			ui->checkBox_4->setEnabled(true);
+		}
+		else if (checkBoxName == "checkBox_3")
+		{
+			ui->checkBox_2->setEnabled(false);
+			ui->checkBox_4->setEnabled(false);
+			ui->checkBox_5->setEnabled(true);
+			ui->checkBox_6->setEnabled(true);
+		}
+		else if (checkBoxName == "checkBox_5") ui->checkBox_6->setEnabled(false);
+		else if (checkBoxName == "checkBox_6") ui->checkBox_5->setEnabled(false);
+	}
+	else
+	{
+		if (checkBoxName == "checkBox_2")
+		{
+			ui->checkBox_3->setEnabled(true);
+			ui->checkBox_5->setEnabled(true);
+			ui->checkBox_6->setEnabled(true);
+		}
+		else if (checkBoxName == "checkBox_3")
+		{
+			ui->checkBox_2->setEnabled(true);
+			ui->checkBox_4->setEnabled(true);
+		}
+		else if (checkBoxName == "checkBox_5") ui->checkBox_6->setEnabled(true);
+		else if (checkBoxName == "checkBox_6") ui->checkBox_5->setEnabled(true);
+	}
+}
+
+void DatasetGeneratorUI::exclusiveToggle(bool checked)
+{
+	QObject* signalSender = sender();
+	QString checkBoxName = signalSender->objectName();
+
+	if (checked == true)
+	{
+		if (checkBoxName == "checkBox_5") ui->checkBox_6->setChecked(false);
+		else if (checkBoxName == "checkBox_6") ui->checkBox_5->setChecked(false);
+	}
+
 }
 
--- hackathon/MK/DatasetGenerator/datasetGeneratorUI.h
+++ hackathon/MK/DatasetGenerator/datasetGeneratorUI.h
@@ -3,23 +3,26 @@
 
 #include "Dataset_Generator_plugin.h"
 #include "ui_DatasetGenerator.h"
-
+#include <v3d_interface.h>
 
 namespace Ui 
 {
 	class DatasetGeneratorUI;
 } 
 
-class DatasetGeneratorUI: public QDialog
+class DatasetGeneratorUI : public QDialog
 {
-	
+
+	Q_OBJECT
 
 public:
 	DatasetGeneratorUI(QWidget* parent, V3DPluginCallback2* callback);
-	~DatasetGeneratorUI() {};
+	~DatasetGeneratorUI();
 
 public slots:
 	void selectClicked();
+	void checkboxToggled(bool);
+	void exclusiveToggle(bool);
 
 private:
 	Ui::DatasetGeneratorUI* ui;
