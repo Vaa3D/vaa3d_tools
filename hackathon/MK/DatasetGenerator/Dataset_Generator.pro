bdf6544c3c257d0a8aeb59d0e54baead06ac7278
MKRangers <hsienchik@alleninstitute.org>
2017-11-30 11:46:05 -0800

    DatasetGenerator: reorganize files

--- hackathon/MK/DatasetGenerator/Dataset_Generator.pro
+++ hackathon/MK/DatasetGenerator/Dataset_Generator.pro
@@ -12,7 +12,6 @@ HEADERS += Operator.h
 SOURCES	+= Dataset_Generator_plugin.cpp
 SOURCES	+= $$VAA3DPATH/v3d_main/basic_c_fun/v3d_message.cpp
 SOURCES += $$INCLUDEPATH/basic_surf_objs.cpp
-SOURCES += datasetGenerator_func.cpp
 SOURCES += datasetGeneratorUI.cpp
 SOURCES += Operator.cpp
 
--- hackathon/MK/DatasetGenerator/Operator.cpp
+++ hackathon/MK/DatasetGenerator/Operator.cpp
@@ -574,3 +574,136 @@ void Operator::getImageFolders()
 	}
 	closedir(dir);
 }
+
+void Operator::pick_save()
+{
+	QString imgName;
+
+	for (QVector<QString>::iterator it = this->imageFolders.begin(); it != this->imageFolders.end(); ++it)
+	{
+		QStringList folderName = it->split("\\");
+		QStringList imgPreList1 = folderName[9].split("/");
+		QStringList imgPreList2 = imgPreList1[1].split("_");
+		imgName = imgPreList2[0];
+		//qDebug() << imgName;
+
+		const char* swcDIR;
+		QString inputSWCPath = this->inputSWCdir + "\\";
+		string str = inputSWCPath.toStdString();
+		swcDIR = str.c_str();
+		//cout << swcDIR << endl;
+		QString intoSWCFolder = inputSWCPath;
+		DIR* dir;
+		struct dirent *ent;
+		struct dirent* imgEnt;
+
+		if ((dir = opendir(swcDIR)) != NULL)
+		{
+			bool flag = false;
+			QString swcFolderName;
+			while ((ent = readdir(dir)) != NULL)
+			{
+				for (size_t i = 0; i<30; ++i)
+				{
+					if (ent->d_name[i] == NULL) break;
+					swcFolderName = swcFolderName + QChar(ent->d_name[i]);
+				}
+
+				if (swcFolderName == imgName)
+				{
+					intoSWCFolder = intoSWCFolder + swcFolderName;
+					swcFolderName.clear();
+					break;
+				}
+				swcFolderName.clear();
+			}
+			//qDebug() << swcFolderName;
+			swcFolderName.clear();
+		}
+		//qDebug() << intoSWCFolder;
+		closedir(dir);
+
+		QString outputfolder = "Z:\\IVSCC\\allAxon\\" + imgName;
+		QDir().mkdir(outputfolder);
+
+		string str2 = intoSWCFolder.toStdString();
+		const char* intoSWCdir = str2.c_str();
+		if ((dir = opendir(intoSWCdir)) != NULL)
+		{
+			int _count = 0;
+			bool flag = false;
+			QString fileName;
+			while ((ent = readdir(dir)) != NULL)
+			{
+				for (size_t i = 0; i<30; ++i)
+				{
+					if (ent->d_name[i] == NULL) break;
+					fileName = fileName + QChar(ent->d_name[i]);
+				}
+
+				if (fileName == "manual.swc_p.swc")
+				{
+					QString nodeX, nodeY, nodeZ;
+					QString swcFullName = intoSWCFolder + "\\manual.swc_p.swc";
+					NeuronTree inputSWCTree = readSWC_file(swcFullName);
+					for (QList<NeuronSWC>::iterator neuronIt = inputSWCTree.listNeuron.begin(); neuronIt != inputSWCTree.listNeuron.end(); ++neuronIt)
+					{
+						if (neuronIt->type == 2)
+						{
+							nodeX = QString::number(int(neuronIt->x));
+							nodeY = QString::number(int(neuronIt->y));
+							nodeZ = QString::number(int(neuronIt->z));
+
+							const char* imgDir;
+							string str3 = it->toStdString();
+							imgDir = str3.c_str();
+							DIR* getImgDir;
+							//cout << imgDir << endl;
+							if ((getImgDir = opendir(imgDir)) != NULL)
+							{
+								while ((imgEnt = readdir(getImgDir)) != NULL)
+								{
+									QString imgFileName;
+									for (size_t i = 0; i<50; ++i)
+									{
+										if (imgEnt->d_name[i] == NULL) break;
+										imgFileName = imgFileName + QChar(imgEnt->d_name[i]);
+									}
+									if (imgFileName == "." || imgFileName == "..")
+									{
+										imgFileName.clear();
+										continue;
+									}
+									//qDebug() << imgFileName;
+
+									QStringList nameSplit = imgFileName.split("_");
+									QStringList xNameSplit = nameSplit[5].split("x");
+									QStringList yNameSplit = nameSplit[6].split("y");
+									QStringList zSplit = nameSplit[7].split("z");
+									QStringList zNameSplit = zSplit[1].split(".");
+									QString xName = xNameSplit[1];
+									QString yName = yNameSplit[1];
+									QString zName = zNameSplit[0];
+
+									if (nodeX == xName && nodeY == yName && nodeZ == zName)
+									{
+										QString command = "copy " + *it + "\\" + imgFileName;
+										command = command + " " + outputfolder + "\\" + imgFileName;
+										command.replace("/", "\\");
+										string commandStr = command.toStdString();
+										const char* systemCmd = commandStr.c_str();
+										cout << systemCmd << endl;
+										system(systemCmd);
+									}
+								}
+							}
+							closedir(getImgDir);
+						}
+					}
+				}
+				fileName.clear();
+			}
+		}
+		closedir(dir);
+	}
+}
