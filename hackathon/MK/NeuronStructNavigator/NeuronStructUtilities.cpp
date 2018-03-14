b4844f7b04ad23a5e3b49fe60fe59ca23fe7169f
MKRangers <hsienchik@alleninstitute.org>
2018-03-14 12:05:54 -0700

    modify

--- hackathon/MK/NeuronStructNavigator/NeuronStructUtilities.cpp
+++ hackathon/MK/NeuronStructNavigator/NeuronStructUtilities.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <list>
+#include <deque>
 #include <ctime>
 
 #include "NeuronStructUtilities.h"
@@ -61,33 +62,149 @@ void swcSlicer(NeuronTree* inputTreePtr, vector<NeuronTree>* outputTreesPtr, int
 		outputTreesPtr->push_back(*it);
 }
 
-void bkgNode_Gen(NeuronTree* inputTreePtr, NeuronTree* outputTreePtr, long int dims[])
+void sigNode_Gen(NeuronTree* inputTreePtr, NeuronTree* outputTreePtr, float ratio, float distance)
 {
-	
-}
-
-void bkgNode_Gen_somaArea(NeuronTree* intputTreePtr, NeuronTree* outputTreePtr, int xLength, int yLength, int zLength, float ratio, float selectedCenter[])
-{
-	NeuronSWC somaNode;
-	if (selectedCenter[0] == selectedCenter[1] == selectedCenter[2] == 0)
+	int nodeCount = 0;
+	for (QList<NeuronSWC>::iterator it = inputTreePtr->listNeuron.begin(); it != inputTreePtr->listNeuron.end(); ++it)
 	{
-		cout << "test" << endl;
-		for (QList<NeuronSWC>::iterator it = intputTreePtr->listNeuron.begin(); it != intputTreePtr->listNeuron.end(); ++it)
+		int foldCount = 0;
+		while (foldCount <= ratio)
 		{
-			if (it->parent == -1)
+			int randNumX = rand() % int(distance * 2) + int(it->x - distance);
+			int randNumY = rand() % int(distance * 2) + int(it->y - distance);
+			int randNumZ = rand() % int(distance * 2) + int(it->z - distance);
+
+			++nodeCount;
+			if (nodeCount % 10000 == 0) cout << nodeCount << " signal nodes generated." << endl;
+
+			NeuronSWC newNode;
+			newNode.x = randNumX;
+			newNode.y = randNumY;
+			newNode.z = randNumZ;
+			newNode.type = 2;
+			newNode.radius = 1;
+			newNode.parent = -1;
+			outputTreePtr->listNeuron.push_back(newNode);
+
+			++foldCount;
+		}
+	}
+
+
+	/*deque<NeuronSWC> swcQueu;
+	float xMin = 10000, xMax = 0;
+	float yMin = 10000, yMax = 0;
+	float zMin = 10000, zMax = 0;
+	for (QList<NeuronSWC>::iterator it = inputTreePtr->listNeuron.begin(); it != inputTreePtr->listNeuron.end(); ++it)
+	{
+		if (it->x <= xMin) xMin = it->x;
+		if (it->x >= xMax) xMax = it->x;
+		if (it->y <= yMin) yMin = it->y;
+		if (it->y >= yMax) yMax = it->y;
+		if (it->z <= zMin) zMin = it->z;
+		if (it->z >= zMax) zMax = it->z;
+		swcQueu.push_back(*it);
+	}*/
+
+	//int targetNodeNum = int(inputTreePtr->listNeuron.size() * ratio);
+	//int nodeCount = 0;
+	//while (nodeCount <= targetNodeNum)
+	//{
+		
+
+		/*int randNumX = rand() % int(xMax - xMin + 1) + int(xMin);
+		int randNumY = rand() % int(yMax - yMin + 1) + int(yMin);
+		int randNumZ = rand() % int(zMax - zMin + 1) + int(zMin);
+
+		bool flag = false;
+		for (deque<NeuronSWC>::iterator it = swcQueu.begin(); it != swcQueu.end(); ++it)
+		{
+			float distSqr;
+			float diffx = float(randNumX) - it->x;
+			float diffy = float(randNumY) - it->y;
+			float diffz = float(randNumZ) - it->z;
+			distSqr = diffx * diffx + diffy * diffy + diffz * diffz;
+
+			if (distSqr >= distance * distance)
 			{
-				somaNode = *it;
+				flag = true;
 				break;
 			}
 		}
-	}
-	else
-	{
-		somaNode.x = selectedCenter[0];
-		somaNode.y = selectedCenter[1];
-		somaNode.z = selectedCenter[2];
-	}
 
+		if (flag == false)
+		{
+			++nodeCount;
+			if (nodeCount % 10000 == 0) cout << nodeCount << " signal nodes generated." << endl;
+
+			NeuronSWC newNode;
+			newNode.x = randNumX;
+			newNode.y = randNumY;
+			newNode.z = randNumZ;
+			newNode.type = 2;
+			newNode.radius = 1;
+			newNode.parent = -1;
+			outputTreePtr->listNeuron.push_back(newNode);
+		}*/
+	//}
+}
+
+void bkgNode_Gen(NeuronTree* inputTreePtr, NeuronTree* outputTreePtr, long int dims[], float ratio, float distance)
+{
+	QList<NeuronSWC> neuronList = inputTreePtr->listNeuron;
+	int targetBkgNodeNum = int(neuronList.size() * ratio);
+	cout << targetBkgNodeNum << " targeted bkg nodes to ge generated." << endl;
+	int bkgNodeCount = 0;
+	while (bkgNodeCount <= targetBkgNodeNum)
+	{
+		int randNumX = rand() % (dims[0] - 200) + 200;
+		int randNumY = rand() % (dims[1] - 200) + 200;
+		int randNumZ = rand() % dims[2];
+
+		bool flag = false;
+		for (QList<NeuronSWC>::iterator it = neuronList.begin(); it != neuronList.end(); ++it)
+		{
+			float distSqr;
+			float diffx = float(randNumX) - it->x;
+			float diffy = float(randNumY) - it->y;
+			float diffz = float(randNumZ) - it->z;
+			distSqr = diffx * diffx + diffy * diffy + diffz * diffz;
+
+			if (distSqr <= distance * distance)
+			{
+				flag = true;
+				break;
+			}
+		}
+
+		if (flag == false)
+		{
+			++bkgNodeCount;
+			if (bkgNodeCount % 10000 == 0) cout << bkgNodeCount << " bkg nodes generated." << endl;
+
+			NeuronSWC newBkgNode;
+			newBkgNode.x = randNumX;
+			newBkgNode.y = randNumY;
+			newBkgNode.z = randNumZ;
+			newBkgNode.type = 3;
+			newBkgNode.radius = 1;
+			newBkgNode.parent = -1;
+			outputTreePtr->listNeuron.push_back(newBkgNode);
+		}
+	}
+}
+
+void bkgNode_Gen_somaArea(NeuronTree* intputTreePtr, NeuronTree* outputTreePtr, int xLength, int yLength, int zLength, float ratio, float distance)
+{
+	NeuronSWC somaNode;
+	for (QList<NeuronSWC>::iterator it = intputTreePtr->listNeuron.begin(); it != intputTreePtr->listNeuron.end(); ++it)
+	{
+		if (it->parent == -1)
+		{
+			somaNode = *it;
+			break;
+		}
+	}
 	float xlb = somaNode.x - float(xLength);
 	float xhb = somaNode.x + float(xLength);
 	float ylb = somaNode.y - float(yLength);
@@ -117,7 +234,7 @@ void bkgNode_Gen_somaArea(NeuronTree* intputTreePtr, NeuronTree* outputTreePtr, 
 			float diffz = float(randNumZ) - it->z;
 			distSqr = diffx * diffx + diffy * diffy + diffz * diffz;
 
-			if (distSqr <= 400)
+			if (distSqr <= distance * distance)
 			{
 				flag = true;
 				break;
@@ -127,7 +244,7 @@ void bkgNode_Gen_somaArea(NeuronTree* intputTreePtr, NeuronTree* outputTreePtr, 
 		if (flag == false)
 		{
 			++bkgNodeCount;
-			if (bkgNodeCount % 1000 == 0) cout << bkgNodeCount << " bkg nodes generated within the soma area." << endl;
+			if (bkgNodeCount % 10000 == 0) cout << bkgNodeCount << " bkg nodes generated within the soma area." << endl;
 
 			NeuronSWC newBkgNode;
 			newBkgNode.x = randNumX;
--- hackathon/MK/NeuronStructNavigator/NeuronStructUtilities.h
+++ hackathon/MK/NeuronStructNavigator/NeuronStructUtilities.h
@@ -14,7 +14,9 @@ using namespace std;
 // ------- swcSlicer disembles SWC into "SWC slices" based on z coordinates ------- //
 void swcSlicer(NeuronTree* inputTreePtr, vector<NeuronTree>* outputTreesPtr, int thickness = 0);
 
-void bkgNode_Gen(NeuronTree* inputTreePtr, NeuronTree* outputTreePtr, long int dims[]);
-void bkgNode_Gen_somaArea(NeuronTree* inputTreePtr, NeuronTree* outputTreePtr, int xLength, int yLength, int zLength, float ratio, float selectedCenter[] = (0, 0, 0));
+void sigNode_Gen(NeuronTree* inputTreePtr, NeuronTree* outputTreePtr, float ratio, float distance);
+
+void bkgNode_Gen(NeuronTree* inputTreePtr, NeuronTree* outputTreePtr, long int dims[], float ratio, float distance);
+void bkgNode_Gen_somaArea(NeuronTree* inputTreePtr, NeuronTree* outputTreePtr, int xLength, int yLength, int zLength, float ratio, float distance);
 
 #endif 
