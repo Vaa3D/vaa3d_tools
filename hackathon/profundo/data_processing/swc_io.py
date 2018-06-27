ececc4a6c791d440289d715a708ff7f60709d31c
Richard Decal <richard.decal@ncf.edu>
2018-06-27 16:35:32 -0700

    fixed cli calls, added step_length param

--- hackathon/profundo/data_processing/swc_io.py
+++ hackathon/profundo/data_processing/swc_io.py
@@ -2,7 +2,6 @@ import os
 from collections import defaultdict
 import numpy as np
 import pandas as pd
-from subprocess import call
 
 
 def get_fnames_and_abspath_from_dir(reldir):
@@ -91,19 +90,13 @@ def swc_to_img(fname, output_dir="../data/06_synthetic_branches/"):
     pass
 
 
-def resample_swc(input_fname, input_fpath, output_dir="../data/04_human_branches_filtered_upsampled/"):
+def resample_swc(input_fname, input_fpath, vaad3d_bin_path, step_length=1.0, output_dir="../data/04_human_branches_filtered_upsampled/"):
     """
     sometimes, inter-node distances can be v large, which is bad for subsampling.
     this is a wrapper to call Vaa3D's resample_swc script
-    """
-    import tkinter as tk
-    from tkinter import filedialog
-
-    root = tk.Tk()
-    root.withdraw()
-
-    vaa3d_path = filedialog.askopenfilename(title='Select compiled Vaa3d binary')
     
+    see https://github.com/Vaa3D/Vaa3D_Wiki/wiki/resample_swc.wiki
+    """
     output_dir = os.path.abspath(output_dir)
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
@@ -112,7 +105,18 @@ def resample_swc(input_fname, input_fpath, output_dir="../data/04_human_branches
     
     # https://stackoverflow.com/a/4376421/4212158
     v3d_plugin_name = "resample_swc"
-    call([vaa3d_path, "-x", v3d_plugin_name, "-f", v3d_plugin_name, "-i", input_fpath, "-o", outfile_fpath])
+    
+    cli_dict = {"v3d_bin": vaad3d_bin_path,
+               "plugin": v3d_plugin_name,
+               "in": input_fpath,
+               "out": outfile_fpath,
+               "step": step_length}
+    
+    print("running \n")
+    print("{v3d_bin} -x {plugin} -f {plugin} -i {in} -o {out}".format(**cli_dict))
+    os.system("{v3d_bin} -x {plugin} -f {plugin} -i {in} -o {out} -p {step}".format(**cli_dict))
+    
+    
     
     return outfile_fpath
     
