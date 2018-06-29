bff78702e20c8a8a8ce66a1013dd0376eaea1041
Richard Decal <richard.decal@ncf.edu>
2018-06-29 16:15:07 -0700

    cleaning up, add_trajectory method

--- gym_profundo/envs/rendering.py
+++ gym_profundo/envs/rendering.py
@@ -30,12 +30,12 @@ class Viewer(object):
         self.agent_trajectory = [[], [], []]
         # internally, we call this "Gold standard data" because we don't actually
         # have ground truth, but this an easier
-        self.ground_truth = None  # TODO
+        self.ground_truth = self._gen_rand_trajectory(100)
 
         #self.window = pyglet.window.Window(width=width, height=height, display=display)
         #self.window.on_close = self.window_closed_by_user
         self.isopen = True
-        self.geoms = []
+        self.trajectories = []
         self.onetime_geoms = []
 
 
@@ -49,18 +49,17 @@ class Viewer(object):
         assert right > left and top > bottom
         scalex = self.width/(right-left)
         scaley = self.height/(top-bottom)
-        self.transform = Transform(
-            translation=(-left*scalex, -bottom*scaley),
-            scale=(scalex, scaley))
 
-    def add_geom(self, geom):
-        self.geoms.append(geom)
+    def add_trajectory(self, trajectory):
+        # make sure trajectory is 3D
+        assert(np.shape(trajectory)[0] == 3)
+        self.trajectories.append(trajectory)
 
     def add_onetime(self, geom):
         self.onetime_geoms.append(geom)
 
     def render(self, return_rgb_array=False):
-        self.window.clear()`
+        self.window.clear()
         self.window.switch_to()
         self.window.dispatch_events()
         self.transform.enable()
@@ -70,19 +69,6 @@ class Viewer(object):
             geom.render()
         self.transform.disable()
         arr = None
-        if return_rgb_array:
-            image_data = buffer.get_image_data()
-            arr = np.fromstring(image_data.data, dtype=np.uint8, sep='')
-            # In https://github.com/openai/gym-http-api/issues/2, we
-            # discovered that someone using Xmonad on Arch was having
-            # a window of size 598 x 398, though a 600 x 400 window
-            # was requested. (Guess Xmonad was preserving a pixel for
-            # the boundary.) So we use the buffer height/width rather
-            # than the requested one.
-            arr = arr.reshape(buffer.height, buffer.width, 4)
-            arr = arr[::-1,:,0:3]
-        self.window.flip()
-        self.onetime_geoms = []
         return arr if return_rgb_array else self.isopen
 
     def _gen_rand_trajectory(self, n_nodes, stepsize=0.1):
@@ -111,173 +97,6 @@ class Viewer(object):
     def __del__(self):
         self.close()
 
-def _add_attrs(geom, attrs):
-    if "color" in attrs:
-        geom.set_color(*attrs["color"])
-    if "linewidth" in attrs:
-        geom.set_linewidth(attrs["linewidth"])
-
-class Geom(object):
-    def __init__(self):
-        self._color=Color((0, 0, 0, 1.0))
-        self.attrs = [self._color]
-    def render(self):
-        for attr in reversed(self.attrs):
-            attr.enable()
-        self.render1()
-        for attr in self.attrs:
-            attr.disable()
-    def render1(self):
-        raise NotImplementedError
-    def add_attr(self, attr):
-        self.attrs.append(attr)
-    def set_color(self, r, g, b):
-        self._color.vec4 = (r, g, b, 1)
-
-class Attr(object):
-    def enable(self):
-        raise NotImplementedError
-    def disable(self):
-        pass
-
-class Transform(Attr):
-    def __init__(self, translation=(0.0, 0.0), rotation=0.0, scale=(1,1)):
-        self.set_translation(*translation)
-        self.set_rotation(rotation)
-        self.set_scale(*scale)
-    def enable(self):
-        glPushMatrix()
-        glTranslatef(self.translation[0], self.translation[1], 0) # translate to GL loc ppint
-        glRotatef(RAD2DEG * self.rotation, 0, 0, 1.0)
-        glScalef(self.scale[0], self.scale[1], 1)
-    def disable(self):
-        glPopMatrix()
-    def set_translation(self, newx, newy):
-        self.translation = (float(newx), float(newy))
-    def set_rotation(self, new):
-        self.rotation = float(new)
-    def set_scale(self, newx, newy):
-        self.scale = (float(newx), float(newy))
-
-class Color(Attr):
-    def __init__(self, vec4):
-        self.vec4 = vec4
-    def enable(self):
-        glColor4f(*self.vec4)
-
-class LineStyle(Attr):
-    def __init__(self, style):
-        self.style = style
-    def enable(self):
-        glEnable(GL_LINE_STIPPLE)
-        glLineStipple(1, self.style)
-    def disable(self):
-        glDisable(GL_LINE_STIPPLE)
-
-class LineWidth(Attr):
-    def __init__(self, stroke):
-        self.stroke = stroke
-    def enable(self):
-        glLineWidth(self.stroke)
-
-class Point(Geom):
-    def __init__(self):
-        Geom.__init__(self)
-    def render1(self):
-        glBegin(GL_POINTS) # draw point
-        glVertex3f(0.0, 0.0, 0.0)
-        glEnd()
-
-class FilledPolygon(Geom):
-    def __init__(self, v):
-        Geom.__init__(self)
-        self.v = v
-    def render1(self):
-        if   len(self.v) == 4 : glBegin(GL_QUADS)
-        elif len(self.v)  > 4 : glBegin(GL_POLYGON)
-        else: glBegin(GL_TRIANGLES)
-        for p in self.v:
-            glVertex3f(p[0], p[1],0)  # draw each vertex
-        glEnd()
-
-def make_circle(radius=10, res=30, filled=True):
-    points = []
-    for i in range(res):
-        ang = 2*math.pi*i / res
-        points.append((math.cos(ang)*radius, math.sin(ang)*radius))
-    if filled:
-        return FilledPolygon(points)
-    else:
-        return PolyLine(points, True)
-
-def make_polygon(v, filled=True):
-    if filled: return FilledPolygon(v)
-    else: return PolyLine(v, True)
-
-def make_polyline(v):
-    return PolyLine(v, False)
-
-def make_capsule(length, width):
-    l, r, t, b = 0, length, width/2, -width/2
-    box = make_polygon([(l,b), (l,t), (r,t), (r,b)])
-    circ0 = make_circle(width/2)
-    circ1 = make_circle(width/2)
-    circ1.add_attr(Transform(translation=(length, 0)))
-    geom = Compound([box, circ0, circ1])
-    return geom
-
-class Compound(Geom):
-    def __init__(self, gs):
-        Geom.__init__(self)
-        self.gs = gs
-        for g in self.gs:
-            g.attrs = [a for a in g.attrs if not isinstance(a, Color)]
-    def render1(self):
-        for g in self.gs:
-            g.render()
-
-class PolyLine(Geom):
-    def __init__(self, v, close):
-        Geom.__init__(self)
-        self.v = v
-        self.close = close
-        self.linewidth = LineWidth(1)
-        self.add_attr(self.linewidth)
-    def render1(self):
-        glBegin(GL_LINE_LOOP if self.close else GL_LINE_STRIP)
-        for p in self.v:
-            glVertex3f(p[0], p[1],0)  # draw each vertex
-        glEnd()
-    def set_linewidth(self, x):
-        self.linewidth.stroke = x
-
-class Line(Geom):
-    def __init__(self, start=(0.0, 0.0), end=(0.0, 0.0)):
-        Geom.__init__(self)
-        self.start = start
-        self.end = end
-        self.linewidth = LineWidth(1)
-        self.add_attr(self.linewidth)
-
-    def render1(self):
-        glBegin(GL_LINES)
-        glVertex2f(*self.start)
-        glVertex2f(*self.end)
-        glEnd()
-
-class Image(Geom):
-    def __init__(self, fname, width, height):
-        Geom.__init__(self)
-        self.width = width
-        self.height = height
-        img = pyglet.image.load(fname)
-        self.img = img
-        self.flip = False
-    def render1(self):
-        self.img.blit(-self.width/2, -self.height/2, width=self.width, height=self.height)
-
-# ================================================================
-
 class SimpleImageViewer(object):
     def __init__(self, display=None):
         self.window = None
@@ -301,7 +120,6 @@ class SimpleImageViewer(object):
                 self.isopen = False
 
         assert len(arr.shape) == 3, "You passed in an image with the wrong number shape"
-        image = pyglet.image.ImageData(arr.shape[1], arr.shape[0], 'RGB', arr.tobytes(), pitch=arr.shape[1]*-3)
         self.window.clear()
         self.window.switch_to()
         self.window.dispatch_events()
