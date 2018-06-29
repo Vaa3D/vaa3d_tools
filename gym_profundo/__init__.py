2972dfa1d7bbbd7232ff3e01cacf6ebe6ff96fc5
Richard Decal <richard.decal@ncf.edu>
2018-06-29 13:36:28 -0700

    fixed init

--- gym_profundo/__init__.py
+++ gym_profundo/__init__.py
@@ -4,25 +4,9 @@ from gym.envs.registration import register
 logger = logging.getLogger(__name__)
 
 register(
-    id='Soccer-v0',
-    entry_point='gym_soccer.envs:SoccerEnv',
+    id='Profundo-v0',
+    entry_point='gym_profundo.envs:ProfundoEnv',
     timestep_limit=1000,
     reward_threshold=1.0,
-    nondeterministic = True,
-)
-
-register(
-    id='SoccerEmptyGoal-v0',
-    entry_point='gym_soccer.envs:SoccerEmptyGoalEnv',
-    timestep_limit=1000,
-    reward_threshold=10.0,
-    nondeterministic = True,
-)
-
-register(
-    id='SoccerAgainstKeeper-v0',
-    entry_point='gym.envs:SoccerAgainstKeeperEnv',
-    timestep_limit=1000,
-    reward_threshold=8.0,
     nondeterministic = True,
 )
