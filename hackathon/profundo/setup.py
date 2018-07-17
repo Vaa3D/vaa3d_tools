784398a8c48b804d039471fc1f9c94b35a75de07
Richard Decal <richard.decal@ncf.edu>
2018-07-16 17:21:30 -0700

    fixed setup

--- hackathon/profundo/setup.py
+++ hackathon/profundo/setup.py
@@ -1,6 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='gym_profundo',
+setup(name='profundo',
       version='0.0.1',
+      packages = find_packages(),
       install_requires=['gym>=0.2.3']
 )
