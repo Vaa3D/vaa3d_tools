7fcf3b256dfe202dd8d13b63557eff4505cc3820
Richard Decal <richard.decal@ncf.edu>
2018-07-17 19:09:26 -0700

    deleting vestigial code

--- hackathon/profundo/agent/sampleTrain.py
+++ hackathon/profundo/agent/sampleTrain.py
@@ -65,51 +65,24 @@ class files(object):
 
         self.dir = directory
 
-        # todo make it generic for directories and files with different scenarios
-        self.images_list = self._listImages()
-        self.landmarks_list = self._listLandmarks()
-        self.all_landmarks_list = self._listLandmarks_all()
-
-    def _listImages(self):
-
-        childDirs = listFiles(self.dir, '*')
-
-        image_files = []
-
-        for child in childDirs:
-            dir_path = os.path.join(self.dir, child)
-            if not (os.path.isdir(dir_path)): continue
-            # todo: extend to all nifti image extensions
-            file_name = listFiles(dir_path, '*.nii.gz')
-            file_path = os.path.join(dir_path, file_name[0])
-            image_files.append(file_path)
-
-        return image_files
-
-
-    def sample_random(self):
-        """ return a random sampled ImageRecord from the list of files
-        """
-        # todo: fix seed for a fair comparison between models
-        random_idx = np.random.randint(low=0, high=len(self.images_list))
-        sitk_image, image = NiftiImage().decode(self.images_list[random_idx])
-        landmark = np.array(sitk_image.TransformPhysicalPointToIndex(self.landmarks_list[random_idx]))
-        return image, landmark, random_idx
-
-    def sample_circular(self, shuffle=False):
-        """ return a random sampled ImageRecord from the list of files
-        """
-        if shuffle:
-            indexes = rng.choice(x, len(x), replace=False)
-        else:
-            indexes = np.arange(self.num_files)
-
-        while True:
-            for idx in indexes:
-                sitk_image, image = NiftiImage().decode(self.images_list[idx])
-                landmark = np.array(sitk_image.TransformPhysicalPointToIndex(self.landmarks_list[idx]))
-                image_filename = self.images_list[idx][:-7]
-                yield image, landmark, image_filename
+        # self.images_list = self._listImages()
+        # self.landmarks_list = self._listLandmarks()
+        # self.all_landmarks_list = self._listLandmarks_all()
+    #
+    # def _listImages(self):
+    #
+    #     childDirs = listFiles(self.dir, '*')
+    #
+    #     image_files = []
+    #
+    #     for child in childDirs:
+    #         dir_path = os.path.join(self.dir, child)
+    #         if not (os.path.isdir(dir_path)): continue
+    #         file_name = listFiles(dir_path, '*.nii.gz')
+    #         file_path = os.path.join(dir_path, file_name[0])
+    #         image_files.append(file_path)
+    #
+    #     return image_files
 
     @property
     def num_files(self):
