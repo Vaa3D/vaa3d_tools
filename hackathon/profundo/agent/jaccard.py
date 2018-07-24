572c70009aa7058ca084dd16f55e45017876366e
Richard Decal <richard.decal@ncf.edu>
2018-07-24 16:19:52 -0700

    sanitizing inputs

--- hackathon/profundo/agent/jaccard.py
+++ hackathon/profundo/agent/jaccard.py
@@ -30,8 +30,8 @@ def jaccard(im1, im2):
     identical if `im1` and `im2` are switched.
 
     """
-    # im1 = np.asarray(im1).astype(np.bool)
-    # im2 = np.asarray(im2).astype(np.bool)
+    im1 = im1.flatten().astype(np.bool)
+    im2 = im2.flatten().astype(np.bool)
 
     if im1.shape != im2.shape:
         raise ValueError("Shape mismatch: im1 and im2 must have the same shape.")
