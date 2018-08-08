dbdabcd907c30d327ebdf2f4ee29a9f16fa62bb6
ADAM0912 <38382656+ADAM0912@users.noreply.github.com>
2018-08-08 16:07:51 +0800

    WebVaa3D

---
+++ hackathon/WebVaa3D/WebVaa3d/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebVaa3d.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
