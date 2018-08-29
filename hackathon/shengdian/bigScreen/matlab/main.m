9127316c98fef3c1643c612c482c5fafc9f5e6cb
shengdianjiang <shengdianjiang@gmail.com>
2018-08-29 22:54:35 +0800

    update bigscreen code

---
+++ hackathon/shengdian/bigScreen/matlab/main.m
@@ -0,0 +1,5 @@
+mouseID=17545;
+DataConvertionFileName='seu_allen_dataconversion.xlsx';
+DataSummaryFileName='annotation_summary_17545_LjLiu_08222018.xlsx';
+%Data Conversion
+ReadDataConversion(DataConvertionFileName);
