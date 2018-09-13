066a335cf1b28b22f21973a8f79e929d76a27534
qiangouyang <511517069@qq.com>
2018-09-13 22:44:48 +0800

    update

--- hackathon/ouyang/type/main.cpp
+++ hackathon/ouyang/type/main.cpp
@@ -11,7 +11,8 @@
 # include "v3d_message.h"
 # include <v3d_interface.h>
 # include <iostream>
-#include  <algorithm>
+# include  <algorithm>
+# include "sort_swc.h"
 
 
 
@@ -24,14 +25,58 @@ void detect_type(V3DPluginCallback2 &callback, QWidget *parent)
      NeuronTree tree1=callback.getSWCTeraFly();
      LandmarkList Markers = callback.getLandmarkTeraFly();
      QList<NeuronSWC> tree1swc=tree1.listNeuron;
-     LandmarkList result;
-     vector<int> kinds;
-     for (int i=0;i<tree1swc.size();i++)
+     LandmarkList result1;
+
+     V3DLONG rootid=VOID;
+     V3DLONG thres=100;
+     QList<NeuronSWC> result;
+     QList<NeuronSWC> neuron_nt;
+     neuron_nt.clear();
+     if (SortSWC(tree1swc, result ,rootid, thres))
      {
-       kinds.push_back(tree1swc.at(i).type);
+         neuron_nt=result;
      }
+     tree1swc=neuron_nt;
+     NeuronTree n_t;
+     QHash <int, int> hash_nt;
+
+     for(V3DLONG j=0; j<tree1swc.size();j++){
+         hash_nt.insert(tree1swc[j].n, j);
+     }
+     n_t.listNeuron=tree1swc;
+     n_t.hashNeuron=hash_nt;
+     tree1=n_t;
+
+     vector<int> ids;
+     // Reorder tree ids so that neuron.at(i).n=i+1
+     for(V3DLONG i=0;i<tree1swc.size();i++)
+     {
+         ids.push_back(tree1swc.at(i).n);
+     }
+     for(V3DLONG i=0;i<tree1swc.size();i++)
+     {
+         tree1swc[i].n=i+1;
+         if(tree1swc.at(i).pn !=-1)
+         {
+             tree1swc[i].pn=find(ids.begin(), ids.end(),tree1swc.at(i).pn) - ids.begin()+1;
+         }
+     }
+     /*ids.clear();
+     for (int i=0;i<tree1swc.size();i++){
+         int cc=tree1swc.at(i).n;
+         ids.push_back(cc);
+     }
+     NeuronTree n_t;
+     QHash <int, int> hash_nt;
+
+     for(V3DLONG j=0; j<tree1swc.size();j++){
+         hash_nt.insert(tree1swc[j].n, j);
+     }
+     n_t.listNeuron=tree1swc;
+     n_t.hashNeuron=hash_nt;
+     tree1=n_t;
      //for(int i=100;i<150;i++){
-     //cout<<"-------------================-"<<kinds.at(i)<<endl;}
+     //cout<<"-------------================-"<<tree1swc.at(i).n<<endl;}
      /*int temp;
      int j=0;
      for(int i=j+1;i<kinds.size();i++)
@@ -42,8 +87,14 @@ void detect_type(V3DPluginCallback2 &callback, QWidget *parent)
              kinds.at(i)=temp;
          }
      }*/
+     vector<int> kinds;
+     for (int i=0;i<tree1swc.size();i++)
+     {
+       kinds.push_back(tree1swc.at(i).type);
+     }
+     int max = *max_element(kinds.begin(),kinds.end());
      int ct=0;
-     for (int i=0;i<20;i++)
+     for (int i=0;i<max;i++)
      {
          for (int j=0;j<kinds.size();j++)
          {
@@ -54,15 +105,10 @@ void detect_type(V3DPluginCallback2 &callback, QWidget *parent)
              }
          }
      }
-     /*int ct=0;
-     for(int i=j+1;i<kinds.size();i++){
-         if(kinds.at(j) != kinds.at(i)) ct++;
-     }*/
-     cout<<"---------------the number of neuron points: "<<kinds.size()<<endl;
      cout<<"---------------the kinds number of neuron type: "<<ct<<endl;
 
      //get the childslist
-     V3DLONG neuronNum = tree1.listNeuron.size();
+     /*V3DLONG neuronNum = tree1.listNeuron.size();
      childs = QVector< QVector<V3DLONG> >(neuronNum, QVector<V3DLONG>() );
      for (V3DLONG i=0;i<neuronNum;i++)
      {
@@ -71,22 +117,58 @@ void detect_type(V3DPluginCallback2 &callback, QWidget *parent)
          childs[tree1.hashNeuron.value(par)].push_back(i);
      }
      vector<int> suspoint;
-     for (int i=0;i<tree1swc.size();i++)
-     {
-         if (childs[i].size()==2)
+     //for (int i=0;i<tree1swc.size();i++)
+     //{
+         /*if (childs[i].size()==2)
          {
              int child1=childs[i][0];
              int child2=childs[i][1];
              if(tree1swc.at(i).type != tree1swc.at(child1).type) suspoint.push_back(child1);
              else if(tree1swc.at(i).type != tree1swc.at(child2).type) suspoint.push_back(child2);
          }
-         else if(childs[i].size()==1)
+         if(childs[i].size()==1)
          {
              int child1=childs[i][0];
+             int type1=tree1swc.at(i).type; int type2=tree1swc.at(child1).type;
+             if (type1 != type2)
+             suspoint.push_back(child1);
              if(tree1swc.at(i).type != tree1swc.at(child1).type) {suspoint.push_back(child1);}
          }
+       }*/
+     /*vector<int> suspoint;
+     for (int i=0;i<tree1swc.size();i++)
+      {
+         int child1=childs[i][0];
+         int type1=tree1swc.at(i).type; int type2=tree1swc.at(child1).type;
+         if (type1 == type2)
+         suspoint.push_back(child1);
+
+       }*/
+
+     // use iterator to find the points having different types
+     vector<int> suspoint;
+     for (int i=0;i<tree1swc.size();i++){
+         int type1=tree1swc.at(i).type;
+         int pt=tree1swc.at(i).pn;
+         if(pt!=-1)
+         {
+             vector<int>::iterator iter=find(ids.begin(), ids.end(), pt);
+             int a=distance(ids.begin(),iter);
+             int type2=tree1swc.at(a-1).type;
+             if(type1 != type2)
+                 suspoint.push_back(i);}
      }
-     cout<<"=========the suspoints number: "<<suspoint.size()<<endl;
+
+     /*vector<int> suspoint;
+     int j=0;
+     for (int i=j+1;i<kinds.size();i++){
+         if (kinds.at(i)!=kinds.at(j))
+             suspoint.push_back(i);
+     }*/
+
+
+
+     cout<<"================the suspoints number: "<<suspoint.size()<<endl;
      LocationSimple m;
      for(int i=0;i<suspoint.size();i++)
      {
@@ -96,13 +178,13 @@ void detect_type(V3DPluginCallback2 &callback, QWidget *parent)
              m.color.r = 255;
              m.color.g = 255;
              m.color.b = 255;
-             result.push_back(m);
+             result1.push_back(m);
            }
      for (int i=0;i<Markers.size();i++)
      {
-             result.push_back(Markers.at(i));
+             result1.push_back(Markers.at(i));
      }
-     callback.setLandmarkTeraFly(result);
+     callback.setLandmarkTeraFly(result1);
 
   }
 
