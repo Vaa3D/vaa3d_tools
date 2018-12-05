2c0000493c559c5fdcb9710f5219d238e52903a2
liqi <644274027@qq.com>
2018-12-05 19:51:21 +0800

    Update VR Sever

---
+++ hackathon/liqi/VR_Server/server.h
@@ -0,0 +1,46 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <QObject>
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <QString>
+#include <QStringList>
+#include <QMap>
+
+#include "basic_surf_objs.h"
+
+#define PORT 1234
+
+
+class Server : QObject {
+    Q_OBJECT
+public:
+    explicit Server(QObject* parent = 0);
+    void sendUserList();
+    void sendToAll(const QString&);
+	void sendColorMsg();
+        void sendCreatorMsg();
+	void AddToNTList(QString &msg, int type);
+	bool DeleteSegment(QString segName);
+public slots:
+    void onNewConnection();
+    void onDisconnect();
+    void onReadyRead();
+private:
+    QTcpServer* server;
+    QMap<QTcpSocket*,QString> clients;
+	QMap<QString,int> historyclients;
+	int clientNum;
+	NeuronTree wholeNT;
+	QList<NeuronTree> NeuronList;
+	int sketchNum;
+};
+
+struct clientproperty{
+	int number;
+	QString name;
+	int colortype;
+        bool Creator;
+};
+#endif // SERVER_H
