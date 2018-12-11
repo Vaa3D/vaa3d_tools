5b191c1c873d5794caa9ecf01eb6ce279829770f
Zhi Zhou <alan.zhizhou@gmail.com>
2018-12-10 17:34:44 -0800

    added smoothing function usng GD in refine_swc plugin

---
+++ hackathon/zhi/refine_swc/refine_swc_funcs.h
@@ -0,0 +1,32 @@
+#ifndef __REFINE_SWC_FUNCS_H__
+#define __REFINE_SWC_FUNCS_H__
+
+#include <QtGui>
+#include <v3d_interface.h>
+#include <vector>
+
+using namespace std;
+
+struct Point;
+struct Point
+{
+    double x,y,z,r;
+    V3DLONG type;
+    Point* p;
+    V3DLONG childNum;
+    V3DLONG level,seg_id;
+    QList<float> fea_val;
+};
+
+typedef vector<Point*> Segment;
+typedef vector<Point*> Tree;
+
+NeuronTree refineSWCTerafly(V3DPluginCallback2 &callback,QString fname_img, NeuronTree nt);
+NeuronTree breakSWC(NeuronTree nt, double dist);
+NeuronTree smoothSWCTerafly(V3DPluginCallback2 &callback,QString fname_img, NeuronTree nt, QList<ImageMarker>& break_points);
+double distTwoSegs(NeuronTree nt, Segment * seg);
+void break_path(Segment * seg, double step, QList<ImageMarker>& break_points);
+NeuronTree breakSWC(NeuronTree input, double step, QList<ImageMarker>& break_points);
+
+#endif
+
