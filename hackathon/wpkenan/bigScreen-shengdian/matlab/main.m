39efe9c1add236ccfe2f0e094da1ba9c3662d3e0
wpkenan@gmail.com <llfwyyx@163.com>
2019-01-02 21:03:55 +0800

    delete

--- hackathon/wpkenan/bigScreen-shengdian/matlab/main.m
+++
@@ -1,5 +0,0 @@
-mouseID=17545;
-DataConvertionFileName='seu_allen_dataconversion.xlsx';
-DataSummaryFileName='annotation_summary_17545_LjLiu_08222018.xlsx';
-%Data Conversion
-ReadDataConversion(DataConvertionFileName);
