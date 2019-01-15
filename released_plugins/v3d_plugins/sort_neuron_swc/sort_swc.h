2004fec9adf29362a6c0773ee6f77118570fea29
pengxie-bioinfo <peng.xie@utdallas.edu>
2019-01-14 17:22:08 -0800

    px_0114

--- released_plugins/v3d_plugins/sort_neuron_swc/sort_swc.h
+++ released_plugins/v3d_plugins/sort_neuron_swc/sort_swc.h
@@ -21,28 +21,28 @@
 
 using namespace std;
 
-class MessageBox : public QMessageBox
-{
-public:
-    int timeout;
-    bool autoClose;
-    int currentTime;
+//class MessageBox : public QMessageBox
+//{
+//public:
+//    int timeout;
+//    bool autoClose;
+//    int currentTime;
 
-    void MessageBox::showEvent ( QShowEvent * event ) {
-        currentTime = 0;
-        if (autoClose) {
-        this->startTimer(1000);
-        }
-    }
+//    void MessageBox::showEvent ( QShowEvent * event ) {
+//        currentTime = 0;
+//        if (autoClose) {
+//        this->startTimer(1000);
+//        }
+//    }
 
-    void MessageBox::timerEvent(QTimerEvent *event)
-    {
-        currentTime++;
-        if (currentTime>=timeout) {
-        this->done(0);
-        }
-    }
-};
+//    void MessageBox::timerEvent(QTimerEvent *event)
+//    {
+//        currentTime++;
+//        if (currentTime>=timeout) {
+//        this->done(0);
+//        }
+//    }
+//};
 
 #ifndef VOID
 #define VOID 1000000000
