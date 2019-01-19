52b2403ee548e8dbddeaa0f1af0029d2d1d68955
hnuwangchao <13137916153@163.com>
2019-01-19 17:25:13 +0800

    new branch points detection method

---
+++ hackathon/Chao_Wang/Branch/branch_detection_model.cpp
@@ -0,0 +1,6 @@
+#include "branch_detection_model.h"
+
+Branch_Detection_Model::Branch_Detection_Model()
+{
+
+}
---
+++ hackathon/Chao_Wang/Branch/branch_detection_model.h
@@ -0,0 +1,11 @@
+#ifndef BRANCH_DETECTION_MODEL_H
+#define BRANCH_DETECTION_MODEL_H
+
+
+class Branch_Detection_Model
+{
+public:
+    Branch_Detection_Model();
+};
+
+#endif // BRANCH_DETECTION_MODEL_H
