390954a59a41c682ffbc8c1069bcb24179d763cd
Zhi Zhou <alan.zhizhou@gmail.com>
2019-02-27 16:17:20 -0800

    neuron completeness v1.2: add markers to indicate wrong types and large gap segments

--- hackathon/zhi/neuron_completeness/neuron_completeness_plugin.cpp
+++ hackathon/zhi/neuron_completeness/neuron_completeness_plugin.cpp
@@ -40,9 +40,13 @@ void TestPlugin::domenu(const QString &menu_name, V3DPluginCallback2 &callback, 
         NeuronTree nt = openDlg->nt;
         QList<NeuronSWC> sorted_neuron;
         SortSWC(nt.listNeuron, sorted_neuron ,VOID, 0);
+
+        LandmarkList markerlist;
+
         QHash<int,int> map_type;
         QMultiMap<int, QList<NeuronSWC> > multi_neurons;
         int first = 0;
+        int cur_type = -1;
         for(V3DLONG i=0; i<sorted_neuron.size();i++)
         {
             if(sorted_neuron.at(i).pn == -1)
@@ -57,29 +61,76 @@ void TestPlugin::domenu(const QString &menu_name, V3DPluginCallback2 &callback, 
             if(!map_type.count(sorted_neuron.at(i).type))
             {
                 map_type[sorted_neuron.at(i).type] = 1;
+                if(sorted_neuron.at(i).type!=1 && sorted_neuron.at(i).type!=2 && sorted_neuron.at(i).type!=3)
+                {
+                    LocationSimple t;
+                    t.x = sorted_neuron.at(i).x;
+                    t.y = sorted_neuron.at(i).y;
+                    t.z = sorted_neuron.at(i).z;
+                    t.color.r = 255;t.color.g = 0; t.color.b = 0;
+                    markerlist.push_back(t);
+                }
+            }else
+            {
+                if(sorted_neuron.at(i).type!=1 && sorted_neuron.at(i).type!=2 && sorted_neuron.at(i).type!=3 && sorted_neuron.at(i).type !=cur_type)
+                {
+                    LocationSimple t;
+                    t.x = sorted_neuron.at(i).x;
+                    t.y = sorted_neuron.at(i).y;
+                    t.z = sorted_neuron.at(i).z;
+                    t.color.r = 255;t.color.g = 0; t.color.b = 0;
+                    markerlist.push_back(t);
+                }
             }
+            cur_type = sorted_neuron.at(i).type;
         }
         multi_neurons.insert(sorted_neuron.size()-first,sorted_neuron.mid(first,sorted_neuron.size()-first));
         QList<double> dist;
-        dist.push_back(0);
-        QMultiMap<int, QList<NeuronSWC> >::iterator it = multi_neurons.end()-1;
-        QList<NeuronSWC> largest_neuron = it.value();
-
-        for (it = multi_neurons.end()-2; it != multi_neurons.begin()-1; --it)
+        QVector<QVector<double> > matrix_dis(multi_neurons.size(),QVector<double>(multi_neurons.size(),MAX_DOUBLE));
+        int row =0;
+        for (QMultiMap<int, QList<NeuronSWC> >::iterator it1 = multi_neurons.end()-1; it1 != multi_neurons.begin()-1; --it1)
         {
-            QList<NeuronSWC> cur = it.value();
-            double minD = minDist(largest_neuron,cur);
+            QList<NeuronSWC> neuron1= it1.value();
+            double minD = MAX_DOUBLE;
+            int col=0;
+            for (QMultiMap<int, QList<NeuronSWC> >::iterator it2 = multi_neurons.end()-1; it2 != multi_neurons.begin()-1; --it2)
+            {
+                if(it1 != it2)
+                {
+                    if(matrix_dis[row][col] != MAX_DOUBLE)
+                        minD = MIN(minD,matrix_dis[row][col]);
+                    else
+                    {
+                        QList<NeuronSWC> neuron2 = it2.value();
+                        double cur_dis = minDist(neuron1,neuron2);
+                        matrix_dis[row][col] = cur_dis;
+                        matrix_dis[col][row] = cur_dis;
+                        minD = MIN(minD,cur_dis);
+                    }
+                }
+                col++;
+            }
+            row++;
             dist.push_back(minD);
+            if(minD>20)
+            {
+                LocationSimple t;
+                t.x = neuron1.at(0).x;
+                t.y = neuron1.at(0).y;
+                t.z = neuron1.at(0).z;
+                t.color.r = 0;t.color.g = 0; t.color.b = 255;
+                markerlist.push_back(t);
+            }
         }
 
         QString info_tree;
         int index = 1;
-        for (it = multi_neurons.end()-1; it != multi_neurons.begin()-1; --it)
+        for (QMultiMap<int, QList<NeuronSWC> >::iterator it = multi_neurons.end()-1; it != multi_neurons.begin()-1; --it)
         {
             QList<NeuronSWC> cur = it.value();
             double cur_dist = dist.front(); dist.erase(dist.begin());
             double cur_per = 100*(double)cur.size()/(double)sorted_neuron.size();
-            info_tree += QString("neuron-tree %1 : percentage is %2%,  distance is %3<br>").arg(index++).arg(QString::number(cur_per,'f',2)).arg(QString::number(cur_dist,'f',2));
+            info_tree += QString("neuron-tree %1 : percentage is %2%,  gap is %3<br>").arg(index++).arg(QString::number(cur_per,'f',2)).arg(QString::number(cur_dist,'f',2));
         }
         QMessageBox infoBox;
         infoBox.setText("Neuron completeness:");
@@ -99,6 +150,31 @@ void TestPlugin::domenu(const QString &menu_name, V3DPluginCallback2 &callback, 
                     .arg(info_type.toStdString().c_str()));
 
         infoBox.exec();
+        if(markerlist.size() != 0)
+        {
+            QList <V3dR_MainWindow *> cur_list_3dviewer = callback.getListAll3DViewers();
+            bool b_found=false;
+            int i;
+            for (i=0; i<cur_list_3dviewer.size(); i++)
+            {
+                QString curname = callback.getImageName(cur_list_3dviewer[i]).remove("3D View [").remove("]");
+                if(curname == openDlg->file_name)
+                {
+                    b_found = true;
+                    break;
+                }
+            }
+
+            V3dR_MainWindow *cur_win;
+            if(b_found)
+                cur_win = cur_list_3dviewer[i];
+            else
+                cur_win = callback.open3DViewerForSingleSurfaceFile(openDlg->file_name);
+
+            if(cur_win)
+                callback.setHandleLandmarkList_Any3DViewer(cur_win,markerlist);
+        }
+
 	}
 	else
 	{
