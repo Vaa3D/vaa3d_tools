ca37450fb29c00f2469dbdf5f6bd58c0aac7a623
MKRangers <hsienchik@alleninstitute.org>
2019-03-06 18:21:35 -0800

    branch crop plugin naming conflict in git index

---
+++ hackathon/sujunZhao/Branch_Crop/branch_crop_plugin.cpp
@@ -0,0 +1,66 @@
+/* branch_crop_plugin.cpp
+ * This is a test plugin, you can use it as a demo.
+ * 2018-12-19 : by YourName
+ */
+ 
+#include "v3d_message.h"
+#include <vector>
+#include "branch_crop_plugin.h"
+#include <stdio.h>
+#include <iostream>
+#include <v3d_interface.h>
+//#include "../get_terminal.h"
+using namespace std;
+Q_EXPORT_PLUGIN2(branch_crop, TestPlugin)
+
+//void get_branches(V3DPluginArgList & input, V3DPluginArgList & output, V3DPluginCallback2 & callback);
+//QList<int> get_branch_points(NeuronTree nt, bool include_root);
+
+QStringList TestPlugin::menulist() const
+{
+	return QStringList() 
+        <<tr("get_branch_sample")
+		<<tr("about");
+}
+
+QStringList TestPlugin::funclist() const
+{
+	return QStringList()
+        <<tr("get_branch_point")
+        <<tr("branch_point_sample")
+		<<tr("help");
+}
+
+void TestPlugin::domenu(const QString &menu_name, V3DPluginCallback2 &callback, QWidget *parent)
+{
+    if (menu_name == tr("get_branch_sample"))
+	{
+         v3d_msg("To be implemented.");
+	}
+	else
+	{
+		v3d_msg(tr("This is a test plugin, you can use it as a demo.. "
+			"Developed by YourName, 2018-12-19"));
+	}
+}
+
+bool TestPlugin::dofunc(const QString & func_name, const V3DPluginArgList & input, V3DPluginArgList & output, V3DPluginCallback2 & callback,  QWidget * parent)
+{
+//	vector<char*> infiles, inparas, outfiles;
+//	if(input.size() >= 1) infiles = *((vector<char*> *)input.at(0).p);
+//	if(input.size() >= 2) inparas = *((vector<char*> *)input.at(1).p);
+//	if(output.size() >= 1) outfiles = *((vector<char*> *)output.at(0).p);
+
+    if (func_name == tr("get_branch_point"))
+	{
+        get_branches(input,output,callback);
+	}
+    else if (func_name == tr("branch_point_sample"))
+	{
+		v3d_msg("To be implemented.");
+	}
+    else if (func_name == tr("help")){
+        v3d_msg("To be implemented.");
+    }
+
+}
