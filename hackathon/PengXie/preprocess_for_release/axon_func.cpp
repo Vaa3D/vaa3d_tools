dd66a6e16bfe9e73cfe49f50c2f397d703291e12
pengxie-bioinfo <peng.xie@utdallas.edu>
2019-03-13 18:46:36 -0700

    preprocess_for_release

---
+++ hackathon/PengXie/preprocess_for_release/axon_func.cpp
@@ -0,0 +1,447 @@
+//functions for axon analysis
+//by Peng Xie
+//2018-08-16
+
+#ifdef DISABLE_V3D_MSG
+#define DISABLE_V3D_MSG
+#endif
+
+#define DEFAULT 1000000000
+
+#include "axon_func.h"
+#include "crop_swc_main.h"
+
+#define MAXSIZE 1000000
+
+QList<int> match_axon_and_lpa(NeuronTree axon, NeuronTree lpa){
+
+    QList<int> lpa_id;
+
+    // 1.1 Load whole axon data
+    QList<int> plist;
+    QList<int> nlist;
+    for(int i=0; i<axon.listNeuron.size();i++){
+        NeuronSWC node=axon.listNeuron.at(i);
+        plist.append(node.pn);
+        nlist.append(node.n);
+    }
+
+    // 2 Check whether match
+    bool matched = 1; // Whether the id's of axon and lpa matches
+    for(int i=0; i<lpa.listNeuron.size();i++){
+        NeuronSWC node=lpa.listNeuron.at(i);
+        int i_in_axon = nlist.indexOf(node.n);
+        if(i_in_axon == -1){  // lpa id doesn't exsit in axon: break the loop.
+            matched = 0;
+            break;
+        }
+        NeuronSWC node_axon = axon.listNeuron.at(i_in_axon);  // The matching node in axon
+        if(node.x != node_axon.x || node.y != node_axon.y || node.z != node_axon.z){
+            matched = 0;
+            break;
+        }
+        lpa_id.append(node.n);
+    }
+
+    if(matched){
+        return lpa_id;
+    }
+    else{
+        printf("ID's of axon and lpa do not match!\n");
+        int soma = get_soma(axon);
+        lpa_id.clear();
+        QList<int> lpa = find_long_axon(axon, soma);
+        for(int i=0; i<lpa.size(); i++){
+            lpa_id.append(axon.listNeuron.at(lpa.at(i)).n);
+        }
+        return lpa_id;
+    }
+}
+QList <int> find_long_axon(NeuronTree nt, int soma){
+
+    printf("welcome to use find_long_axon\n");
+    const int N = nt.listNeuron.size();
+    QList<int> name_list;
+    for(int i=0; i<N; i++){
+        name_list.append(nt.listNeuron.at(i).n);
+    }
+
+//    list<int> children[MAXSIZE];
+//    for(int i=0; i<N; i++){
+//        NeuronSWC node = nt.listNeuron.at(i);
+//        int pid = name_list.lastIndexOf(node.pn);
+//        if(pid<0){
+//            continue;
+//        }
+//        children[pid].push_back(i);
+//    }
+    QVector<QVector<V3DLONG> > children;
+    children = QVector< QVector<V3DLONG> >(N, QVector<V3DLONG>() );
+    for (V3DLONG i=0;i<N;i++)
+    {
+        int pid = name_list.lastIndexOf(nt.listNeuron.at(i).pn);
+        if (pid<0) continue;
+        children[pid].push_back(i);
+    }
+
+
+    // 1. find longest path from soma
+    // DFS
+    // Initialization
+    int pid=soma;
+    QList<double> distance;
+    for(int i=0;i<N; i++){distance.append(-1);}
+    QStack<int> pstack;
+    pstack.push(pid);
+    bool is_push = false;
+    distance[pid]=0;
+
+    // DFS search
+    while(!pstack.isEmpty()){
+        is_push = false;
+        pid = pstack.top();
+        // whether exist unvisited children of pid
+        // if yes, push child to stack;
+//        for(int i=0; i<nt.listNeuron.size();i++){  // This loop can be more efficient, improve it later!
+        for(V3DLONG *i=children[pid].begin(); i!=children[pid].end(); ++i)
+        {
+            if((nt.listNeuron.at(*i).pn)==nt.listNeuron.at(pid).n && distance.at(*i)==(-1.0)){
+                pstack.push(*i);
+                distance[*i]=distance.at(pid)+computeDist2(nt.listNeuron.at(pid), nt.listNeuron.at(*i), 0.2,0.2,1);
+                is_push=true;
+                break;
+            }
+        }
+        // else, pop pid
+        if(!is_push){
+            pstack.pop();
+        }
+    }
+    QList<double> sort_distance = distance;
+    qSort(sort_distance);
+    double longest_distance = sort_distance.last();
+    int endpoint = distance.lastIndexOf(longest_distance);
+    cout<<"Longest distance:\t"<<distance.at(endpoint)<<"\t"<<"node "<<nt.listNeuron.at(endpoint).n <<endl;
+
+    // 2. Return a list of node ids of the long projection axon
+    QList <int> lpa;
+    int cur_id = endpoint;
+    lpa.prepend(cur_id);
+    while(nt.listNeuron.at(cur_id).type!=1){
+        cur_id = name_list.lastIndexOf(nt.listNeuron.at(cur_id).pn); // Move to the parent node
+        if(cur_id==name_list.lastIndexOf(nt.listNeuron.at(cur_id).pn)){break;} // check self loop; should not exist;
+        lpa.prepend(cur_id);
+        if(nt.listNeuron.at(cur_id).pn<0){  // Reached root
+            break;
+        }
+    }
+    return lpa;
+}
+QList<double> arbor_distribution(QString whole_axon_swc, QString lpa_swc){
+
+    printf("Welcome to use arbor_distribution\n");
+
+    // 1. Load data
+
+    // 1.1 Load whole axon data
+    NeuronTree axon = readSWC_file(whole_axon_swc);
+    QList<int> plist;
+    QList<int> nlist;
+    const int N = axon.listNeuron.size();
+    for(int i=0; i<axon.listNeuron.size();i++){
+        NeuronSWC node=axon.listNeuron.at(i);
+        plist.append(node.pn);
+        nlist.append(node.n);
+    }
+    // 1.2 Load lpa
+    NeuronTree lpa = readSWC_file(lpa_swc);
+    // 1.3 Match axon and lpa
+    QList<int> lpa_id = match_axon_and_lpa(axon, lpa);
+    lpa = get_subtree_by_name(axon, lpa_id);
+
+    // 2. Branch size at every node of lpa
+    QList<double> lpa_size;
+    for(int i=0; i<lpa.listNeuron.size();i++){lpa_size.append(0);}
+    double branch_size;
+    QVector<QVector<V3DLONG> > children;
+    children = QVector< QVector<V3DLONG> >(N, QVector<V3DLONG>() );
+    for (V3DLONG i=0;i<N;i++)
+    {
+        int pid = nlist.lastIndexOf(axon.listNeuron.at(i).pn);
+        if (pid<0) continue;
+        children[pid].push_back(i);
+    }
+
+    for(int i=0; i<axon.listNeuron.size(); i++){
+        branch_size=0;
+        NeuronSWC node = axon.listNeuron.at(i);
+        // find the start of a non-long-projection branch
+        if(lpa_id.indexOf(node.n) == (-1) && lpa_id.indexOf(node.pn) != (-1)){
+            // Initialization
+            int pid=i;
+            QList<int> visited;
+            for(int j=0;j<axon.listNeuron.size(); j++){visited.append(0);}
+            QStack<int> pstack;
+            pstack.push(pid);
+            bool is_push = false;
+            visited[pid]=1;
+
+            // DFS using stack
+            while(!pstack.isEmpty()){
+                is_push = false;
+                pid = pstack.top();
+                // whether exist unvisited children of pid
+                // if yes, push child to stack;
+//                for(int j=0; j<axon.listNeuron.size();j++){  // This loop can be more efficient, improve it later!
+                for(V3DLONG *j=children[pid].begin(); j!=children[pid].end(); ++j){
+                    if((axon.listNeuron.at(*j).pn)==nlist.at(pid) && visited.at(*j)==0){
+                        pstack.push(*j);
+                        visited[*j]=1;
+//                        branch_size+=1;
+                        
+                        is_push=true;
+                        break;
+                    }
+                }
+                // else, pop pid
+                if(!is_push){
+                    pstack.pop();
+                }
+            }
+
+            // Add to the parent node on lpa
+            int pid_lpa = lpa_id.indexOf(node.pn);
+            if(pid_lpa==-1){cout<<"warning\n";break;}
+            lpa_size[pid_lpa] += branch_size;
+        }
+    }
+
+    cout<<3<<endl;
+    // 3. Branch density at every node of lpa
+    QList<double> lpa_density;
+    for(int i=0; i<lpa.listNeuron.size();i++){
+        lpa_density.append(lpa_size.at(i));
+//        lpa_density.append(0);
+//        for(int j=0; j<lpa.listNeuron.size();i++){
+//            lpa_density[i] += lpa_size.at(j) * exp(-pow(i-j, 2));
+//        }
+    }
+
+    return lpa_density;
+}
+bool export_branch_distribution(QList<double> lpa_density, QString output_fileName){
+    FILE * fp=0;
+    fp = fopen((char *)qPrintable(output_fileName), "wt");
+    for(int i=0; i<lpa_density.size(); i++){
+        fprintf(fp, "%d\t%f\n", i, lpa_density.at(i));
+    }
+    fclose(fp);
+    return 1;
+}
+bool axon_retype(QString whole_axon_swc, QString lpa_swc, QString output_swc, bool proximal_distal){
+
+    printf("Welcome to use axon_retype\n");
+
+    // 0. File check
+    if(!fexists(whole_axon_swc)){
+        return 0;
+    }
+    if (!(whole_axon_swc.endsWith(".swc") || whole_axon_swc.endsWith(".SWC")
+          || whole_axon_swc.endsWith(".eswc") || whole_axon_swc.endsWith(".ESWC"))){
+        printf("Error: Input file is not SWC.\n");
+        return 0;
+    }
+    if(output_swc.size()==0){
+        if (whole_axon_swc.endsWith(".swc") || whole_axon_swc.endsWith(".SWC")){
+            output_swc = whole_axon_swc.left(whole_axon_swc.length()-4)+".retype.swc";
+        }
+        if (whole_axon_swc.endsWith(".eswc") || whole_axon_swc.endsWith(".ESWC")){
+            output_swc = whole_axon_swc.left(whole_axon_swc.length()-5)+".retype.swc";
+        }
+    }
+    QString proximal_swc;
+    QString distal_swc;
+    if (output_swc.endsWith(".swc") || output_swc.endsWith(".SWC")){
+        proximal_swc = output_swc.left(output_swc.length()-11)+".proximal_axon.swc";
+        distal_swc = output_swc.left(output_swc.length()-11)+".distal_axon.swc";
+    }
+    else if (whole_axon_swc.endsWith(".eswc") || whole_axon_swc.endsWith(".ESWC")){
+        proximal_swc = output_swc.left(output_swc.length()-12)+".proximal_axon.eswc";
+        distal_swc = output_swc.left(output_swc.length()-12)+".distal_axon.eswc";
+    }
+
+    // 1. Load data
+    // 1.1 Load whole axon data
+    NeuronTree axon = readSWC_file(whole_axon_swc);
+    QList<int> plist;
+    QList<int> nlist;
+    for(int i=0; i<axon.listNeuron.size();i++){
+        NeuronSWC node=axon.listNeuron.at(i);
+        plist.append(node.pn);
+        nlist.append(node.n);
+    }
+    // 1.2 Load lpa
+    NeuronTree lpa = readSWC_file(lpa_swc);
+    // 1.3 Match axon and lpa
+    QList<int> lpa_id = match_axon_and_lpa(axon, lpa);
+    lpa = get_subtree_by_name(axon, lpa_id);
+
+    QList<double> path_distance;
+    path_distance.append(0);
+    double cur_distance = 0;
+    for(int i=1; i<lpa.listNeuron.size(); i++){
+        cur_distance += computeDist2(lpa.listNeuron.at(i-1), lpa.listNeuron.at(i), 0.2,0.2,1);
+        path_distance.append(cur_distance);
+    }
+    double total_path_distance = cur_distance;
+    qDebug()<<"Long projection axon path distance:\t"<<total_path_distance;
+
+    QList<bool> is_proximal;
+    QList<bool> is_distal;
+    NeuronTree proximal_axon;
+    NeuronTree distal_axon;
+    for(int i=0; i<lpa.listNeuron.size();i++){
+        NeuronSWC node=lpa.listNeuron.at(i);
+        if((path_distance.at(i)/total_path_distance)<0.2){
+            is_proximal.append(1);
+//            lpa.listNeuron[i].type=0;
+            proximal_axon.listNeuron.append(node);
+        }
+        else{
+            is_proximal.append(0);
+        }
+        if((path_distance.at(i)/total_path_distance)>0.7){
+            is_distal.append(1);
+//            lpa.listNeuron[i].type=1;
+            distal_axon.listNeuron.append(node);
+        }
+        else{
+            is_distal.append(0);
+        }
+    }
+//    export_listNeuron_2swc(lpa.listNeuron, "proximal_distal.swc");
+
+    // 2. label non-long-projection axon braches
+    // 2.1 default mode: retype axon clusters as proximal or distal
+    int proximal_retype = 5;
+    int distal_retype = 6;
+    // 2.2 optional model: label branch with a different type if passes size_thres.
+    int cur_retype = 2;
+    int size_thres = 10;
+    QList<int> branch_ind;
+    const int N=axon.listNeuron.size();
+//    list<int> children[MAXSIZE];
+//    for(int i=0; i<N; i++){
+//        NeuronSWC node = axon.listNeuron.at(i);
+//        int pid = nlist.lastIndexOf(node.pn);
+//        if(pid<0){continue;}
+//        children[pid].push_back(i);
+//    }
+    QVector<QVector<V3DLONG> > children;
+    children = QVector< QVector<V3DLONG> >(N, QVector<V3DLONG>() );
+    for (V3DLONG i=0;i<N;i++)
+    {
+        int pid = nlist.lastIndexOf(axon.listNeuron.at(i).pn);
+        if (pid<0) continue;
+        children[pid].push_back(i);
+    }
+
+    for(int i=1; i<axon.listNeuron.size(); i++){
+        branch_ind.clear();
+        NeuronSWC node = axon.listNeuron.at(i);
+        // find the start of a non-long-projection branch
+        if(lpa_id.indexOf(node.n) == (-1) && lpa_id.indexOf(node.pn) != (-1)){
+
+            // Initialization
+            int pid=i;
+            QList<int> visited;
+            for(int j=0;j<axon.listNeuron.size(); j++){visited.append(0);}
+            QStack<int> pstack;
+            pstack.push(pid);
+            bool is_push = false;
+            visited[pid]=1;
+            branch_ind.append(pid);
+
+            // DFS using stack
+            while(!pstack.isEmpty()){
+                is_push = false;
+                pid = pstack.top();
+                // whether exist unvisited children of pid
+                // if yes, push child to stack;
+//                for(int j=0; j<axon.listNeuron.size();j++){  // This loop can be more efficient, improve it later!
+                for(V3DLONG *j=children[pid].begin(); j!=children[pid].end(); ++j)
+                {
+                    if((axon.listNeuron.at(*j).pn)==nlist.at(pid) && visited.at(*j)==0){
+                        pstack.push(*j);
+                        visited[*j]=1;
+                        branch_ind.append(*j);
+                        is_push=true;
+                        break;
+                    }
+                }
+                // else, pop pid
+                if(!is_push){
+                    pstack.pop();
+                }
+            }
+
+        }
+        // retype the branch
+        if(branch_ind.size()>0){
+            // Mode 1
+            if(proximal_distal){
+                int branch_head = branch_ind.at(0);
+                int branch_at_lpa = lpa_id.indexOf(plist.at(branch_head));
+                // Proximal cluster
+                if(is_proximal.at(branch_at_lpa)){
+                    cur_retype = proximal_retype;
+                }
+                // Distal cluster
+                else{
+                    cur_retype = distal_retype;
+                }
+                for(int j=0; j<branch_ind.size(); j++){
+                    axon.listNeuron[branch_ind.at(j)].type=cur_retype;
+                }
+            }
+            // Mode 2
+            else{
+                if(branch_ind.size()>size_thres){
+                    cur_retype++;
+                    for(int j=0; j<branch_ind.size(); j++){
+                        axon.listNeuron[branch_ind.at(j)].type=cur_retype;
+                    }
+                }
+            }
+        }
+        // proximal and distal axon
+        if(branch_ind.size()>0){
+            int branch_head = branch_ind.at(0);
+            int branch_at_lpa = lpa_id.indexOf(plist.at(branch_head));
+            // Proximal cluster
+            if(is_proximal.at(branch_at_lpa)){
+                for(int j=0; j<branch_ind.size(); j++){
+                    NeuronSWC node = axon.listNeuron.at(branch_ind.at(j));
+                    proximal_axon.listNeuron.append(node);
+                }
+            }
+            // Distal cluster
+            if(is_distal.at(branch_at_lpa)){
+                for(int j=0; j<branch_ind.size(); j++){
+                    NeuronSWC node = axon.listNeuron.at(branch_ind.at(j));
+                    distal_axon.listNeuron.append(node);
+                }
+            }
+        }
+    }
+//    proximal_axon = missing_parent(proximal_axon);
+//    proximal_axon = my_SortSWC(proximal_axon, proximal_axon.listNeuron.at(0).n, 0);
+//    writeSWC_file(proximal_swc, proximal_axon);
+    crop_swc(whole_axon_swc, proximal_swc, 200, 0, 0, 1);
+    distal_axon = missing_parent(distal_axon);
+    distal_axon = my_SortSWC(distal_axon, distal_axon.listNeuron.at(0).n, 0);
+    writeSWC_file(output_swc, axon);
+    writeSWC_file(distal_swc, distal_axon);
+
+    return 1;
+}
---
+++ hackathon/PengXie/preprocess_for_release/neurite_analysis_main.cpp
@@ -0,0 +1,263 @@
+//neurite_analysis_main.cpp
+//extract different types of neurite from a single tree: dendrite; long projection axon; axon clusters
+//by Peng Xie
+//2018-08-13
+
+#ifdef DISABLE_V3D_MSG
+#define DISABLE_V3D_MSG
+#endif
+
+#define DEFAULT 1000000000
+
+#include "neurite_analysis_main.h"
+
+#if !defined(Q_OS_WIN32)
+#include "unistd.h"
+#endif
+
+#if defined(Q_OS_WIN32)
+#include "getopt_tool.h"
+#include "io.h"
+#endif
+
+
+NeuronTree return_long_axon(NeuronTree nt, int soma, bool color_tree){
+    QList<int> idlist = find_long_axon(nt, soma);
+    if(color_tree){
+        nt = color_subtree_by_id(nt, idlist);
+    }
+    else{
+        nt = get_subtree_by_id(nt, idlist);
+    }
+    return nt;
+}
+NeuronTree return_axon(NeuronTree nt, int soma){
+
+    printf("Welcome to use return_axon.\n");
+    // 1. Get axon subtrees starting from soma
+    // 1.1 extract axon nodes
+    NeuronTree new_tree = get_subtree_by_type(nt, 2);
+    QList<int> name_list;
+    for(int i=0; i<nt.listNeuron.size();i++){
+        NeuronSWC node = nt.listNeuron.at(i);
+        name_list.append(node.n);
+    }
+    // 1.2 Add axon-connected dendrite segments to axon subtrees
+    // Find out whether any axon node connected to dendrite
+    QList<int> a2d_ind;
+    for(int i=0; i<nt.listNeuron.size(); i++){
+        NeuronSWC node = nt.listNeuron.at(i);
+        int pn_ind = name_list.indexOf(node.pn);
+        if(pn_ind<0){continue;}
+        if((nt.listNeuron.at(pn_ind).type==3) && (node.type==2)){
+            a2d_ind.append(i);
+            printf("Node %d is connected to dendrite.\n", i);
+        }
+    }
+    // If any, trace back to soma;
+    if(a2d_ind.size()>0){
+        if(a2d_ind.size()>1){printf("Warning: %d axon nodes connected to dendrite!\n", a2d_ind.size());}
+        for(int i=0; i<a2d_ind.size(); i++){
+            bool soma_reached=0;
+            NeuronSWC node = nt.listNeuron.at(a2d_ind.at(i));
+            while(node.pn != -1){
+                int pn_ind = name_list.indexOf(node.pn);
+                node = nt.listNeuron.at(pn_ind);
+                if(node.type==3){new_tree.listNeuron.append(node);}
+                if(node.type==1){soma_reached==1;}
+            }
+            if(!soma_reached){
+                printf("Warning: axon nodes connected to dendrite but cannot trace back to soma!\n");
+            }
+        }
+    }
+
+    nt = single_tree(new_tree, soma);
+    return nt;
+
+}
+NeuronTree return_dendrite(NeuronTree nt, int soma){
+    nt = get_subtree_by_type(nt, 3);
+    nt = single_tree(nt, soma);
+    return nt;
+}
+
+bool neurite_analysis(QString qs_input, QString qs_output, string extract_type){
+
+    printf("welcome to use neurite_analysis\n");
+    if(!fexists(qs_input)){
+        return 0;
+    }
+
+    // 1. Load data
+    NeuronTree nt;
+
+    if (qs_input.endsWith(".swc") || qs_input.endsWith(".SWC") || qs_input.endsWith(".eswc") || qs_input.endsWith(".ESWC"))
+    {
+        nt = readSWC_file(qs_input);
+    }
+
+    if (qs_output.size()==0)
+    {
+        char *tag;
+        if(extract_type == "a"){tag = strdup(".axon");}
+        if(extract_type == "l"){tag = strdup(".long_axon");}
+        if(extract_type == "c"){tag = strdup(".cluster_axon");}
+        if(extract_type == "d"){tag = strdup(".dendrite");}
+        if (qs_input.endsWith(".swc") || qs_input.endsWith(".SWC")){qs_output = qs_input.left(qs_input.length()-4)+tag+".swc";}
+        if (qs_input.endsWith(".eswc") || qs_input.endsWith(".ESWC")){qs_output = qs_input.left(qs_input.length()-5)+tag+".swc";}
+    }
+
+    // 2. Examine input
+    // 2.1 Check soma
+    int soma=get_soma(nt);
+    cout<<soma<<endl;
+    if(soma<0){return 0;}
+    if(nt.listNeuron.at(soma).pn != (-1)){
+        printf("Exit: soma is not root!\n");
+//        nt = my_SortSWC(nt, soma, 0);
+//        writeSWC_file(qs_input+".sorted.swc", nt);
+        return 0;
+    }
+
+    // 3. Extract the specified type of neurite
+    printf("Extracting neurite\n");
+    if(extract_type == "a"){
+        nt=return_axon(nt, soma);
+    }
+    if(extract_type == "l"){
+        nt = return_long_axon(nt, soma, false);
+    }
+    if(extract_type == "d"){nt=return_dendrite(nt, soma);}
+    nt = my_SortSWC(nt, 1, 0);
+
+    writeSWC_file(qs_output, nt);
+    return 1;
+}
+
+bool neurite_analysis_dofunc(const V3DPluginArgList & input, V3DPluginArgList & output)
+{
+
+    //0. read arguments
+    vector<char*>* inlist = (vector<char*>*)(input.at(0).p);
+    vector<char*>* outlist = NULL;
+    vector<char*>* paralist = NULL;
+
+    char *dfile_input = NULL;
+    char *dfile_output = NULL;
+    string extract_type = "a";
+    bool color_tree = 0;
+
+    if(1==1){
+        if(input.size() != 2)
+        {
+            printf("Please specify parameter set.\n");
+            printHelp_neurite_analysis();
+            return false;
+        }
+        paralist = (vector<char*>*)(input.at(1).p);
+
+
+        if (paralist->size()!=1)
+        {
+            printf("Please specify all paramters in one text string.\n");
+            printHelp_neurite_analysis();
+            return false;
+        }
+
+
+        char * paras = paralist->at(0);
+        int argc = 1;
+        enum {kArgMax = 64};
+        char *argv[kArgMax];
+
+        //parsing parameters
+        if (paras)
+        {
+            int len = strlen(paras);
+            for (int i=0;i<len;i++)
+            {
+                if (paras[i]=='#')
+                    paras[i] = '-';
+            }
+
+            char* pch = strtok(paras, " ");
+            while (pch && argc<kArgMax)
+            {
+                argv[argc++] = pch;
+                pch = strtok(NULL, " ");
+            }
+        }
+        else
+            printHelp_neurite_analysis();
+
+        int c;
+        static char optstring[]="hi:o:t:c:";
+        extern char * optarg;
+        extern int optind, opterr;
+        optind = 1;
+        while ((c = getopt(argc, argv, optstring))!=-1)
+        {
+            switch (c)
+            {
+                case 'h':
+                    printHelp_neurite_analysis();
+                    return 0;
+                    break;
+                case 'i':
+                    if (strcmp(optarg,"(null)")==0 || optarg[0]=='-')
+                    {
+                        fprintf(stderr, "Found illegal or NULL parameter for the option -i.\n");
+                        return 1;
+                    }
+                    dfile_input = optarg;
+                    cout << "Input file name:\t" << dfile_input <<endl;
+                    break;
+                case 'o':
+                    if (strcmp(optarg,"(null)")==0 || optarg[0]=='-')
+                    {
+                        fprintf(stderr, "Found illegal or NULL parameter for the option -o.\n");
+                        return 1;
+                    }
+                    dfile_output = optarg;
+                    break;
+                case 't':
+                    if (strcmp(optarg,"(null)")==0 || optarg[0]=='-')
+                    {
+                        fprintf(stderr, "Found illegal or NULL parameter for the option -t.\n");
+                        return 1;
+                    }
+                    extract_type = optarg;
+                    break;
+                case 'c':
+                    if (strcmp(optarg,"(null)")==0 || optarg[0]=='-')
+                    {
+                        fprintf(stderr, "Found illegal or NULL parameter for the option -c.\n");
+                        return 1;
+                    }
+                    color_tree = (atoi(optarg)==1);
+                    break;
+            }
+        }
+    }
+
+    return neurite_analysis(QString(dfile_input), QString(dfile_output), extract_type);
+}
+
+void printHelp_neurite_analysis()
+{
+    printf("\nVaa3D plugin: modifiled pre-processing step for BlastNeuron pipeline, including: \n");
+    printf("\t1) delete the branches in a neuron which have a length smaller the prune size ( by default the prune size is 5 %% of neuron/tree diameter.\n");
+    printf("\t2) resample the neurons along the segments with the step size (default: 1). \n");
+    printf("\t3) sort the neurons along the segments with the step size (deyyplt: 1). \n");
+    printf("\t4) rotate the neuron to align its longest dimension to x axis\n");
+    printf("\t#i <neuron_filename> :   input neuron structure (.swc) name\n");
+    printf("\t#o <output_filename> :   output file name.\n");
+    printf("\t                         if not specified, it is \"inputName.extract_type.swc\"\n");
+    printf("\t#t <extract_type> :  which neurite type to extract.\n");
+    printf("\t                         if not specified, long projection axon will be extracted\n");
+    printf("\t#c <color_tree> :  whether to label the specified type by color.\n");
+    printf("\t                         if not specified, only return the subtree\n");
+    printf("\t                         if specified as 1, whole neuron will be returned, with the specified part showing white coloor\n");
+    printf("Usage: vaa3d -x preprocess -f neurite_analysis -p \"#i input.swc #o result.swc #t a #c 1\"\n");
+}
