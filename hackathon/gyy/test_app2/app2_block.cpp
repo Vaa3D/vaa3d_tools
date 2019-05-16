69f9e88eaee04932ab455c42aa2fe3279aaccd58
balala <balala@localhost.localdomain>
2019-05-16 00:47:43 -0400

    added gyy folder

---
+++ hackathon/gyy/test_app2/app2_block.cpp
@@ -0,0 +1,14 @@
+#include "app2_block.h"
+
+#include "v3d_basicdatatype.h"
+#include "color_xyz.h"
+#include "v3d_message.h"
+#include <iostream>
+#include "block.h"
+using namespace std;
+
+void app2_block(block &b)
+{
+
+
+}
