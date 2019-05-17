bf463e6983a6e42d142d01289a1c620e326e3238
shengdian jiang <37288782+shengdianjiang@users.noreply.github.com>
2019-05-17 17:36:07 +0800

    add code

--- hackathon/XuanZhao/Hackathon_demo1/some_class.h
+++ hackathon/XuanZhao/Hackathon_demo1/some_class.h
@@ -22,6 +22,79 @@ public:
     QString name;
 };
 
+struct PARA_DEMO1
+{
+    int dx;
+    int dy;
+    int dz;
+    PARA_DEMO1()
+    {
+        dx=128;
+        dy=128;
+        dz=64;
+    }
+
+    bool DEMO1()
+    {
+        {
+            QDialog * dialog= new QDialog();
+            dialog->setWindowTitle("get parameters on Hackathon_demo1");
+            //dialog->setBaseSize(50,50);
+            QGridLayout * layout = new QGridLayout();
+
+            QSpinBox * dx_spinbox = new QSpinBox();
+            //dx_spinbox->show();
+            dx_spinbox->setRange(1,65535);
+            dx_spinbox->setValue(dx);
+            QSpinBox * dy_spinbox = new QSpinBox();
+            //dy_spinbox->show();
+            dy_spinbox->setRange(1,65535);
+            dy_spinbox->setValue(dy);
+            QSpinBox * dz_spinbox = new QSpinBox();
+            //dz_spinbox->show();
+            dz_spinbox->setRange(1,65535);
+            dz_spinbox->setValue(dz);
+
+            layout->addWidget(new QLabel("dx"),0,0);
+            layout->addWidget(dx_spinbox,0,1,1,5);
+            layout->addWidget(new QLabel("dy"),1,0);
+            layout->addWidget(dy_spinbox,1,1,1,5);
+            layout->addWidget(new QLabel("dz"),2,0);
+            layout->addWidget(dz_spinbox,2,1,1,5);
+
+            QHBoxLayout * hbox3 = new QHBoxLayout();
+            QPushButton * ok = new QPushButton(" ok ");
+            ok->setDefault(true);
+            QPushButton * cancel = new QPushButton("cancel");
+            hbox3->addWidget(cancel);
+            hbox3->addWidget(ok);
+
+            layout->addLayout(hbox3,7,0,1,6);
+            dialog->setLayout(layout);
+
+
+            QObject::connect(ok, SIGNAL(clicked()), dialog, SLOT(accept()));
+            QObject::connect(cancel, SIGNAL(clicked()), dialog, SLOT(reject()));
+
+
+            if(dialog->exec() != QDialog::Accepted)
+                return false;
+
+            dx=dx_spinbox->value();
+            dy=dy_spinbox->value();
+            dz=dz_spinbox->value();
+
+            if(dialog)
+            {
+                delete dialog; dialog=0;
+            }
+        }
+
+        return true;
+
+    }
+};
+
 
 
 
