b51d754109e1cd029cf07ccd1e8eb6d37d887d70
liqi <644274027@qq.com>
2019-05-20 11:00:34 +0800

    update server.exe

--- hackathon/liqi/VR_Server/server.cpp
+++ hackathon/liqi/VR_Server/server.cpp
@@ -1,9 +1,10 @@
 #include "server.h"
 #include <QString>
 #include <QRegExp>
-
+#include<vector>
 
 std::vector<clientproperty> clientsproperty;
+std::vector<QString> messagelist;
 QString Creator;
 Server::Server(QObject* parent) : QObject(parent) {
     server = new QTcpServer(this);
@@ -28,6 +29,13 @@ void Server::sendUserList() {
 
 void Server::sendToAll(const QString& msg) {
     foreach (QTcpSocket* socket, clients.keys()) {
+        socket->write(msg.toUtf8());
+    }
+}
+void Server::sendToUser(const QString& msg,const QString& name) {
+    foreach (QTcpSocket* socket, clients.keys()) {
+        QString username = clients.value(socket);
+        if(username == name)
         socket->write(msg.toUtf8());
     }
 }
@@ -78,8 +86,9 @@ void Server::onDisconnect() {
 	for(int i=0;i<clientsproperty.size();i++)
 	{
 		if(clientsproperty.at(i).name!=username) continue;
-		clientsproperty.erase(clientsproperty.begin()+i);
-		i--;
+                //clientsproperty.erase(clientsproperty.begin()+i);
+                clientsproperty[i].online = false;
+                //i--;
 	}
     sendUserList();
 }
@@ -93,6 +102,7 @@ void Server::onReadyRead() {
 	QRegExp markerRex("^/marker:(.*)$");
 	QRegExp delmarkerRex("^/del_marker:(.*)$");
 	QRegExp dragnodeRex("^/drag_node:(.*)$");
+        QRegExp askmessageRex("^/ask:(.*)$");
     QTcpSocket* socket = (QTcpSocket*)sender();
     while (socket->canReadLine()) {
         QString line = QString::fromUtf8(socket->readLine()).trimmed();
@@ -104,103 +114,119 @@ void Server::onReadyRead() {
 									clientNum,
 									user,
                                                                         21,
-                                                                        false
+                                                                        false,
+                                                                        true,
+                                                                        0
+
 			}; 
 			
-			if(!historyclients.contains(user))
+             if(!containsClient(clientsproperty,user))// see if user is login before,if false ,push new user
 			{	
 				clientNum=(clientNum+1)%11;
 				qDebug()<<"the num of this client is " <<clientNum;
 				client00.colortype=clientNum+2;//21+(clientNum*20)%(275-21);
-				historyclients.insert(user,client00.colortype);
+                                //historyclients.insert(user,client00.colortype);
 				qDebug()<<"this client's colortype is  " <<client00.colortype;
+                                clientsproperty.push_back(client00);
 			}
 			else
 			{
-				client00.colortype = historyclients[user];
+                                //client00.colortype = historyclients[user];
+                clientsproperty[getuser(clientsproperty,user)].online = true;
 				qDebug()<<"the num of this client is " <<clientNum;
 				qDebug()<<"this client's colortype is  " <<client00.colortype;
 			}
-                        if(historyclients.size()==1||user == Creator)
+                        int onlineusernum = 0;
+                        for(int i= 0;i<clientsproperty.size();i++)// search online user num
                         {
-                            client00.Creator = true;
+                            if(clientsproperty[i].online)
+                                onlineusernum++;
+                        }
+                        if(onlineusernum == 1||user == Creator)//update user name
+                        {
+                            clientsproperty[getuser(clientsproperty,user)].Creator = true;
                             qDebug()<<"current creator is "<<user;
                             Creator = user;
                         }
                         sendToAll(QString("/system:" + user + " joined.\n"));
                         sendUserList();
-			clientsproperty.push_back(client00);
+
 			sendColorMsg();
                         sendCreatorMsg();
         }
         else if (messageRex.indexIn(line) != -1) {
             QString user = clients.value(socket);
             QString msg = messageRex.cap(1);
-            sendToAll(QString(user + ":" + msg + "\n"));
+           // sendToAll(QString(user + ":" + msg + "\n"));  now client ask for message ,instead of  send to all
+            messagelist.push_back(QString(user + ":" + msg + "\n"));
             qDebug() << "User:" << user;
             qDebug() << "Message:" << msg;
-			int ctype=21;
-			for(int i=0;i<clientsproperty.size();i++)
-			{
-				if(user==clientsproperty[i].name)
-				{
-					ctype = clientsproperty[i].colortype;
-					break;
-				}
-			}
-			AddToNTList(msg,ctype);
+            int ctype=21;
+            for(int i=0;i<clientsproperty.size();i++)
+            {
+                if(user==clientsproperty[i].name)
+                {
+                    ctype = clientsproperty[i].colortype;
+                    break;
+                }
+            }
+            AddToNTList(msg,ctype);
         }
         else if (deleteRex.indexIn(line) != -1) {
             QString user = clients.value(socket);
             QString delID = deleteRex.cap(1);
-            sendToAll(QString("/del_curve:" +user+" "+delID + "\n"));
-			qDebug() << "User:" << user<<" deleted Segment #"<< delID;
-			bool delerror = DeleteSegment(delID);
-			if(delerror==true)
-				qDebug()<<"Segment Deleted.";
-			else
-				qDebug()<<"Cannot Find the Segment ";
+           // sendToAll(QString("/del_curve:" +user+" "+delID + "\n"));
+             messagelist.push_back(QString("/del_curve:" +user+" "+delID + "\n"));
+            qDebug() << "User:" << user<<" deleted Segment #"<< delID;
+            bool delerror = DeleteSegment(delID);
+            if(delerror==true)
+                qDebug()<<"Segment Deleted.";
+            else
+                qDebug()<<"Cannot Find the Segment ";
         }
-		else if(markerRex.indexIn(line) != -1) {
-			 QString user = clients.value(socket);
-			 QString markermsg = markerRex.cap(1);
-			 sendToAll(QString("/marker:" +user+" "+markermsg + " \n"));
+        else if(markerRex.indexIn(line) != -1) {
+             QString user = clients.value(socket);
+             QString markermsg = markerRex.cap(1);
+            // sendToAll(QString("/marker:" +user+" "+markermsg + " \n"));
+             messagelist.push_back(QString("/marker:" +user+" "+markermsg + " \n"));
              qDebug() << "User:" << user;
              qDebug() << "marker Position:" << markermsg;
-		}
-		else if(delmarkerRex.indexIn(line) != -1) {
-			 QString user = clients.value(socket);
-			 QString delmarkerpos = delmarkerRex.cap(1);
-			 sendToAll(QString("/del_marker:" +user+" "+delmarkerpos + " \n"));
+        }
+        else if(delmarkerRex.indexIn(line) != -1) {
+             QString user = clients.value(socket);
+             QString delmarkerpos = delmarkerRex.cap(1);
+             //sendToAll(QString("/del_marker:" +user+" "+delmarkerpos + " \n"));
+             messagelist.push_back(QString("/del_marker:" +user+" "+delmarkerpos + " \n"));
              qDebug() << "User:" << user;
              qDebug() << "delete marker Position:" << delmarkerpos;
-		}
-		else if(dragnodeRex.indexIn(line) != -1) {
-			 QString user = clients.value(socket);
-			 QString dragnodepos = dragnodeRex.cap(1);
-			 sendToAll(QString("/drag_node:" +user+" "+dragnodepos + " \n"));
-			 QStringList dragnodePOS = dragnodeRex.cap(1).split(" ");
-			 if(dragnodePOS.size()<5) 
-			 {
-				 qDebug()<<"error! size < 5";
-				 return;
-			 }
-			 int ntnum = dragnodePOS.at(0).toInt();
-			 int swcnum = dragnodePOS.at(1).toInt();
-			 float mx = dragnodePOS.at(2).toFloat();
-			 float my = dragnodePOS.at(3).toFloat();
-			 float mz = dragnodePOS.at(4).toFloat();
+        }
+        else if(dragnodeRex.indexIn(line) != -1) {
+             QString user = clients.value(socket);
+             QString dragnodepos = dragnodeRex.cap(1);
+             //sendToAll(QString("/drag_node:" +user+" "+dragnodepos + " \n"));
+             messagelist.push_back(QString("/drag_node:" +user+" "+dragnodepos + " \n"));
+             QStringList dragnodePOS = dragnodeRex.cap(1).split(" ");
+             if(dragnodePOS.size()<5)
+             {
+                 qDebug()<<"error! size < 5";
+                 return;
+             }
+             int ntnum = dragnodePOS.at(0).toInt();
+             int swcnum = dragnodePOS.at(1).toInt();
+             float mx = dragnodePOS.at(2).toFloat();
+             float my = dragnodePOS.at(3).toFloat();
+             float mz = dragnodePOS.at(4).toFloat();
              qDebug() << "User:" << user;
              qDebug() << "drag node' seiral number and new Position:" << dragnodepos;
-			 //update node position in server' NTList
-			 if((ntnum>=NeuronList.size())||(ntnum<0)) return;
-			 if((swcnum<0)||(swcnum>=NeuronList.at(ntnum).listNeuron.size())) return;
-			 NeuronSWC ss = NeuronList.at(ntnum).listNeuron.at(swcnum);
-			 ss.x = mx;
-			 ss.y = my;
-			 ss.z = mz;
-			 NeuronList[ntnum].listNeuron[swcnum]=ss;
-		}
+             //update node position in server' NTList
+             if((ntnum>=NeuronList.size())||(ntnum<0)) return;
+             if((swcnum<0)||(swcnum>=NeuronList.at(ntnum).listNeuron.size())) return;
+             NeuronSWC ss = NeuronList.at(ntnum).listNeuron.at(swcnum);
+             ss.x = mx;
+             ss.y = my;
+             ss.z = mz;
+             NeuronList[ntnum].listNeuron[swcnum]=ss;
+        }
         else if (hmdposRex.indexIn(line) != -1) {
 			//qDebug()<<"run here";
             QString user = clients.value(socket);
@@ -209,6 +235,13 @@ void Server::onReadyRead() {
             //qDebug() << "User:" << user;
             //qDebug() << "HMD Position:" << hmd;
         }
+        else if (askmessageRex.indexIn(line) != -1)
+        {
+            QString user = clients.value(socket);
+            //QString msg = messageRex.cap(1);
+            //qDebug()<<"update user message with "<<user;
+            UpdateUsermessage(user);
+        }    
         else {
             qDebug() << "Bad message from " << socket->peerAddress().toString();
         }
@@ -286,3 +319,53 @@ bool Server::DeleteSegment(QString segName)
 	}
 	return false;
 }
+bool Server::containsClient(std::vector<clientproperty> Clients,QString name)
+{
+    for(int i=0;i<Clients.size();i++)
+    {
+        if(Clients[i].name == name )
+        {
+            return true;
+        }
+    }
+    return false;
+}
+int Server::getuser(std::vector<clientproperty> clients,QString name)
+{
+    for(int i=0;i<clients.size();i++)
+    {
+        if(clients[i].name == name )
+            return i;
+    }
+    return -1;
+}
+void Server::UpdateUsermessage(QString user)
+{
+        int messageindex = clientsproperty[getuser(clientsproperty,user)].messageindex;
+   //send to clientsproperty[getuser(user)]
+        //qDebug()<<"messageindex = "<<messageindex;
+        //qDebug()<<"messagelist.size = "<<messagelist.size();
+    if(messageindex < messagelist.size())
+    {
+
+        if(clientsproperty[getuser(clientsproperty,user)].online)
+        {
+            sendToUser(messagelist[messageindex],user);
+            clientsproperty[getuser(clientsproperty,user)].messageindex++;
+        qDebug()<<"send user message success";}
+        else
+            qDebug()<<"user is outline can't update messagelist";
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
