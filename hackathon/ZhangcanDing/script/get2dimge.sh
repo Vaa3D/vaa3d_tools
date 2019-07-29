c8b7d8c6b61db583186d1f62ab4dc861fc448639
ZhangcanDing <dingzhangcan@gmail.com>
2019-07-29 15:10:38 +0800

    add refine_swc script

--- hackathon/ZhangcanDing/script/get2dimge.sh
+++ hackathon/ZhangcanDing/script/get2dimge.sh
@@ -6,13 +6,16 @@ swcfolder=$1
 cd $swcfolder
 for i in `ls *auto.swc`
 do 
-	img=` echo ${i:0:30}*.v3draw `
+	img=` echo ${i:0:16}*.v3draw `
+	#img=`echo ${i%_*}.v3draw `
+	
 	vaa3d -x ML_get_sample -f get_2D_block -i $i -p $img 2 -o $swcfolder
 done
 
-for i in `ls *manual.eswc`
+for i in `ls *manual.swc`
 do 
-	img=` echo ${i:0:30}*.v3draw `
+	img=` echo ${i:0:16}*.v3draw `
+	#img=` echo ${i%_*}.v3draw` 
 	vaa3d -x ML_get_sample -f get_2D_block -i $i -p $img 1 -o $swcfolder
 done
 
