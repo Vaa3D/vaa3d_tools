bf906e16d7a8c379ca1488f4c6914622882c916d
MKRangers <hsienchik@alleninstitute.org>
2021-06-16 17:35:11 -0700

    reorganizing folders, deleting irrelevant stuff

--- hackathon/MK/Fragmented_autoTrace/progressMonitor.cpp
+++
@@ -1,7 +0,0 @@
-#include "progressMonitor.h"
-
-ProgressMonitor::ProgressMonitor(bool& terminationSwitch) : progressDlgPtr(new Ui::progressDlg)
-{
-	progressDlgPtr->setupUi(this);
-	this->show();
-}
