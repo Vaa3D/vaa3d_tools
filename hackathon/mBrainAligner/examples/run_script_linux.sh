a31ed9b0394cf35ca29a05c6f44488b3554f395c
Reaneyli <30484142+Reaneyli@users.noreply.github.com>
2021-08-06 01:10:17 +0800

    Update run_script_linux.sh

--- hackathon/mBrainAligner/examples/run_script_linux.sh
+++ hackathon/mBrainAligner/examples/run_script_linux.sh
@@ -17,34 +17,34 @@
 # -o the result save path
 
 # registration fMOST sample target
-../dist/linux_bin/global_registration  -f target/CCF_25_u8_xpad.v3draw -c target/CCF_mask.v3draw 
+../binary/linux_bin/global_registration  -f target/CCF_25_u8_xpad.v3draw -c target/CCF_mask.v3draw 
 -m subject/fMOST_18458_raw.v3draw  -p r+f+n -o result/fMOST/ -d 1
 
-../dist/linux_bin/local_registration  -p config/fMOST_config.txt -s result/fMOST/global.v3draw 
+../binary/linux_bin/local_registration  -p config/fMOST_config.txt -s result/fMOST/global.v3draw 
 -m subject/fMOST_segmentation/ -l target/target_landmarks/low_landmarks.marker  -g target/ -o result/fMOST/
 
 
 # registration LSFM sample target
-../dist/linux_bin/global_registration  -f target/CCF_25_u8_xpad.v3draw -c target/CCF_mask.v3draw 
+../binary/linux_bin/global_registration  -f target/CCF_25_u8_xpad.v3draw -c target/CCF_mask.v3draw 
 -m subject/LSFM_raw.v3draw  -p r+f+n -o result/LSFM/ -d 70
 
-../dist/linux_bin/local_registration  -p config/LSFM_config.txt -s result/LSFM/global.v3draw 
+../binary/linux_bin/local_registration  -p config/LSFM_config.txt -s result/LSFM/global.v3draw 
 -l target/target_landmarks/low_landmarks.marker  -g target/ -o result/LSFM/
 
 
 # registration MRI sample target
-../dist/linux_bin/global_registration  -f target/CCF_25_u8_xpad.v3draw -c target/CCF_mask.v3draw 
+../binary/linux_bin/global_registration  -f target/CCF_25_u8_xpad.v3draw -c target/CCF_mask.v3draw 
 -m subject/MRI_raw.v3draw  -p r+f+n -o result/MRI/ -d 20
 
-../dist/linux_bin/global_registration  -p config/MRI_config.txt -s result/MRI/global.v3draw 
+../binary/linux_bin/global_registration  -p config/MRI_config.txt -s result/MRI/global.v3draw 
 -l target/target_landmarks/low_landmarks.marker  -g target/ -o result/MRI/
 
 
 # registration VISoR sample target
-../dist/linux_bin/global_registration  -f target/CCF_25_u8_xpad.v3draw -c target/CCF_mask.v3draw 
+../binary/linux_bin/global_registration  -f target/CCF_25_u8_xpad.v3draw -c target/CCF_mask.v3draw 
 -m subject/VISoR_raw.v3draw  -p r+f+n -o result/VISoR/ -d 20
 
-../dist/linux_bin/local_registration  -p config/VISoR_config.txt -s result/VISoR/global.v3draw 
+../binary/linux_bin/local_registration  -p config/VISoR_config.txt -s result/VISoR/global.v3draw 
 -l target/target_landmarks/low_landmarks.marker  -g target/ -o result/VISoR/
 
 pause
