811eb1545f25774b90362fbea3abb684117c7897
reaneyli <30484142+reaneyli@users.noreply.github.com>
2022-04-24 22:39:21 +0800

    Remove the padding option
    The input marker file is no padding

--- hackathon/mBrainAligner/examples/swc_registration/run_swc_warp.bat
+++ hackathon/mBrainAligner/examples/swc_registration/run_swc_warp.bat
@@ -18,13 +18,12 @@
 :: -a <average_template image size_x>   input average_template brain image size_x.
 :: -b <average_template image size_y>   input average_template brain image size_y.
 :: -c <average_template image size_z>   input average_template brain image size_z.
-:: -p <pading x>       input template brain xpad, if you don't want to keep it, default 20.
 :: <the path to output swc file>
 :: -r <out_resample_swc>    output resample_swc file full name.
 :: -f <out_affine_swc>     output affine_swc file full name.
 :: -s <out_stps_swc>     output stps_swc file full name.
 
 
-swc_registration.exe -C ./sample_data\raw_RPM_tar.marker -M ./sample_data\raw_RPM_sub.marker -o ./sample_data\original_data.swc -T ./sample_data\ori_local_registered_tar.marker -S ./sample_data\ori_local_registered_sub.marker -d ./sample_data\raw_FFD_grid.swc -X 37184 -Y 54592 -Z 9952 -x 581 -y 853 -z 622 -a 568 -b 320 -c 456 -p 20 -r ./result\resample_data.swc -f ./result\global_data.swc -s ./result\local_registered_data.swc
+swc_registration.exe -C ./sample_data\raw_RPM_tar.marker -M ./sample_data\raw_RPM_sub.marker -o ./sample_data\original_data.swc -T ./sample_data\ori_local_registered_tar.marker -S ./sample_data\ori_local_registered_sub.marker -d ./sample_data\raw_FFD_grid.swc -X 37184 -Y 54592 -Z 9952 -x 581 -y 853 -z 622 -a 528 -b 320 -c 456 -r ./result\resample_data.swc -f ./result\global_data.swc -s ./result\local_registered_data.swc
 
 pause
