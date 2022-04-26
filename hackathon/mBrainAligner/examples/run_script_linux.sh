658a9f98e420ee96059156f19d27474b3b6de975
reaneyli <30484142+reaneyli@users.noreply.github.com>
2022-04-26 10:55:46 +0800

    Simplified Run Script

--- hackathon/mBrainAligner/examples/run_script_linux.sh
+++ hackathon/mBrainAligner/examples/run_script_linux.sh
@@ -1,45 +1,46 @@
-#！/bin/bash
-# <<global_registration.exe>> config
-# -f <input_fixed_image> .raw/.tif/.lsm file containing 3D template/target stack to which a subject image will be warped/mapped.        
-#                                     The .raw file is in V3D's RAW format.
-# -m <input_moving_image> .raw/.tif/.lsm file containing 3D template/target stack to which a subject image will be warped/mapped.        
-#                                     The .raw file is in V3D's RAW format.
-# -c <fixed_image_mask> The foreground mask of fixed image(When you select rpm(-p r) in the global registration, this option must be used).
-# -d <moving_image_threshold> This option can remove image artifacts(if less than the given threshold,then the pixel value = 0, the default is 30).
-# -p <global_registration_methods>，a:affine, r:rpm，f:ffd, example:a+f,r+f (It is recommended that you only need to select one option in "affine" and "rpm")
-# -o <Results_save_path> Global registration result will saved here.
+#<<global_registration.exe>> config
+# -f fixed_image
+# -m moving_image
+# -c recentered_image_pad_scr_image (if you select "rpm")
+# -d moving_image_threshold (if less than the given threshold,then the pixel value = 0. The default is 30)
+# -l the x-axis/y-axis/z-axispadding size of the target image
+# -p <registration_methods>，a:affine, r:rpm，f:ffd, example:a+f,r+f (It is recommended that you only need to select one option in "affine" and "rpm")
+# -o the result save path
+# -u <gpu mode>,0:gpu_off,1:gpu_on
 
 #<<local_registration.exe>> config
-# -p <algorithm config file>, If the user uses the registration sample data, the interface needs to provide the user with four modes of registration (fMOST,LSFM,VISoR,MRI), 
+# -p <Algorithm config file>, If the user uses the registration sample data, the interface needs to provide the user with four modes of registration (fMOST,LSFM,VISoR,MRI), 
 #                                and the corresponding config file is (the file name is fixed) :fMOST_config.txt,LSFM_config.txt,VISoR_config.txt,MRI_config.txt
-#                                If the user is registering with your own data, we need to generate "**.txt" file (see fMOST_config.txt file).
-# -s <input_moving_image> .raw/.tif/.lsm file containing 3D template/target stack to which a subject image will be warped/mapped. The .raw file is in V3D's RAW format.       
-#                          The input in the example is the result of global registration, so this option is set to "result/fMOST/global.v3draw".
-# -m <fMOST_segmentation_data> If the "Select_modal" is set to 0 in the fMOST_config.txt,, this option must be used. 
+#                                If the user is registering with private data, we need to generate "...txt" file (see fmost_config.txt file).
+# -s <global_result_image>  The file name is fixed. If the result save path is "result/fMOST/", 
+#                           then "-s" is "result/fMOST/global.v3draw".
+# -m <fmost_segmentation_result> 
 # -l <landmarks_file_path> example: "high_landmarks.txt" "middle_landmarks.txt" "low_landmarks.txt" 
-# -o <Results_save_path> Local registration result will saved here.
+# -o the result save path
+# -u <gpu mode>,0:gpu_off,1:gpu_on  If your runtime environment does not have GPU, please select "0"
 
-# registration fMOST sample target
-../binary/linux_bin/global_registration  -f target/CCF_25_u8_xpad.v3draw -c target/CCF_mask.v3draw -m subject/fMOST_18458_raw.v3draw  -p r+f+n -o result/fMOST/ -d 1
+# registration fMOST sample data
+../binary/linux_bin/global_registration  -f target/50um/  -m subject/fMOST_18458_raw.v3draw  -p r+f+n -o result/fMOST/ -d 1 -l 40+0+0 -u 1
+../binary/linux_bin/local_registration  -p config/fMOST_withou_seg_config.txt -s result/fMOST/global.v3draw -m subject/fMOST_segmentation_50um/ -l target/50um/target_landmarks/low_landmarks.marker  -g target/50um/ -o result/fMOST1/ -u 1  
 
-../binary/linux_bin/local_registration  -p config/fMOST_config.txt -s result/fMOST/global.v3draw -m subject/fMOST_segmentation/ -l target/target_landmarks/low_landmarks.marker  -g target/ -o result/fMOST/
-
-
-# registration LSFM sample target
-../binary/linux_bin/global_registration  -f target/CCF_25_u8_xpad.v3draw -c target/CCF_mask.v3draw -m subject/LSFM_raw.v3draw  -p r+f+n -o result/LSFM/ -d 70
-
-../binary/linux_bin/local_registration  -p config/LSFM_config.txt -s result/LSFM/global.v3draw -l target/target_landmarks/low_landmarks.marker  -g target/ -o result/LSFM/
+../binary/linux_bin/global_registration  -f target/50um/  -m subject/18052_red_mm_RSA_x2.nii.gz  -p r+f+n -o result/fMOST_nii/ -d 1 -l 20+0+0 -u 1
+../binary/linux_bin/local_registration  -p config/fMOST_config.txt -s result/fMOST_nii/global.nii.gz -l target/50um/target_landmarks/low_landmarks.marker  -g target/50um/ -o result/fMOST_nii/ -u 1  
 
 
-# registration MRI sample target
-../binary/linux_bin/global_registration  -f target/CCF_25_u8_xpad.v3draw -c target/CCF_mask.v3draw -m subject/MRI_raw.v3draw  -p r+f+n -o result/MRI/ -d 20
+# registration LSFM sample data
+../binary/linux_bin/global_registration  -f target/50um/ -m subject/LSFM_raw.v3draw  -p r+f+n -o result/LSFM/ -d 70 -l 30+30+30 -u 1
+../binary/linux_bin/local_registration  -p config/LSFM_config.txt -s result/LSFM/global.v3draw -l target/50um/target_landmarks/low_landmarks.marker  -g target/50um/ -o result/LSFM/ -u 1
 
-../binary/linux_bin/local_registration  -p config/MRI_config.txt -s result/MRI/global.v3draw -l target/target_landmarks/low_landmarks.marker  -g target/ -o result/MRI/
+# registration MRI sample data
+../binary/linux_bin/global_registration  -f target/50um/ -m subject/MRI_raw.v3draw  -p r+f+n -o result/MRI/ -d 20 -l 30+30+30 -u 1
+../binary/linux_bin/local_registration  -p config/MRI_config.txt -s result/MRI/global.v3draw -l target/50um/target_landmarks/low_landmarks.marker  -g target/50um/ -o result/MRI/ -u 1
 
+# registration VISoR sample data
+../binary/linux_bin/global_registration  -f target/50um/ -m subject/VISoR_raw.v3draw  -p r+f+n -o result/VISoR/ -d 20 -l 30+30+30 -u 1
+../binary/linux_bin/local_registration  -p config/VISoR_config.txt -s result/VISoR/global.v3draw -l target/50um/target_landmarks/low_landmarks.marker  -g target/50um/ -o result/VISoR/ -u 1
 
-# registration VISoR sample target
-../binary/linux_bin/global_registration  -f target/CCF_25_u8_xpad.v3draw -c target/CCF_mask.v3draw -m subject/VISoR_raw.v3draw  -p r+f+n -o result/VISoR/ -d 20
-
-../binary/linux_bin/local_registration  -p config/VISoR_config.txt -s result/VISoR/global.v3draw -l target/target_landmarks/low_landmarks.marker  -g target/ -o result/VISoR/
+# registration LSFM half brain sample data
+../binary/linux_bin/global_registration  -f target/half/  -m subject/LSFM_half_raw.v3draw  -p r+f -o result/light_sheet1/ -d 70 -l 20+30+30 -u 1
+../binary/linux_bin/local_registration  -p config/LSFM_half_config.txt -s result/light_sheet1/global.v3draw -l target/half/target_landmarks/high_landmarks_half.marker  -g target/half/ -o result/LSFM_half/ -u 1 
 
 pause
