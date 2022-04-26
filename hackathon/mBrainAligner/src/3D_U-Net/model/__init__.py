a1c5b7b3f67f60d41e8c53370ee0cbf6538e5ed5
reaneyli <30484142+reaneyli@users.noreply.github.com>
2022-04-26 10:21:42 +0800

    update

--- hackathon/mBrainAligner/src/3D_U-Net/model/__init__.py
+++ hackathon/mBrainAligner/src/3D_U-Net/model/__init__.py
@@ -1,3 +1,1 @@
-from .unet_model import unet_model_3d,FRnet,current_unet_model,FRnet_C
-from .classification_model import unet_classification_model_3d
-from .DS_model import DS_model,DS_C_model,UpdateDS_model
+from .unet_model import unet_model_3d
